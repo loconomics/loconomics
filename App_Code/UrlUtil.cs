95aa9c4905f5f9fbffa4e3ba2a605d4ccf5b5f82
IagoSRL <iagosrl@gmail.com>
2012-03-23 15:19:59 +0100

    Added a static class UrlUtil with utitility properties to know what is the current url, site and app, absolute and relative. With this, fixed an error in the popup, loading.gif and help-point limitation.

---
+++ App_Code/UrlUtil.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+
+/// <summary>
+/// Url Utilities
+/// </summary>
+public static class UrlUtil
+{
+    /// <summary>
+	/// Get the Site Root Url of the current request.
+    /// Example: http://www.loconomics.com
+	/// </summary>
+	/// <returns></returns>
+    public static string SiteUrl
+    {
+        get
+        {
+            HttpContext context = HttpContext.Current;
+            string baseUrl = context.Request.Url.Scheme + "://" + context.Request.Url.Authority;
+            return baseUrl;
+        }
+    }
+    /// <summary>
+	/// Get the current virtual app path, will be empty for root app.
+    /// Example: /beta/ or /
+	/// </summary>
+	/// <returns></returns>
+    public static string AppPath
+    {
+        get
+        {
+            HttpContext context = HttpContext.Current;
+            string url = context.Request.ApplicationPath;
+            if (url.EndsWith("/"))
+                return url;
+            else
+                return url + "/";
+        }
+    }
+    /// <summary>
+	/// Get the current virtual app url, is just the SiteUrl with the AppPath
+    /// Example: http://www.loconomics.com/beta/
+	/// </summary>
+	/// <returns></returns>
+    public static string AppUrl{
+        get
+        {
+            return SiteUrl + AppPath;
+        }
+    }
+}
