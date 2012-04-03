e9dec96db5076881d0082637bc140d77fae2059e
IagoSRL <iagosrl@gmail.com>
2012-04-03 10:00:11 +0200

    Enhanced the class UrlUtil with better LangPath and LangUrl implementations, with method to produce the Json structure to use the class from javascript.

--- App_Code/UrlUtil.cs
+++ App_Code/UrlUtil.cs
@@ -9,6 +9,7 @@ public static class UrlUtil
 {
     /// <summary>
 	/// Get the Site Root Url of the current request.
+    /// Never will return a trailing slash.
     /// Example: http://www.loconomics.com
 	/// </summary>
 	/// <returns></returns>
@@ -17,12 +18,12 @@ public static class UrlUtil
         get
         {
             HttpContext context = HttpContext.Current;
-            string baseUrl = context.Request.Url.Scheme + "://" + context.Request.Url.Authority;
-            return baseUrl;
+            return context.Request.Url.Scheme + "://" + context.Request.Url.Authority;
         }
     }
     /// <summary>
 	/// Get the current virtual app path, will be empty for root app.
+    /// Ever will return a begining and trailing slash (or unique slash if there is no path)
     /// Example: /beta/ or /
 	/// </summary>
 	/// <returns></returns>
@@ -40,6 +41,7 @@ public static class UrlUtil
     }
     /// <summary>
 	/// Get the current virtual app url, is just the SiteUrl with the AppPath
+    /// Ever will return a trailing slash
     /// Example: http://www.loconomics.com/beta/
 	/// </summary>
 	/// <returns></returns>
@@ -51,22 +53,52 @@ public static class UrlUtil
     }
     /// <summary>
 	/// Get the current virtual app path plus the language path
+    /// Ever will return a begining and trailing slash.
 	/// </summary>
 	/// <returns></returns>
     public static string LangPath{
         get
         {
-            return AppPath + "en_US/";
+            return AppPath + LangId + "/";
         }
     }
     /// <summary>
 	/// Get the current virtual app url plus the language path
+    /// Ever will return a trailing slash.
 	/// </summary>
 	/// <returns></returns>
     public static string LangUrl{
         get
         {
-            return AppUrl + "en_US/";
+            return AppUrl + LangId + "/";
         }
+    }
+    /// <summary>
+    /// Get the Lang Identifier in URL format.
+    /// Example: en_US, es_ES
+    /// </summary>
+    public static string LangId
+    {
+        get
+        {
+            return System.Globalization.CultureInfo.CurrentUICulture.Name.Replace('-', '_');
+        }
+    }
+    /// <summary>
+    /// Returns a Json string with all the static data included in this class.
+    /// Util to use from javascript code.
+    /// </summary>
+    /// <returns></returns>
+    public static string ToJson()
+    {
+        var d = new Dictionary<string, string>(){
+            { "SiteUrl", SiteUrl },
+            { "AppPath", AppPath },
+            { "AppUrl", AppUrl },
+            { "LangPath", LangPath },
+            { "LangUrl", LangUrl },
+            { "LangId", LangId }
+        };
+        return System.Web.Helpers.Json.Encode(d);
     }
 }
