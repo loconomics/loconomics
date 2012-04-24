b6eaaffc64bfdf742c97e84dfa41a2cc35170802
IagoSRL <iagosrl@gmail.com>
2012-04-24 18:24:07 +0200

    Added a new class LocoRessources to save common used strings. Fixed some errors on Validation use and added ValidationSummary to form pages, removing no needed checks like IsValid before prints the summary.

---
+++ App_Code/LocoRessources.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Ressources for common texts
+/// </summary>
+public static class LocoRessources
+{
+    public const string ValidationSummaryTitle = "Please correct the errors and try again:";
+}
