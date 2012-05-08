6f97b426aed5aaa3335908d2bc8a09cde9763bf1
IagoSRL <iagosrl@gmail.com>
2012-05-08 16:40:02 +0200

    Fixed a bug related to TabbedUX and DAshboard/Account/Personal.

--- Scripts/script.js
+++ Scripts/script.js
@@ -12,14 +12,10 @@ var errorBlock = function (error, reload, style) {
 };
 
 $.fn.HasScrollBar = function () {
+    if (!this) return false;
     //note: clientHeight= height of holder
     //scrollHeight= we have content till this height
-    var _elm = $(this)[0];
-    var _hasScrollBar = false;
-    if ((_elm.clientHeight < _elm.scrollHeight) || (_elm.clientWidth < _elm.scrollWidth)) {
-        _hasScrollBar = true;
-    }
-    return _hasScrollBar;
+    return (this.clientHeight < this.scrollHeight) || (this.clientWidth < this.scrollWidth);
 }
 
 /*
