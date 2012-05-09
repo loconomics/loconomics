ec98a3396c941e3f4be33cd62747a9efe6b0d68c
IagoSRL <iagosrl@gmail.com>
2012-05-09 16:06:59 +0200

    Added some helpers, generic functions. Started implementation of BookingRequestDetails page. Added JSON control to the Dashboard.js ajax call that load BookingRequestDetails.

--- Scripts/Dashboard.js
+++ Scripts/Dashboard.js
@@ -28,7 +28,33 @@
                 url: UrlUtil.LangPath + "Booking/$BookingRequestDetails/",
                 data: { BookingRequestID: $(this).data('booking-request-id') },
                 success: function (data, text, jx) {
-                    $tab.html(data);
+                    // If is a JSON result:
+                    if (typeof (data) === 'object') {
+                        // Special Code 1: do a redirect
+                        if (data.Code == 1) {
+                            window.location = data.Result;
+                        } else if (data.Code == 2) {
+                            $tab.unblock();
+                            popup(data.Result, { width: 410, height: 320 });
+                        } else { // data.Code < 0
+                            // There is an error code.
+
+                            // Unblock loading:
+                            box.unblock();
+                            // Block with message:
+                            var message = data.Code + ": " + (data.Result ? data.Result.ErrorMessage ? data.Result.ErrorMessage : data.Result : '');
+                            box.block({
+                                message: 'Error: ' + message,
+                                css: popupStyle(popupSize('small'))
+                            })
+                            .click(function () { box.unblock(); });
+
+                            // Do not unblock in complete function!
+                            autoUnblockLoading = false;
+                        }
+                    } else {
+                        $tab.html(data);
+                    }
                 },
                 error: ajaxErrorPopupHandler,
                 complete: function () {
