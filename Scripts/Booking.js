9975d5ca768b3da8ca93dbd3f8e91631b10203dc
IagoSRL <iagosrl@gmail.com>
2012-05-23 17:21:14 +0200

    Implemented Josh suggestion in Booking/Schedule: When a customer is to select a time, they should have the end time preset to be XX hours after the beginning.  The XX hours will come from the pricing wizard in step 1

--- Scripts/Booking.js
+++ Scripts/Booking.js
@@ -8,29 +8,104 @@
         sel.siblings('.enter-new-location, .enter-new-location:hidden').hide('fast');
     }
 }
+function lcTime (hour, minute, second) {
+    this.hour = Math.floor(parseFloat(hour || 0));
+    this.minute = Math.floor(parseFloat(minute || 0));
+    this.second = Math.floor(parseFloat(second || 0));
+
+    this.toString = function () {
+        var h = this.hour.toString();
+        if (h.length == 1)
+            h = '0' + h;
+        var m = this.minute.toString();
+        if (m.length == 1)
+            m = '0' + m;
+        var s = this.second.toString();
+        if (s.length == 1)
+            s = '0' + s;
+        return h + lcTime.delimiter + m + lcTime.delimiter + s;
+    };
+    this.addHours = function (hours) {
+        this.addMinutes(hours * 60);
+        return this;
+    };
+    this.addMinutes = function (minutes) {
+        var m = this.hour * 60 + this.minute;
+        var ntime = minutes + m;
+        this.hour = Math.floor(ntime / 60);
+        this.minute = ntime % 60;
+        return this;
+    };
+};
+// For spanish and english works good ':' as delimiter
+lcTime.delimiter = ':';
+lcTime.parse = function (strtime) {
+    strtime = (strtime || '').split(this.delimiter);
+    if (strtime.length > 1) {
+        return new lcTime(strtime[0], strtime[1], (strtime.length > 2 ? strtime[2] : 0));
+    }
+    return null;
+};
+
+
 $(document).ready(function () {
     // Check when location changes:
     $('body').delegate('.select-location', 'change', bookingChangeLocation);
 
     $('#schedule').bind('endLoadWizardStep', function () {
-        // Execute first time when showing the step
-        $.proxy(bookingChangeLocation, $('.select-location'))();
-    });
-    $('#schedule').bind('reloadedHtmlWizardStep', function () {
+        // Getting the tab content for payment that is not loaded at the start
+        var tab = $('#schedule');
+
+        // Loading, with retard
+        var loadingtimer = setTimeout(function () {
+            tab.block(loadingBlock);
+        }, gLoadingRetard);
+
+
+        $.ajax({
+            // Request $Payment partial page, with all the original url parameters
+            url: UrlUtil.LangPath + "Booking/$Schedule/" + location.search,
+            type: 'GET',
+            success: function (data, text, jx) {
+                // load tab content
+                tab.html(data);
+
+                // Execute first time when showing the step
+                $.proxy(bookingChangeLocation, $('.select-location'))();
+            },
+            error: ajaxErrorPopupHandler,
+            complete: function () {
+                // Disable loading
+                clearTimeout(loadingtimer);
+                // Unblock
+                tab.unblock();
+            }
+        });
+    }).bind('reloadedHtmlWizardStep', function () {
         // Execute after reloading html of this step
         $.proxy(bookingChangeLocation, $('.select-location'))();
+    }).delegate('.start-time :input', 'change', function () {
+        // a var serviceDurationHours must be created by the $Schedule page
+        if (typeof (serviceDurationHours) != 'undefined') {
+            // our minimum time interval is half hours, .5, round upper to that
+            var rest = serviceDurationHours % .5;
+            if (rest > 0)
+                serviceDurationHours = serviceDurationHours + 0.5 - rest;
+            var $t = $(this);
+            var starttime = lcTime.parse($t.val());
+            $t.closest('fieldset').find('.end-time :input').val(starttime.addHours(serviceDurationHours).toString());
+        }
     });
 
     // Load payment content on step change:
     $('#payment').bind('endLoadWizardStep', function () {
         // Getting the tab content for payment that is not loaded at the start
-        // like previous steps.
         var paymentTab = $('#payment');
 
         // Loading, with retard
         var loadingtimer = setTimeout(function () {
             paymentTab.block(loadingBlock);
-        }, 600);
+        }, gLoadingRetard);
 
         $.ajax({
             // Request $Payment partial page, with all the original url parameters
@@ -38,7 +113,7 @@ $(document).ready(function () {
             type: 'GET',
             success: function (data, text, jx) {
                 // load tab content
-                $('#payment').html(data);
+                paymentTab.html(data);
             },
             error: ajaxErrorPopupHandler,
             complete: function () {
