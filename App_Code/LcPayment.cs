9fb13385bfe20c84ed5c94e43b0cd98159eea380
IagoSRL <iagosrl@gmail.com>
2012-05-25 16:32:19 +0200

    Created the class LcPayment for general functions related with payments and Braintree (just now, the method NewBraintreeGateway gives a configured gateway ready to use, with all web.config configuration and on sandbox or production depending on that). Implemented the Account/Taxes form, but need some config on Braintree and testing.

---
+++ App_Code/LcPayment.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using Braintree;
+using System.Web.WebPages;
+
+/// <summary>
+/// Descripción breve de LcPayment
+/// </summary>
+public static class LcPayment
+{
+    public static BraintreeGateway NewBraintreeGateway()
+    {
+        BraintreeGateway gateway;
+        if (ConfigurationManager.AppSettings["Braintree.InSandbox"].AsBool()) {
+            //SandBox API keys for testing
+            gateway = new BraintreeGateway 
+            {
+                Environment = Braintree.Environment.SANDBOX,
+                MerchantId = ConfigurationManager.AppSettings["Braintree.Sandbox.MerchantId"], 
+                PublicKey = ConfigurationManager.AppSettings["Braintree.Sandbox.PublicKey"],
+                PrivateKey = ConfigurationManager.AppSettings["Braintree.Sandbox.PrivateKey"]
+            };
+        } else {
+            gateway = new BraintreeGateway
+            {
+                Environment = Braintree.Environment.PRODUCTION,
+                MerchantId = ConfigurationManager.AppSettings["Braintree.Production.MerchantId"],
+                PublicKey = ConfigurationManager.AppSettings["Braintree.Production.PublicKey"],
+                PrivateKey = ConfigurationManager.AppSettings["Braintree.Production.PrivateKey"]
+            };
+        }
+        return gateway;
+    }
+}
