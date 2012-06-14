bbcd7813164c35d747cf207d3e25d265ae8b05a9
IagoSRL <iagosrl@gmail.com>
2012-06-14 13:04:25 +0200

    Changed the 'get data code' for Service Cats and Attributes on Provider-sign-up. Added a new static class LcData with GetServiceCatsAndItsAttributes method (adapted code from JSON/GetServiceAttributes.cshtml), to use too from Dashboard PositionServices (to show all category attributes in a generic mode, without dupe code) Affect issue #1.

---
+++ App_Code/LcData.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebMatrix.Data;
+
+/// <summary>
+/// Descripción breve de LcData
+/// </summary>
+public static class LcData
+{
+    public static List<object> GetServiceCatsAndItsAttributes(int positionId, string filter = null)
+    {
+        var rcats = new List<object>();
+        var catsFilter = new List<int>();
+        
+        switch (filter) {
+            case "provider-services":
+                //catsFilter.AddRange(new int[]{1, 2, 3, 4, 5, 7});
+                //catsFilter.AddRange(new int[]{1, 2, 4, 5, 7});
+                break;
+        }
+
+        var sqlcat = "exec GetServiceAttributeCategories @0,1,1";
+        var sqlattribute = "exec GetServiceAttributes @0,@1,1,1";
+
+        using (var db = Database.Open("sqlloco"))
+        {
+            var catrow = db.Query(sqlcat, positionId);
+
+            // Iterate the categories
+            foreach (var cat in catrow)
+            {
+                // Apply filtering, if there are
+                if (catsFilter.Count > 0 && !catsFilter.Contains(cat.ServiceAttributeCategoryID))
+                {
+                    continue;
+                }
+                // Copy data to a new structure
+                var rcat = new Dictionary<string, object>(){
+                { "ServiceAttributeCategoryID", cat.ServiceAttributeCategoryID },
+                { "ServiceAttributeCategoryName", cat.ServiceCat }
+            };
+                // Getting attributes of the category
+                rcat["ServiceAttributes"] = db.Query(sqlattribute, positionId, cat.ServiceAttributeCategoryID);
+                rcats.Add(rcat);
+            }
+        }
+        return rcats;
+    }
+}
