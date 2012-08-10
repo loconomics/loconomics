5da01f2ba91411c392acc1617b23d72e23965da8
IagoSRL <iagosrl@gmail.com>
2012-08-10 12:52:06 +0200

    AvailabilityCalendar: removed the week number from the information, leaving only the week days (first and end days). #83

--- App_Code/LcRessources.cs
+++ App_Code/LcRessources.cs
@@ -29,7 +29,7 @@ public static class LcRessources
         ,{ "Language Level Description", "" }
 
         ,{ "postal-code-validation-error", "Postal Code is not valid" }
-        ,{ "quit-without-save", "You will lost changes if continue, re you sure?" }
+        ,{ "quit-without-save", "You will lost changes if continue, are you sure?" }
     };
     public static string GetText(string key) {
         if (textress.ContainsKey(key))
