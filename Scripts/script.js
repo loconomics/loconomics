4925ef1c26ec959a24ce3bc05d46e2abea34ba99
IagoSRL <iagosrl@gmail.com>
2012-10-08 14:03:47 +0200

    Added console wrapper to scripts.js to avoid problems with 'lost' console line scripts #14

--- Scripts/script.js
+++ Scripts/script.js
@@ -2,6 +2,16 @@
 // OUR namespace (abbreviated Loconomics), same as 'var LC' with caution
 window['LC'] = window['LC'] || {};
 
+/* ===============
+Console Wrapper
+*/
+// usage: log('inside coolFunc', this, arguments);
+// paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
+window.log = function f() { log.history = log.history || []; log.history.push(arguments); if (this.console) { var args = arguments, newarr; args.callee = args.callee.caller; newarr = [].slice.call(args); if (typeof console.log === 'object') log.apply.call(console.log, console, newarr); else console.log.apply(console, newarr); } };
+// make it safe to use console.log always
+(function (a) { function b() { } for (var c = "assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","), d; !!(d = c.pop()); ) { a[d] = a[d] || b; } })
+(function () { try { console.log(); return window.console; } catch (a) { return (window.console = {}); } } ());
+
 /* Generic blockUI options sets */
 var loadingBlock = { message: '<img src="' + UrlUtil.AppPath + 'img/loading.gif"/>' };
 var errorBlock = function (error, reload, style) {
