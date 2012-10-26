c34315f35c68e982fcece9621a731d18ea93e18b
IagoSRL <iagosrl@gmail.com>
2012-10-26 13:50:23 +0200

    Fixed the problem with validation errors attached to the element being showed still after being fixed with a form.ajax execution valid (specially that cases in that popup success message is showed). #141 Added general setup of validation scripts on scripts.js as a LC function, re-used on LcHelpers and adapted to use 'window' object for some global variables scope. #14 Fixed other problem with validation: after a successful save, client-side validation stop works; now validation logic is reapllied after a save, what means is working still as expected.

--- Scripts/script.js
+++ Scripts/script.js
@@ -534,6 +534,28 @@ LC.connectPopupAction = function (applyToSelector) {
         return false;
     });
 }
+LC.setupValidation = function (reapplyOnlyTo) {
+    reapplyOnlyTo = reapplyOnlyTo || document;
+    if (!window['jqueryValidateUnobtrusiveLoaded']) window['jqueryValidateUnobtrusiveLoaded'] = false;
+    if (!jqueryValidateUnobtrusiveLoaded) {
+        jqueryValidateUnobtrusiveLoaded = true;
+        Modernizr.load([
+                { load: UrlUtil.AppPath + "Scripts/jquery/jquery.validate.min.js" },
+                { load: UrlUtil.AppPath + "Scripts/jquery/jquery.validate.unobtrusive.min.js" }
+            ]);
+    } else {
+        // Check first if validation is enabled (can happen that twice includes of
+        // this code happen at same page, being executed this code after first appearance
+        // with the switch jqueryValidateUnobtrusiveLoaded changed
+        // but without validation being already loaded and enabled)
+        if ($ && $.validator && $.validator.unobtrusive) {
+            // Apply the validation rules to the new elements
+            $(document).removeData('validator');
+            $(document).removeData('unobtrusiveValidation');
+            $.validator.unobtrusive.parse(reapplyOnlyTo);
+        }
+    }
+}
 
 /*******************
 * Popup related 
@@ -870,6 +892,17 @@ function setValidationSummaryAsValid(container) {
     .removeClass('validation-summary-errors')
     .addClass('validation-summary-valid')
     .find('>ul>li').remove();
+
+    // Set all fields validation inside this form (affected by the summary too)
+    // as valid too
+    $('.field-validation-error', container)
+    .removeClass('field-validation-error')
+    .addClass('field-validation-valid')
+    .text('');
+
+    // Re-apply setup validation to ensure is working, because just after a successful
+    // validation, asp.net unobtrusive validation stops working on client-side.
+    LC.setupValidation();
 }
 function guidGenerator() {
     var S4 = function () {
@@ -1630,4 +1663,14 @@ $(function () {
             TabbedUX.getTabContext(this).menuitem.removeClass('notify-changes'); //has-tooltip
         });
     })();
+
+
+    /*
+     * Communicate that script.js is ready to be used
+     * and the common LC lib too.
+     * Both are ensure to be raised ever after page is ready too.
+     */
+    $(document)
+    .trigger('lcScriptReady')
+    .trigger('lcLibReady');
 });
