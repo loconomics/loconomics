e1966765477f3830011deb8f97950d0b86dac7d0
IagoSRL <iagosrl@gmail.com>
2012-11-08 19:29:03 +0100

    New booking/schedule/, date-picker styled and with new script and date-picker options; fixed a bug in scripts after a post-back. #148

--- Scripts/Booking.js
+++ Scripts/Booking.js
@@ -133,6 +133,27 @@ LC.setupScheduleCalendar = function () {
     });
 };
 
+LC.initScheduleStep = function () {
+    var tab = $('#schedule');
+
+    // Execute first time when showing the step
+    $.proxy(bookingChangeLocation, $('.select-location'))();
+
+    applyDatePicker(tab);
+
+    // Read current hidden date to be set in show date (previous apply datepicker options)
+    var date = $('#hideDate').val();
+
+    $("#showDate")
+    .datepicker('setDate', date)
+    .datepicker('option', 'dateFormat', 'DD, M d, yy')
+    .datepicker('option', 'altField', $('#hideDate'))
+    .datepicker('option', 'altFormat', $.datepicker._defaults.dateFormat)
+    .datepicker('option', 'numberOfMonths', 2);
+
+    LC.setupScheduleCalendar();
+};
+
 $(document).ready(function () {
     // Setup Schedule step:
     $('#schedule').on('endLoadWizardStep', function () {
@@ -152,12 +173,7 @@ $(document).ready(function () {
                 // load tab content
                 tab.html(data);
 
-                // Execute first time when showing the step
-                $.proxy(bookingChangeLocation, $('.select-location'))();
-
-                applyDatePicker(tab);
-
-                LC.setupScheduleCalendar();
+                LC.initScheduleStep();
             },
             error: ajaxErrorPopupHandler,
             complete: function () {
@@ -168,63 +184,7 @@ $(document).ready(function () {
             }
         });
     }).on('reloadedHtmlWizardStep', function () {
-        // Execute after reloading html of this step
-        $.proxy(bookingChangeLocation, $('.select-location'))();
-        LC.setupScheduleCalendar();
-    });
-    $('body').on('change', '.start-time :input', function () {
-        // a var serviceDurationHours must be created by the $Schedule page
-        if (typeof (serviceDurationHours) != 'undefined') {
-            // our minimum time interval is half hours, .5, round upper to that
-            var rest = serviceDurationHours % .5;
-            if (rest > 0)
-                serviceDurationHours = serviceDurationHours + 0.5 - rest;
-            var $t = $(this);
-            var starttime = lcTime.parse($t.val());
-            $t.closest('fieldset').find('.end-time :input').val(starttime.addHours(serviceDurationHours).toDayTimeString());
-        }
-    })
-    .on('change', '.select-location', bookingChangeLocation)
-    .on('click', '.availability-calendar .datetimes > li', function () {
-        var $t = $(this);
-        if ($t.hasClass('free') || $t.hasClass('tentative')) {
-            // Recognizing selected day and time
-            var pos = $t.index() + 1; // base 1
-            var row = Math.ceil(pos / 7) - 1; // base 0
-            var col = (pos % 7) - 1; // base 0
-            // For selDay, first we get the first-week-day from calendar and then calculate the selected:
-            var selDay = new Date($t.closest('.calendar').data('showed-date'));
-            selDay.setDate(selDay.getDate() + col);
-            var selHour = new lcTime(7 + (row / 2), (row % 2 == 1 ? 30 : 0), 0);
-            //console.log('Selected date-time: ' + selDay.getFullYear() + '/' + (selDay.getMonth() + 1) + '/' + selDay.getDate() + " " + selHour);
-            // Setting in the form the date and time
-            var $f = $t.closest('.tab-body').find('form.schedule');
-            var fields = preferredF = getSelectDateTimeFieldsFor('preferred-option', $f);
-            if (fields.date.val())
-                fields = getSelectDateTimeFieldsFor('alternative-option-1', $f);
-            if (fields.date.val())
-                fields = getSelectDateTimeFieldsFor('alternative-option-2', $f);
-            if (fields.date.val())
-                fields = preferredF;
-            fields.date.val($.datepicker.formatDate($.datepicker._defaults.dateFormat, selDay));
-            fields.startTime.val(selHour.toString())
-                .change(); // Force change event to auto-update endTime
-
-            // If is in a popup:
-            var popupContainer = $t.closest('.calendar').data('popup-container');
-            if (popupContainer)
-                smoothBoxBlock(null, popupContainer);
-        }
-
-        // dateSet can be: preferred-option, alternative-option-1, alternative-option-2
-        function getSelectDateTimeFieldsFor(dateSet, $container) {
-            $container = $container.find('.select-date-time.' + dateSet);
-            return {
-                date: $container.find('.date :input'),
-                startTime: $container.find('.start-time :input'),
-                endTime: $container.find('.end-time :input')
-            };
-        }
+        LC.initScheduleStep();
     });
 
     // Load payment content on step change:
