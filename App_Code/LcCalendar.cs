1b93b8bc72b3765a1a9e67629f4c6d976da5e62f
IagoSRL <iagosrl@gmail.com>
2013-01-15 18:14:16 +0100

    Created a new helper class for Calendar data and tasks called LcCalendar to wrap the complicated CalendarDll API created CASS. #149 Implemented a static method to retrieve the Availability of an user to be used in AvailabilityCalendarWidget

---
+++ App_Code/LcCalendar.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CalendarDll;
+using DDay.iCal;
+using DDay.Collections;
+
+/// <summary>
+/// Descripción breve de LcCalendar
+/// </summary>
+public static class LcCalendar
+{
+    public static List<CalendarDll.ProviderAvailabilityResult> GetUserAvailability(int userID, DateTime dateStart, DateTime dateEnd)
+    {
+        var lcCalendar = new CalendarDll.CalendarUtils();
+        return
+            lcCalendar.GetFreeEvents(
+                new CalendarDll.CalendarUser(userID),
+                dateStart,
+                dateEnd,
+                DateTime.Now);
+    }
+}
