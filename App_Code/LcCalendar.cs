7be885a50c17d35c608493f27355c68951792f56
IagoSRL <iagosrl@gmail.com>
2013-01-16 11:27:48 +0100

    Implemented CheckUserAvailability in LcCalendar to replace previous stored procedure CheckProviderAvailability, connected to the CASS Calendar code (they didn't implemented the equivalent method but is implemented in the similar way than the old sql but getting the availability table from their methods). #149

--- App_Code/LcCalendar.cs
+++ App_Code/LcCalendar.cs
@@ -26,4 +26,30 @@ public static class LcCalendar
             return db.Query("exec dbo.GetProviderAvailabilityFullSet @0, @1", userID, date));
         }*/
     }
+    /// <summary>
+    /// Check if the user is available for all the time between dateStart and dateEnd
+    /// </summary>
+    /// <param name="userID">UserID as in database</param>
+    /// <param name="dateStart">Start date and time for the time range (greater or equals than dateStart)</param>
+    /// <param name="dateEnd">End date and time for the time range (less than dateEnd)</param>
+    /// <returns>True when is available, False when not</returns>
+    public static bool CheckUserAvailability(int userID, DateTime dateStart, DateTime dateEnd)
+    {
+        foreach (var e in GetUserAvailability(userID, dateStart, dateEnd))
+        {
+            var edt = e.DateSet + e.TimeBlock;
+            if (e.CalendarAvailabilityTypeID != (int)CalendarDll.AvailabilityTypes.FREE &&
+                edt >= dateStart &&
+                edt < dateEnd)
+                return false;
+        }
+        return true;
+
+        // Previous CASS code:
+        /*
+         using (var db = Database.Open("sqlloco")) {
+            return !(bool)db.QueryValue("exec dbo.CheckProviderAvailability @0,@1,@2", userID, dateStart, dateEnd)
+         }
+         */
+    }
 }
