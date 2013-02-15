80253a92ee7eeb2dde50ffa0122067e972daba1f
IagoSRL <iagosrl@gmail.com>
2013-02-15 14:16:50 +0100

    Changed an internal URL affected by #188 changes and added script to update Alert table with new URLs.

---
+++ _DBUpdate/Update Alerts for issue-188.sql
@@ -0,0 +1,16 @@
+﻿UPDATE alert SET
+AlertPageURL = 'Dashboard/Calendar/#availability-form'
+WHERE AlertID = 3
+GO
+UPDATE alert SET
+AlertPageURL = 'Dashboard/Account/#account-profile-profile'
+WHERE AlertID = 9
+GO
+UPDATE alert SET
+AlertPageURL = 'Dashboard/Account/#account-verifications'
+WHERE AlertID = 10
+GO
+UPDATE alert SET
+AlertPageURL = 'Dashboard/Account/#account-verifications'
+WHERE AlertID = 11
+GO
