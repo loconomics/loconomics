bc0b1d7553ace18abd8bfa27b73a5f699ed8d685
IagoSRL <iagosrl@gmail.com>
2013-03-15 11:59:53 +0100

    Added extra feature to our jQuery function hasScrollBar (extragap parameter) and used with a 2 pixels gap to try solve the issue #194 about tabs slider on HiDPI Mac

--- Scripts/script.js
+++ Scripts/script.js
@@ -55,14 +55,28 @@ Array.remove = function (anArray, from, to) {
 /*
  * Our jQuery additions (small plugins)
  */
-$.fn.hasScrollBar = function () {
+/**
+  * HasScrollBar returns an object with bool properties 'vertical' and 'horizontal'
+  * saying if the element has need of scrollbars for each dimension or not (element
+  * can need scrollbars and still not being showed because the css-overlflow property
+  * being set as 'hidden', but still we know that the element requires it and its
+  * content is not being fully displayed).
+  * @extragap, defaults to {x:0,y:0}, lets specify an extra size in pixels for each dimension that alter the real check,
+  * resulting in a fake result that can be interesting to discard some pixels of excess
+  * size (negative values) or exagerate the real used size with that extra pixels (positive values).
+ **/
+$.fn.hasScrollBar = function (extragap) {
+    extragap = $.extend({
+        x: 0,
+        y: 0
+    }, extragap);
     if (!this || this.length == 0) return { vertical: false, horizontal: false };
     //note: clientHeight= height of holder
     //scrollHeight= we have content till this height
     var t = this.get(0);
     return {
-        vertical: this.outerHeight(false) < t.scrollHeight,
-        horizontal: this.outerWidth(false) < t.scrollWidth
+        vertical: this.outerHeight(false) < (t.scrollHeight + extragap.y),
+        horizontal: this.outerWidth(false) < (t.scrollWidth + extragap.x)
     };
 }
 /**
@@ -226,7 +240,7 @@ var TabbedUX = {
     },
     setupSlider: function (tabContainer) {
         var ts = tabContainer.children('.tabs-slider');
-        if (tabContainer.children('.tabs').hasScrollBar().horizontal == true) {
+        if (tabContainer.children('.tabs').hasScrollBar({x:-2}).horizontal == true) {
             tabContainer.addClass('has-tabs-slider');
             if (ts.length == 0) {
                 ts = document.createElement('div');
