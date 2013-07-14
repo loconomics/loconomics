7511367de2e73eccc6dc961897c8b96d46b9cb43
IagoSRL <iagosrl@gmail.com>
2013-07-14 22:27:12 +0200

    Minor change on popup text after save a pricing on cleaning-services, #317

--- web/App_Code/LcPricing/LcPricing.PackageBasePricingTypeConfigs.cs
+++ web/App_Code/LcPricing/LcPricing.PackageBasePricingTypeConfigs.cs
@@ -260,7 +260,7 @@ public static partial class LcPricingModel
                 PriceRateIsRequiredValidationError = "You must set your hourly rate",
 
                 SuccessOnDelete = "Service removed succesfully",
-                SuccessOnSave = "Add/Edit pricings",
+                SuccessOnSave = "Add/Edit pricing",
 
                 SummaryFormat = "{0}",
                 NameAndSummaryFormat = "{0}, {1}",
@@ -294,7 +294,7 @@ public static partial class LcPricingModel
                 PriceRateIsRequiredValidationError = "You must set your hourly rate",
 
                 SuccessOnDelete = "Service removed succesfully",
-                SuccessOnSave = "Add/Edit pricings",
+                SuccessOnSave = "Add/Edit pricing",
 
                 SummaryFormat = "{0}",
                 NameAndSummaryFormat = "{0}, {1}",
