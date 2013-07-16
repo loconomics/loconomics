61f6be839a0a997a73c371152d922267148fe9aa
IagoSRL <iagosrl@gmail.com>
2013-07-16 16:59:55 +0200

    Fixed an error checking user-verification 11 in sql procedure #324

--- web/_DBUpdate/Issue324-E- ut_AutocheckReviewVerifications.sql
+++ web/_DBUpdate/Issue324-E- ut_AutocheckReviewVerifications.sql
@@ -14,7 +14,7 @@ AS BEGIN
 	DECLARE @UserID int, @PositionID int, @RevDate datetime
 	
 	----------------------------------
-	-- 12: Reviewd from former clients
+	-- Reviews
 	
 	SET @cur = CURSOR FOR
 		SELECT	UserID, PositionID
@@ -36,7 +36,7 @@ AS BEGIN
 		IF @RevDate is not null
 			-- There is reviews from former clients, verification confirmed
 			EXEC SetUserVerification @UserID, 12, @RevDate, 1, @PositionID
-		ELSE
+		ELSE BEGIN
 			-- Check if there is a verification already
 			SET @RevDate = null
 			SELECT TOP 1 @RevDate = CreatedDate
@@ -49,6 +49,20 @@ AS BEGIN
 				-- show the verification as done.
 				-- Verification specific for the position
 				EXEC SetUserVerification @UserID, 12, @RevDate, 2, @PositionID
+		END
+
+		-- Check 11: 'Loconomics user reviewed'
+		SET @RevDate = null
+		SELECT TOP 1 @RevDate = CreatedDate
+		FROM UserReviews
+		WHERE ProviderUserID = @UserID
+			AND BookingID > 0
+			AND PositionID = @PositionID
+
+		IF @RevDate is not null
+			EXEC SetUserVerification @UserID, 11, @RevDate, 1, @PositionID
+		ELSE
+			EXEC DelUserVerification @UserID, 11, @PositionID
 
 		FETCH NEXT FROM @cur INTO @UserID, @PositionID
 	END
@@ -56,7 +70,7 @@ AS BEGIN
 	DEALLOCATE @cur
 
     -------------------------------
-	-- 11: Loconomics user reviewed
+	-- Final check
 	
 	SET @cur = CURSOR FOR
 		SELECT	UserID
@@ -67,18 +81,10 @@ AS BEGIN
 	FETCH NEXT FROM @cur INTO @UserID
 	WHILE @@FETCH_STATUS = 0 BEGIN
 
-		-- Check 11: 'Loconomics user reviewed'
-		SET @RevDate = null
-		SELECT TOP 1 @RevDate = CreatedDate
-		FROM UserReviews
-		WHERE ProviderUserID = @UserID
-			AND BookingID > 0
+		-- Remove old user-verifications for 'loconomics reviews' without positionID,
+		-- that doesn't work (and check was already done in previous loop)
+		EXEC DelUserVerification @UserID, 11, 0
 
-		IF @RevDate is not null
-			EXEC SetUserVerification @UserID, 11, @RevDate, 1
-		ELSE
-			EXEC DelUserVerification @UserID, 11
-			
 		-- Remove old user-verifications for 'former customers' without positionID,
 		-- that doesn't work (and check was already done in previous loop)
 		EXEC DelUserVerification @UserID, 12, 0
@@ -93,3 +99,5 @@ END
 GO
 
 EXEC ut_AutocheckReviewVerifications
+
+EXEC TestAllUsersAlerts
