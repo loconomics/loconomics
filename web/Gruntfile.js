5309c64ec6635b70965b83d4211bda0c273362d6
IagoSRL <iagosrl@gmail.com>
2014-01-10 14:17:36 +0100

    Splited some styl/css files for better management of lots of CSS for new dahsboard, added page for Promote adding the old 'website tools' as 'booknowbutton', needs styling. #455

--- web/Gruntfile.js
+++ web/Gruntfile.js
@@ -164,10 +164,9 @@ module.exports = function(grunt) {
           //paths: ['path/to/import', 'another/to/import'],
           // use embedurl('test.png') in our code to trigger Data URI embedding
           urlfunc: 'embedurl',
+          'include css': true,
           banner: assetsBannerTpl
-          /*use: [
-            require('fluidity') // use stylus plugin at compile time
-          ],
+          /*
           import: [      //  @import 'foo', 'bar/moo', etc. into every .styl file
             'foo',       //  that is compiled. These might be findable based on values you gave
             'bar/moo'    //  to `paths`, or a plugin you added under `use`
