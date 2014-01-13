d9494d686778e21cd897b91b39ad42ea248a5f86
IagoSRL <iagosrl@gmail.com>
2014-01-13 22:43:24 +0100

    Solved a problem with the minified version of common.css; an option of CSSMin is a but buggy (advanced mode, enabled by default for lower sizes; it removes some attributes at html:before). Updated assets version to update dev and testing. #455

--- web/Gruntfile.js
+++ web/Gruntfile.js
@@ -185,7 +185,11 @@ module.exports = function(grunt) {
           // Eliminamos todos los comentarios incluso el banner original
           keepSpecialComments: 0,
           // Se añade el banner de nuevo, que incluye un salto de línea antes del código
-          banner: assetsBannerTpl
+          banner: assetsBannerTpl,
+          /* NOTE: Disabled the advanced mode because causes next know problems with our code:
+            - html:before{ background gradient }  it gets removed some rules, letting background white
+           */
+          noAdvanced: true
         },
         files: {
           'Styles/common.min.css': ['Styles/common.css'],
