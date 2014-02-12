3ce17c3f0bff7217883cee5210d4c4ed71bb7ce0
IagoSRL <iagosrl@gmail.com>
2014-02-12 20:37:14 +0100

    new-dashboard #455, calendar #454: added 'sync your existing calendar' to Availability section, with better than before 'reset button' and minor html/css fixes.

---
+++ web/Scripts/app/dashboard/appointmentsCrudl.js
@@ -0,0 +1,28 @@
+﻿/** Availability: calendar appointments page setup for CRUDL use
+**/
+var $ = require('jquery');
+
+// TODO: Replace by real require and not global LC:
+//var ajaxForms = require('../LC/ajaxForms');
+//var crudl = require('../LC/crudl').setup(ajaxForms.onSuccess, ajaxForms.onError, ajaxForms.onComplete);
+//LC.initCrudl = crudl.on;
+var initCrudl = LC.initCrudl;
+
+exports.on = function (containerSelector) {
+
+  var $c = $(containerSelector),
+    crudlSelector = '.DashboardAppointments',
+    $crudlContainer = $c.find(crudlSelector).closest('.DashboardSection-page-section'),
+    $others = $crudlContainer.siblings().add($crudlContainer.find('.DashboardSection-page-section-introduction'));
+
+  var crudl = initCrudl(crudlSelector);
+
+  crudl.elements
+  .on(crudl.settings.events['edit-starts'], function () {
+    $others.xhide({ effect: 'height', duraction: 'slow' });
+  })
+  .on(crudl.settings.events['edit-ends'], function () {
+    $others.xshow({ effect: 'height', duraction: 'slow' });
+  });
+
+};
--- web/Scripts/app/new-dashboard.js
+++ web/Scripts/app/new-dashboard.js
@@ -82,28 +82,8 @@ $(function () {
     }
   });
 
-  /* Calendar */
-  var availabilityCalendar = require('LC/availabilityCalendar');
-  var workHoursList = availabilityCalendar.WorkHours.enableAll();
-  // Setuping the WorkHours calendar data save when the form is submitted
-  $.each(workHoursList, function (i, v) {
-    var workhours = this;
-    var form = workhours.$el.closest('form.ajax, fieldset.ajax');
-    var field = form.find('[name=workhours]');
-    if (field.length === 0)
-      field = $('<input type="hidden" name="workhours" />').appendTo(form);
-    form.on('presubmit', function () {
-      field.val(JSON.stringify(workhours.data));
-    });
-
-
-    // Disabling calendar on field alltime
-    form.find('[name=alltime]').on('change', function () {
-      var $t = $(this),
-        cl = workhours.classes.disabled;
-      if (cl)
-        workhours.$el.toggleClass(cl, $t.prop('checked'));
-    });
-
-  });
+  /* Availabilty */
+  require('./dashboard/weeklySchedule').on();
+  require('./dashboard/calendarSync').on();
+  require('./dashboard/appointmentsCrudl').on('.DashboardAvailability');
 });
