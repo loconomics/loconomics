4cbcfea51069ef6e02348059e99721084c96abea
IagoSRL <iagosrl@gmail.com>
2014-06-24 19:50:09 +0200

    Added instant-saving #524 to availability/calendar-sync

--- web/Scripts/app/dashboard.js
+++ web/Scripts/app/dashboard.js
@@ -112,6 +112,7 @@ function initAvailability(e) {
   // Instant saving
   setInstantSavingSection('.DashboardWeeklySchedule');
   setInstantSavingSection('.DashboardMonthlySchedule');
+  setInstantSavingSection('.DashboardCalendarSync');
 }
 
 /**
