f754c31d8365fbcc0ba2a64f9f148673a9fee36c
IagoSRL <iagosrl@gmail.com>
2014-07-15 22:46:49 +0200

    Created a Test page to do quick testing of the REST API, created REST folder with a Test file that implements an example of REST responses for GET and POST http methods, and placeholders for PUT and DELETE, some pieces of generic reusable code. Created class RESTExtensions for any method extension that aids with REST implementation, and added a ModelState extension to get the full validation/state errors in a suitable way to send as JSON for REST responses. #530

---
+++ web/App_Code/RESTExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Collections;
+using System.Web.WebPages.Html;
+
+/// <summary>
+/// Collection of extensions useful for implementation
+/// of a RESTful API
+/// </summary>
+public static class RESTExtensions
+{
+    public static IEnumerable ErrorsDictionary(this ModelStateDictionary modelState)
+    {
+        if (!modelState.IsValid)
+        {
+            return modelState.ToDictionary(kvp => kvp.Key,
+                kvp => kvp.Value.Errors
+                                .Select(e => e)).ToArray()
+                                .Where(m => m.Value.Count() > 0);
+        }
+        return null;
+    }
+
+    public static IEnumerable Errors(this ModelStateDictionary modelState)
+    {
+        var errors = new Hashtable();
+        foreach (var pair in modelState)
+        {
+            if (pair.Value.Errors.Count > 0)
+            {
+                errors[pair.Key] = pair.Value.Errors.ToList();
+            }
+        }
+        return errors;
+    }
+}
