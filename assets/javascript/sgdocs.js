d41aa31607aab78378702c5717102b0a8c0af4e4
IagoSRL <iagosrl@gmail.com>
2014-10-20 14:49:39 +0200

    Styleguide #575: Enhanced code samples presentation of html removing unneed whitespaces on lines start but preserving the relative indentation.

--- assets/javascript/sgdocs.js
+++ assets/javascript/sgdocs.js
@@ -8,14 +8,23 @@ jQuery(escapeHtmlCodeSamples);
 /**
     Prepare blocks of HTML code samples (elements 'code' with
     class 'html') to be scaped in order to being displayed as
-    code rathern than as html.
+    code rather than as html.
 **/
 function escapeHtmlCodeSamples($){
     $('code.html').each(function() {
         var $t = $(this),
             code = $t.html();
 
-        code = code.replace(/^\s*|\s*$/gm, '');
+        // Remove any blank line on beggining and ending
+        code = code.replace(/^\s*\n|\s*\n?$/gm, '');
+        
+        // Remove indentation of the code example in the original source
+        // but preserving indentation from the first element in the sample:
+        var m = code.match(/^(\s+)/);
+        if (m && m[0])
+            code = code.replace(new RegExp('^' + m[0], 'gm'), '');
+
+        // Encoding :-)
         $t.text(code);
     });
 }
