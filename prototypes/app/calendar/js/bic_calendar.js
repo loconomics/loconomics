fc25e57fd25eb180fe5e09cad7a9ccb10b1f9955
IagoSRL <iagosrl@gmail.com>
2014-11-04 13:41:26 +0100

    Styleguide prototype #575, calendar widget updated, general CSS tweaks

--- prototypes/app/calendar/js/bic_calendar.js
+++ prototypes/app/calendar/js/bic_calendar.js
@@ -1,13 +1,11 @@
 /*
+ *  Bic Calendar - v3.2.2
+ *  A simple twitter bootstrap calendar / agenda to mark events and select range of dates.
+ *  http://bichotll.github.io/bic_calendar
  *
- * bic calendar
- * Autor: bichotll
- * Web-autor: bic.cat
- * Web script: http://bichotll.github.io/bic_calendar/
- * Llicència Apache
- *
+ *  Made by bichotll
+ *  Under Apache License
  */
-
 $.fn.bic_calendar = function(options) {
 
     var opts = $.extend({}, $.fn.bic_calendar.defaults, options);
@@ -18,7 +16,6 @@ $.fn.bic_calendar = function(options) {
 
 
         /*** vars ***/
-
 
         //element called
         var elem = $(this);
@@ -31,6 +28,19 @@ $.fn.bic_calendar = function(options) {
         var calendarId = "bic_calendar";
 
         var events = opts.events;
+
+        //Date obj to calc the day
+        var objFecha;
+        if (opts.date) {
+          if (typeof opts.date == 'string') {
+            var arrayDate = opts.date.split('/');
+            objFecha = new Date(parseInt(arrayDate[2]), parseInt(arrayDate[1]) - 1, parseInt(arrayDate[0]));
+          } else {
+            objFecha = opts.date;
+          }
+        } else {
+          objFecha = new Date();
+        }
 
         var dayNames;
         if (typeof opts.dayNames != "undefined")
@@ -109,9 +119,6 @@ $.fn.bic_calendar = function(options) {
             daysMonthLayer = $('<table class="table">');
 
             listListeralsWeek();
-
-            //Date obj to calc the day
-            var objFecha = new Date();
 
             //current year n current month
             var month = objFecha.getMonth();
