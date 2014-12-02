4f03afb175c452b580afbab1435b2648e909d511
IagoSRL <iagosrl@gmail.com>
2014-12-02 18:45:44 +0100

    prototype #585: moved testing data for clarity.

---
+++ prototypes/app/js/testdata/calendarAppointments.js
@@ -0,0 +1,44 @@
+/** Calendar Appointments test data **/
+var testData = [
+    new Appointment({
+        startTime: new Date(2014, 11, 1, 10, 0, 0),
+        endTime: new Date(2014, 11, 1, 12, 0, 0),
+        pricingSummary: 'Deep Tissue Massage 120m plus 2 more',
+        ptotalPrice: 95.0,
+        locationSummary: '3150 18th Street San Francisco, CA',
+        notesToClient: 'Looking forward to seeing the new color',
+        notesToSelf: 'Ask him about his new color',
+        client: {
+            firstName: 'Joshua',
+            lastName: 'Danielson'
+        }
+    }),
+    new Appointment({
+        startTime: new Date(2014, 11, 1, 13, 0, 0),
+        endTime: new Date(2014, 11, 1, 13, 50, 0),
+        pricingSummary: 'Another Massage 50m',
+        ptotalPrice: 95.0,
+        locationSummary: '3150 18th Street San Francisco, CA',
+        notesToClient: 'Something else',
+        notesToSelf: 'Remember that thing',
+        client: {
+            firstName: 'Joshua',
+            lastName: 'Danielson'
+        }
+    }),
+    new Appointment({
+        startTime: new Date(2014, 11, 1, 16, 0, 0),
+        endTime: new Date(2014, 11, 1, 18, 0, 0),
+        pricingSummary: 'Tissue Massage 120m',
+        ptotalPrice: 95.0,
+        locationSummary: '3150 18th Street San Francisco, CA',
+        notesToClient: '',
+        notesToSelf: 'Ask him about the forgotten notes',
+        client: {
+            firstName: 'Joshua',
+            lastName: 'Danielson'
+        }
+    }),
+];
+
+exports.appointments = testData;
