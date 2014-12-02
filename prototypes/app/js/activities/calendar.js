e5fba667b5e38ae72cfdaf09dbd9e69c0d650900
IagoSRL <iagosrl@gmail.com>
2014-12-02 15:32:41 +0100

    Calendar prototype #585: connected data to specific dates with 'full free' for any other date, so swipe (and its problems with dynamic changes) can be tested completely on daily view.

--- prototypes/app/js/activities/calendar.js
+++ prototypes/app/js/activities/calendar.js
@@ -109,7 +109,74 @@ CalendarActivity.prototype.showDailyView = function showDailyView(date, firstRun
         this.__initedDailyView = true;
         
         // Data
-        var testData = [
+        var testData1 = [
+            new CalendarSlot({
+                startTime: new Date(2014, 11, 1, 0, 0, 0),
+                endTime: new Date(2014, 11, 1, 12, 0, 0),
+                
+                subject: 'Free',
+                description: null,
+                link: '#calendar/new',
+
+                actionIcon: 'glyphicon glyphicon-plus',
+                actionText: null,
+
+                classNames: 'ListView-item--tag-success'
+            }),
+            new CalendarSlot({
+                startTime: new Date(2014, 11, 1, 12, 0, 0),
+                endTime: new Date(2014, 11, 1, 13, 0, 0),
+                
+                subject: 'Josh Danielson',
+                description: 'Deep Tissue Massage',
+                link: '#calendar/appointment/3',
+
+                actionIcon: 'glyphicon glyphicon-plus',
+                actionText: null,
+
+                classNames: null
+            }),
+            new CalendarSlot({
+                startTime: new Date(2014, 11, 1, 13, 0, 0),
+                endTime: new Date(2014, 11, 1, 15, 0, 0),
+
+                subject: 'Do that important thing',
+                description: null,
+                link: '#calendar/event/8',
+
+                actionIcon: 'glyphicon glyphicon-new-window',
+                actionText: null,
+
+                classNames: null
+            }),
+            new CalendarSlot({
+                startTime: new Date(2014, 11, 1, 15, 0, 0),
+                endTime: new Date(2014, 11, 1, 16, 0, 0),
+                
+                subject: 'Iago Lorenzo',
+                description: 'Deep Tissue Massage Long Name',
+                link: '#calendar/appointment/5',
+
+                actionIcon: null,
+                actionText: '$159.90',
+
+                classNames: null
+            }),
+            new CalendarSlot({
+                startTime: new Date(2014, 11, 1, 16, 0, 0),
+                endTime: new Date(2014, 11, 2, 0, 0, 0),
+                
+                subject: 'Free',
+                description: null,
+                link: '#calendar/new',
+
+                actionIcon: 'glyphicon glyphicon-plus',
+                actionText: null,
+
+                classNames: 'ListView-item--tag-success'
+            })
+        ];
+        var testData2 = [
             new CalendarSlot({
                 startTime: new Date(2014, 11, 2, 0, 0, 0),
                 endTime: new Date(2014, 11, 2, 9, 0, 0),
@@ -241,13 +308,47 @@ CalendarActivity.prototype.showDailyView = function showDailyView(date, firstRun
                 classNames: null
             })
         ];
+        var testDataFree = [
+            new CalendarSlot({
+                startTime: new Date(2014, 0, 1, 0, 0, 0),
+                endTime: new Date(2014, 0, 2, 0, 0, 0),
+
+                subject: 'Free',
+                description: null,
+                link: '#calendar/new',
+
+                actionIcon: 'glyphicon glyphicon-plus',
+                actionText: null,
+
+                classNames: 'ListView-item--tag-success'
+            })
+        ];
+        
+        var testData = {
+            '2014-12-01': testData1,
+            '2014-12-02': testData2,
+            'default': testDataFree
+        };
         
         var dailyDataView = {
-            slots: ko.observableArray(testData),
+            slots: ko.observableArray(testData2),
             currentIndex: ko.observable(0)
         };
         
         ko.applyBindings(dailyDataView, this.$dailyView.get(0));
+        
+        this.$datepicker.on('changeDate', function(e) {
+            if (e.viewMode === 'days') {
+                var date = moment(e.date);
+                var sdate = date.format('YYYY-MM-DD');
+                
+                if (testData.hasOwnProperty(sdate)) {
+                    dailyDataView.slots(testData[sdate]);
+                } else {
+                    dailyDataView.slots(testData['default']);
+                }
+            }
+        }.bind(this));
     }
 };
 
