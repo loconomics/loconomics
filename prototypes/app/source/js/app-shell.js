c8ccf3ba1e3ebf1000b8af4b863df32f4460609a
IagoSRL <iagosrl@gmail.com>
2015-02-02 22:32:04 +0100

    app #575: better page not found error message

--- prototypes/app/source/js/app-shell.js
+++ prototypes/app/source/js/app-shell.js
@@ -45,10 +45,20 @@ var shell = new Shell({
 
 // Catch errors on item/page loading, showing..
 shell.on('error', function(err) {
-    var str = err ?
-        typeof(err) === 'string' ? err : JSON.stringify(err) :
-        'Unknow error'
-    ;
+    
+    var str = 'Unknow error';
+    if (err) {
+        if (typeof(err) === 'string') {
+            str = err;
+        }
+        else if (err.message) {
+            str = err.message;
+        }
+        else {
+            str = JSON.stringify(err);
+        }
+    }
+
     // TODO change with a dialog or something
     window.alert(str);
 });
