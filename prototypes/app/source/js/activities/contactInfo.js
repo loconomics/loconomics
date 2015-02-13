41121ff14877acbe162fd2f12a4f8634e99e3c73
IagoSRL <iagosrl@gmail.com>
2015-02-13 19:51:49 +0100

    App #575: adapted contactInfo and locationEdition to the ActivityTitle change and other Styleguide details

--- prototypes/app/source/js/activities/contactInfo.js
+++ prototypes/app/source/js/activities/contactInfo.js
@@ -4,6 +4,7 @@
 'use strict';
 
 var singleton = null,
+    ko = require('knockout'),
     NavBar = require('../viewmodels/NavBar'),
     NavAction = require('../viewmodels/NavAction');
 
@@ -21,16 +22,45 @@ function ContactInfoActivity($activity, app) {
     
     this.$activity = $activity;
     this.app = app;
-    
+    this.dataView = new ViewModel();
+    ko.applyBindings(this.dataView, $activity.get(0));
+
     this.navBar = new NavBar({
         title: '',
         leftAction: NavAction.goBack.model.clone({
-            text: 'Account'
+            text: 'Account',
+            isTitle: true
         }),
         rightAction: NavAction.goHelpIndex
     });
+    
+    app.model.user().onboardingStep.subscribe(function (step) {
+        
+        if (step) {
+            // TODO Set navbar step index
+            // Setting navbar for Onboarding/wizard mode
+            this.navBar.leftAction().text('');
+            // Setting header
+            this.dataView.headerText('How can we reach you?');
+            this.dataView.buttonText('Save and continue');
+        }
+        else {
+            // TODO Remove step index
+            // Setting navbar to default
+            this.navBar.leftAction().text('Account');
+            // Setting header to default
+            this.dataView.headerText('Contact information');
+            this.dataView.buttonText('Save');
+        }
+    }.bind(this));
 }
 
 ContactInfoActivity.prototype.show = function show(options) {
 
 };
+
+function ViewModel() {
+
+    this.headerText = ko.observable('Contact information');
+    this.buttonText = ko.observable('Save');
+}
