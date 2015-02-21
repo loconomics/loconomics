8925cc240e1fa8f62fd85dd7af2a4adb621930d9
IagoSRL <iagosrl@gmail.com>
2015-02-21 23:12:52 +0100

    App #575: fixed (with WORKAROUND) an app CRASH on Android when half-pressing links, being the 'tap' target assigned to a non-link and the link being followed by the WebView to non-existent page, making PhoneGap to crash.

--- prototypes/app/source/js/app.shell.js
+++ prototypes/app/source/js/app.shell.js
@@ -13,6 +13,8 @@ var shell = require('./utils/shell/index'),
     Shell = shell.Shell,
     DomItemsManager = shell.DomItemsManager;
 
+var iOS = /(iPad|iPhone|iPod)/g.test( navigator.userAgent );
+
 // Creating the shell:
 var shell = new Shell({
 
@@ -27,8 +29,17 @@ var shell = new Shell({
 
     indexName: 'index',
 
-    // for faster mobile experience (jquery-mobile event):
-    linkEvent: 'tap',
+    // WORKAROUND: Using the 'tap' event for faster mobile experience
+    // (from jquery-mobile event) on iOS devices, but left
+    // 'click' on others since they has not the slow-click problem
+    // thanks to the meta-viewport.
+    // WORKAROUND: IMPORTANT, using 'click' rather than 'tap' on Android
+    // prevents an app crash (or go out and page not found on Chrome for Android)
+    // because of some 'clicks' happening on
+    // a half-link-element tap, where the 'tap' event detects as target the non-link and the
+    // link gets executed anyway by the browser, not catched so Webview moves to 
+    // a non existant file (and thats make PhoneGap to crash).
+    linkEvent: iOS ? 'tap' : 'click',
 
     // No need for loader, everything comes bundled
     loader: null,
