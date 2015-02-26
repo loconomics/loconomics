46a69fffe57511e433ecf0ce955ec68a4234b465
IagoSRL <iagosrl@gmail.com>
2015-02-26 16:04:12 +0100

    Created REST API #581 to get JobTitle information. Fixed the http code for several validation errors on others API (must be 400, not 404)

---
+++ web/App_Code/LcData.JobTitle.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebMatrix.Data;
+
+/// <summary>
+/// Descripción breve de LcData
+/// </summary>
+public static partial class LcData
+{
+	public static class JobTitle
+    {
+        public static dynamic GetJobTitle(int jobTitleID)
+        {
+            using (var db = Database.Open("sqlloco"))
+            {
+                var job = db.QuerySingle(@"
+                    SELECT
+                        PositionID As jobTitleID,
+                        PositionSingular As singularName,
+                        PositionPlural As pluralName,
+                        Aliases As aliases,
+                        PositionDescription As description,
+                        PositionSearchDescription As searchDescription,
+                        CreatedDate As createdDate,
+                        UpdatedDate As updatedDate
+                    FROM
+                        positions
+                    WHERE
+                        PositionID = @0
+                         AND LanguageID = @1
+                         AND CountryID = @2
+                         AND Active = 1
+                ", jobTitleID, LcData.GetCurrentLanguageID(), LcData.GetCurrentCountryID());
+
+                if (job == null)
+                {
+                    return null;
+                }
+                else
+                {
+                    var pricings = db.Query(@"
+                        SELECT
+                            PricingTypeID As pricingTypeID,
+                            ClientTypeID As clientTypeID,
+                            CreatedDate As createdDate,
+                            UpdatedDate As updatedDate
+                        FROM
+                            positionpricingtype
+                        WHERE
+                            PositionID = @0
+                             AND LanguageID = @1
+                             AND CountryID = @2
+                             AND Active = 1
+                    ", jobTitleID, LcData.GetCurrentLanguageID(), LcData.GetCurrentCountryID());
+
+                    // Return an object that includes the collection of pricings
+                    return new {
+                        jobTitleID = job.jobTitleID,
+                        singularName = job.singularName,
+                        pluralName = job.pluralName,
+                        aliases = job.aliases,
+                        description = job.description,
+                        searchDescription = job.searchDescription,
+                        createdDate = job.createdDate,
+                        updatedDate = job.updatedDate,
+                        pricingTypes = pricings
+                    };
+                }
+            }
+        }
+    }
+}
