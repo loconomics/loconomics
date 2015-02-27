fcbd7295d4ecc313e59ecd464276a5c7063f6918
IagoSRL <iagosrl@gmail.com>
2015-02-27 11:00:27 +0100

    REST API #530 UserJobProfile: implemented job-title 'reactivation'.

--- web/App_Code/LcData.JobTitle.cs
+++ web/App_Code/LcData.JobTitle.cs
@@ -145,6 +145,13 @@ public static partial class LcData
             }
         }
 
+        /// <summary>
+        /// Deactivation consists in switch the status of the profile
+        /// to 'manually disabled / private'.
+        /// </summary>
+        /// <param name="userID"></param>
+        /// <param name="jobTitleID"></param>
+        /// <returns></returns>
         public static bool DeactivateUserJobTitle(int userID, int jobTitleID)
         {
             using (var db = Database.Open("sqlloco")) {
@@ -160,6 +167,68 @@ public static partial class LcData
 
                 // Task done? Almost a record must be affected to be a success
                 return affected > 1;
+            }
+        }
+
+        /// <summary>
+        /// Reactivation consists in switch the status of the profile
+        /// to the 'active' state but only if the profile fullfill the constraints
+        /// for 'active profiles', so the profile can get stucked
+        /// in the non-active state as a result, pending on requesting
+        /// a reactivation once its contraints are fullfilled.
+        /// </summary>
+        /// <param name="userID"></param>
+        /// <param name="jobTitleID"></param>
+        /// <returns></returns>
+        public static bool ReactivateUserJobTitle(int userID, int jobTitleID)
+        {
+            using (var db = Database.Open("sqlloco")) {
+                // Check the current StatusID
+                var statusID = LcData.UserInfo.GetUserPositionStatus(userID, jobTitleID);
+
+                // If status is -1, the records does not exists
+                if (statusID == -1)
+                {
+                    return false;
+                }
+
+                // If status is of type 3 'private profile, manual activation'..
+                if (statusID == 3) {
+                    // ..modify the profile StatusID from 3 to 2 'private profile, automatic activation'
+                    // (because the TestProfileActivation only works for StatusID:2 to avoid unexpected changes)
+                    db.Execute(@"
+                        UPDATE UserProfilePositions SET StatusID = 2
+                        WHERE UserID = @0 AND PositionID = @1
+                         AND LanguageID = @2
+                         AND CountryID = @3
+                    ",userID, jobTitleID,
+                     LcData.GetCurrentLanguageID(), LcData.GetCurrentCountryID());
+                }
+                
+                // It executes the profile activation that checks that all required alerts must be off to activate:
+                db.Execute("EXEC TestProfileActivation @0, @1", userID, jobTitleID);
+                
+                // Check the updated StatusID
+                var newStatusID = LcData.UserInfo.GetUserPositionStatus(userID, jobTitleID);
+                // If the status is 1 'public profile', success!
+                if (newStatusID == 1) {
+                    return true;
+                } else {
+                    // It is Not activated still, required alerts are pending, back to the original
+                    // StatusID if was not 2 originally (it was updated in the middle of the process to run
+                    // the TestProfileActivation procedure)
+                    if (statusID >= 2) {
+                        db.Execute(@"
+                            UPDATE UserProfilePositions SET StatusID = @2
+                            WHERE UserID = @0 AND PositionID = @1
+                             AND LanguageID = @3
+                             AND CountryID = @4
+                        ", userID, jobTitleID, statusID,
+                         LcData.GetCurrentLanguageID(), LcData.GetCurrentCountryID());
+                    }
+
+                    return false;
+                }
             }
         }
         #endregion
