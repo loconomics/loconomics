87856b9277f037c27e5432549630c5b903585883
IagoSRL <iagosrl@gmail.com>
2015-03-31 19:49:11 +0200

    App job titles #587: connected scheduling/job-titles to actual locations data, so the URL goes to the jobTitleID and the number of locations is real

--- prototypes/app/source/js/activities/jobtitles.js
+++ prototypes/app/source/js/activities/jobtitles.js
@@ -4,15 +4,35 @@
 'use strict';
 
 var Activity = require('../components/Activity'),
-    UserJobProfileViewModel = require('../viewmodels/UserJobProfile');
+    ko = require('knockout');
 
 var A = Activity.extends(function JobtitlesActivity() {
     
     Activity.apply(this, arguments);
     
     this.accessLevel = this.app.UserType.LoggedUser;
-    this.viewModel = new UserJobProfileViewModel(this.app);
+    this.viewModel = new ViewModel(this.app);
     this.navBar = Activity.createSubsectionNavBar('Scheduling');
+    
+    // On changing jobTitleID:
+    // - load addresses
+    this.registerHandler({
+        target: this.viewModel.jobTitleID,
+        handler: function(jobTitleID) {
+            if (jobTitleID) {
+                this.app.model.serviceAddresses.getList(jobTitleID)
+                .then(function(list) {
+
+                    list = this.app.model.serviceAddresses.asModel(list);
+                    this.viewModel.addresses(list);
+
+                }.bind(this));
+            }
+            else {
+                this.viewModel.addresses([]);
+            }
+        }.bind(this)
+    });
 });
 
 exports.init = A.init;
@@ -20,9 +40,41 @@ exports.init = A.init;
 A.prototype.show = function show(state) {
     Activity.prototype.show.call(this, state);
 
-    this.viewModel.sync();
-
+    var params = state && state.route && state.route.segments || {};
+    
     //// Set the job title
-    //var jobID = state.route.segments[0] |0;
-    //this.viewModel.jobTitleID(jobID);
+    var jobID = params[0] |0;
+    this.viewModel.jobTitleID(jobID);
 };
+
+function ViewModel(app) {
+    
+    this.jobTitleID = ko.observable(0);
+    
+    this.addresses = ko.observable([]);
+
+    // Computed since it can check several externa loadings
+    this.isLoading = ko.pureComputed(function() {
+
+        // TODO: On services support, add the services loading
+        return app.model.serviceAddresses.state.isLoading();
+        
+    }, this);
+    
+    this.addressesCount = ko.pureComputed(function() {
+        
+        // TODO l10n.
+        // Use i18next plural localization support rather than this manual.
+        var count = this.addresses().length,
+            one = '1 location',
+            more = ' locations';
+        
+        if (count === 1)
+            return one;
+        else
+            // Small numbers, no need for formatting
+            return count + more;
+
+    }, this);
+    
+}
