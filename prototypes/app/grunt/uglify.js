9e26786b142e3b65b80155126f04a8411df61ddc
IagoSRL <iagosrl@gmail.com>
2015-04-03 15:09:49 +0200

    App uglify #709: found the truly cause of the compression bug and the set-up to avoid it, getting maximum compression (but low, since the biggest improvement from advanced options is 'mangle' and cannot enabled); but almost is found, understood and documented. Enabled warnings and gzip compression notice, to detect other possible problems.

--- prototypes/app/grunt/uglify.js
+++ prototypes/app/grunt/uglify.js
@@ -2,7 +2,7 @@
 
 module.exports = {
     'libs': {
-        // Not sure if #709 affects this too, with some testing seems to not affect so left enabled but documented:
+        // Disabled conservative settings, enable if some bugs happens using minified files.
         //'options': { compress: false, mangle: false },
         'files': {
             './build/assets/js/libs.min.js': ['<%= browserify.libs.dest %>']
@@ -10,49 +10,47 @@ module.exports = {
     },
     'app': {
         'options': {
-            // Some recommended settings from http://discuss.emberjs.com/t/uglify-breaks-ember/5557
-            // BUT did not fix the problem with advanced options, so compress is disabled at all.
-            // See #709 for investigation about that.
-            compress: false,
-            /*compress: {
-                global_defs: {
-                    DEBUG: false,
-                    RELEASE: false
-                },
-                sequences: true,
-                properties: true,
-                drop_debugger: true,
-                unsafe: false,
-                conditionals: true,
-                comparisons: true,
-                evaluate: true,
-                booleans: true,
-                dead_code: true,
-                loops: true,
-                unused: true,
-                hoist_funs: false,
-                hoist_vars: false,
-                if_return: true,
-                join_vars: true,
-                cascade: true,
-                warnings: false,
-                negate_iife: false,
-                pure_getters: false,
-                pure_funcs: null,
-                drop_console: true
+            // Custom optimizations to avoid bugs:
+            // - #709
+            //compress: false,
+            compress: {
+                sequences     : true,  // join consecutive statemets with the “comma operator”
+                properties    : true,  // optimize property access: a["foo"] → a.foo
+                dead_code     : true,  // discard unreachable code
+                drop_debugger : true,  // discard “debugger” statements
+                unsafe        : false, // some unsafe optimizations (see below)
+                conditionals  : true,  // optimize if-s and conditional expressions
+                comparisons   : true,  // optimize comparisons
+                evaluate      : true,  // evaluate constant expressions
+                booleans      : true,  // optimize boolean expressions
+                loops         : true,  // optimize loops
+                // Iago: Additionally to drop unused vars/funcs, it drops the functions names too
+                // causing the bug #709 (we use Function.name).
+                // I had set-up jshint to notify about actual unused bars/funcs so
+                // compression stage is not reach if that happens.
+                unused        : false, // drop unused variables/functions
+                hoist_funs    : false, // hoist function declarations
+                hoist_vars    : false, // hoist variable declarations
+                if_return     : true,  // optimize if-s followed by return/continue
+                join_vars     : true,  // join var declarations
+                cascade       : true,  // try to cascade `right` into `left` in sequences
+                side_effects  : true,  // drop side-effect-free statements
+                warnings      : true,  // warn about potentially dangerous optimizations/code
+                global_defs   : {}     // global definitions
             },
-            preserveComments: false,
             report: 'gzip',
-            screw_ie8: true,*/
-            
+
             // To look for compression warnings only:
             /*compress: {
-                warnings: false
+                warnings: true
             },*/
             
             // Reduce variable names
-            // IMPORTANT: Disabled because it cause the bug #709 to re-appear on some pages/activities,
-            // like scheduling/job-titles/addresses
+            // IMPORTANT: Disabled because it causes the bug #709, the problem is that
+            // it changes the functions names too and we are using them, for example when
+            // managing Activities singletons.
+            // See: http://lisperator.net/uglifyjs/mangle (third point, unavoidable)
+            // If in the future becomes an option to avoid mangle function names, can be used.
             mangle: false
         },
         'files': {
@@ -60,7 +58,7 @@ module.exports = {
         }
     },
     'styleguidelibs': {
-        // Not sure if #709 affects this too, with some testing seems to not affect so left enabled but documented:
+        // Disabled conservative settings, enable if some bugs happens using minified files.
         //'options': { compress: false, mangle: false },
         'files': {
             './build/assets/js/styleguidelibs.min.js': ['<%= browserify.styleguidelibs.dest %>']
