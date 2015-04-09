e23238c97e8656614ac787a9dec20edd7996295e
IagoSRL <iagosrl@gmail.com>
2015-04-09 14:02:16 +0200

    App #575: removed unused incomplete pricingTypes API access from appModel.jobTitles

--- prototypes/app/source/js/viewmodels/AppModel.jobTitles.js
+++ prototypes/app/source/js/viewmodels/AppModel.jobTitles.js
@@ -3,7 +3,6 @@
 'use strict';
 
 var localforage = require('localforage'),
-    PricingType = require('../models/PricingType'),
     JobTitle = require('../models/JobTitle'),
     ko = require('knockout');
 
@@ -15,71 +14,8 @@ exports.create = function create(appModel) {
             }
         },
         cache = {
-            jobTitles: {},
-            pricingTypes: null
+            jobTitles: {}
         };
-    
-    /**
-        Convert raw array of pricing types records into
-        an indexed array of models, actually an object
-        with ID numbers as properties,
-        and cache it in memory.
-    **/
-    function mapToPricingTypes(rawItems) {
-        cache.pricingTypes = {};
-        
-        if (rawItems) {
-            rawItems.forEach(function(rawItem) {
-                cache.pricingTypes[rawItem.pricingTypeID] = new PricingType(rawItem);
-            });
-        }
-
-        return cache.pricingTypes;
-    }
-
-    /**
-        Public API
-        Returns a promise to fetch pricing types information
-    **/
-    api.getPricingTypes = function getPricingTypes() {
-        // First, in-memory cache
-        if (cache.pricingTypes) {
-            return Promise.resolve(cache.pricingTypes);
-        }
-        else {
-            // Second, local storage
-            return localforage.getItem('pricingTypes')
-            .then(function(pricingTypes) {
-                if (pricingTypes) {
-                    return mapToPricingTypes(pricingTypes);
-                }
-                else {
-                    // Third and last, remote loading
-                    return appModel.rest.get('pricing-types')
-                    .then(function (raw) {
-                        // Cache in local storage
-                        localforage.setItem('pricingTypes', raw);
-                        return mapToPricingTypes(raw);
-                    });
-                }
-            });
-        }
-    };
-
-    /**
-        Public API
-        Returns a promise to fetch a pricing type by ID
-    **/
-    api.getPricingType = function getPricingType(id) {
-        // The REST API allows to fetch a single pricing type by ID,
-        // if we have that not cached. But since load all is quick (they are a few
-        // and will stay being a short list), we can ask for all and get from that.
-        // So is enough reusing the general 'get all' API and more simple code.
-        // NOTE: The single item API will still be useful for future sync updates.
-        return api.getPricingTypes().then(function(allByID) {
-            return allByID[id] || null;
-        });
-    };
 
     /**
         Public API
