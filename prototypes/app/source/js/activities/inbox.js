f7fb16091e2b7f7913636c8a16847b4e8ace3421
IagoSRL <iagosrl@gmail.com>
2015-04-09 21:20:45 +0200

    App #575: implemented load of threads in Inbox Activity.

--- prototypes/app/source/js/activities/inbox.js
+++ prototypes/app/source/js/activities/inbox.js
@@ -5,37 +5,57 @@
 
 var Activity = require('../components/Activity');
 var ko = require('knockout');
+var MessageView = require('../models/MessageView');
 
 var A = Activity.extends(function InboxActivity() {
     
     Activity.apply(this, arguments);
     
-    this.viewModel = new ViewModel();
+    this.viewModel = new ViewModel(this.app);
     this.accessLevel = this.app.UserType.LoggedUser;
     
     this.navBar = Activity.createSectionNavBar('Inbox');
-    
-    //this.$inbox = $activity.find('#inboxList');
-    
-    // TestingData
-    setSomeTestingData(this.viewModel);
 });
 
 exports.init = A.init;
 
-var MailFolder = require('../models/MailFolder');
+A.prototype.show = function show(options) {
+    Activity.prototype.show.call(this, options);
+    
+    // Messages
+    this.app.model.messaging.getList()
+    .then(function(threads) {
+        this.viewModel.sourceThreads(threads());
+    }.bind(this))
+    .catch(function(err) {
+        this.app.modals.showError({
+            title: 'Error loading messages',
+            error: err
+        });
+    }.bind(this));
+};
 
-function ViewModel() {
+function ViewModel(app) {
+    
+    this.isLoading = app.model.messaging.state.isLoading;
+    this.isSyncing = app.model.messaging.state.isSyncing;
 
-    this.inbox = new MailFolder({
-        topNumber: 20
-    });
+    this.sourceThreads = ko.observableArray([]);
     
     this.searchText = ko.observable('');
-}
-
-/** TESTING DATA **/
-function setSomeTestingData(dataView) {
     
-    dataView.inbox.messages(require('../testdata/messages').messages);
+    this.threads = ko.pureComputed(function() {
+        var t = this.sourceThreads(),
+            s = this.searchText();
+
+        if (!t)
+            return [];
+        else if (!s)
+            return t.map(MessageView.fromThread);
+        else        
+            return t.filter(function(/*thread*/) {
+                // TODO search filtering
+                return true;
+            }).map(MessageView.fromThread);
+    }, this);
 }
