96c3ce4e0169a586df888feb12f2310be411f666
IagoSRL <iagosrl@gmail.com>
2015-04-11 19:29:55 +0200

    Clients API #646: scripts for database changes: added ReferredByUserID in [users], added new [accountstatus] '6: Freelancers client' for not enabled user accounts created by a freelancer (the referenced by ReferredByUserID). Added the DbSync sql template for [accountstatus] Initial file for REST API, empty

---
+++ web/_DBUpdate/_template-INSERT-accountstatus.sql
@@ -0,0 +1,16 @@
+INSERT INTO [accountstatus]
+   ([AccountStatusID]
+   ,[AccountStatusName]
+   ,[AccountStatusDescription]
+   ,[CreatedDate]
+   ,[UpdatedDate]
+   ,[ModifiedBy]
+   ,[Active])
+VALUES
+   (@AccountStatusID
+   ,@AccountStatusName>
+   ,@AccountStatusDescription>
+   ,@CreatedDate>
+   ,@UpdatedDate>
+   ,@ModifiedBy>
+   ,@Active)
