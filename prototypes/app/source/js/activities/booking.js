39f43c918581a54ea8e063acb5647e1d217369d5
IagoSRL <iagosrl@gmail.com>
2015-08-03 14:48:18 +0200

    App #575 Latest UI details added to Client Booking, pricing summary, recurring

--- prototypes/app/source/js/activities/booking.js
+++ prototypes/app/source/js/activities/booking.js
@@ -15,6 +15,8 @@ var A = Activity.extends(function BookingActivity() {
     this.accessLevel = this.app.UserType.LoggedUser;
     this.viewModel = new ViewModel(this.app);
     this.navBar = Activity.createSectionNavBar('Booking');
+    
+    setTestingData(this.viewModel);
 });
 
 exports.init = A.init;
@@ -62,4 +64,73 @@ function ViewModel(app) {
     
     this.supportGratuity = ko.observable(false);
     this.gratuity = ko.observable(0);
+    
+    this.summary = new PricingSummary();
+    
+    this.makeRepeatBooking = ko.observable(false);
+}
+
+var Model = require('../Models/Model');
+var numeral = require('numeral');
+
+function PricingSummary(values) {
+
+    Model(this);
+
+    this.model.defProperties({
+        items: {
+            isArray: true,
+            Model: PricingSummaryItem
+        },
+        feesPercentage: 10
+    }, values);
+
+    this.subtotalPrice = ko.pureComputed(function() {
+        return this.items().reduce(function(total, item) {
+            total += item.price();
+            return total;
+        }, 0);
+    }, this);
+    
+    this.fees = ko.pureComputed(function() {
+        var t = this.subtotalPrice(),
+            f = this.feesPercentage();
+        return t * (f / 100);
+    }, this);
+
+    this.totalPrice = ko.pureComputed(function() {
+        return this.subtotalPrice() + this.fees();
+    }, this);
+    
+    this.feesMessage = ko.pureComputed(function() {
+        var f = numeral(this.fees()).format('$#,##0.00');
+        return '*includes a __fees__ first-time booking fee'.replace(/__fees__/g, f);
+    }, this);
+}
+
+function PricingSummaryItem(values) {
+    
+    Model(this);
+
+    this.model.defProperties({
+        concept: '',
+        price: 0
+    }, values);
+}
+
+function setTestingData(vw) {
+    vw.summary.items([
+        new PricingSummaryItem({
+            concept: 'Deep Tissue Massage',
+            price: 99
+        }),
+        new PricingSummaryItem({
+            concept: 'Special oils',
+            price: 15
+        }),
+        new PricingSummaryItem({
+            concept: 'Gratuity (20%)',
+            price: 22.8
+        })
+    ]);
 }
