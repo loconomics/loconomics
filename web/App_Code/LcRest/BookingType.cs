1ecd5867fd48e0b3034e16447dd0a230b61e9e16
IagoSRL <iagosrl@gmail.com>
2015-09-11 19:11:00 +0200

    Created BookingType LcRest class to fetch type data from database, neededt to know the fees to be applied on a booking. Class with public fields, because can be exposed through REST API for client side calculation of fees. The description and other internal only fields are not exposed, but the (revised) name of types is exposed (currently is thought to work as a code enumeration).

---
+++ web/App_Code/LcRest/BookingType.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de BookingType
+/// </summary>
+public class BookingType
+{
+    #region Fields
+    public int bookingTypeID;
+    public string name;
+    public decimal firstTimeServiceFeeFixed;
+    public decimal firstTimeServiceFeePercentage;
+    public decimal paymentProcessingFeePercentage;
+    public decimal paymentProcessingFeeFixed;
+    public decimal firstTimeServiceFeeMaximum;
+    public decimal firstTimeServiceFeeMinimum;
+    public DateTime updatedDate;
+    #endregion
+
+    #region Instances
+    public BookingType() { }
+
+    public static BookingType FromDB(dynamic record)
+    {
+        if (record == null) return null;
+
+        return new BookingType
+        {
+            bookingTypeID = record.bookingTypeID,
+            name = record.name,
+            firstTimeServiceFeeFixed = record.firstTimeServiceFeeFixed,
+            firstTimeServiceFeePercentage = record.firstTimeServiceFeePercentage,
+            paymentProcessingFeeFixed = record.paymentProcessingFeeFixed,
+            paymentProcessingFeePercentage = record.paymentProcessingFeePercentage,
+            firstTimeServiceFeeMaximum = record.firstTimeServiceFeeMaximum,
+            firstTimeServiceFeeMinimum = record.firstTimeServiceFeeMinimum,
+            updatedDate = record.updatedDate
+        };
+    }
+    #endregion
+
+    #region Fetch
+    const string sqlGetItem = @"
+        SELECT
+            bookingTypeID,
+            bookingTypeName As name,
+            firstTimeServiceFeeFixed,
+            firstTimeServiceFeePercentage,
+            paymentProcessingFeeFixed,
+            paymentProcessingFeePorcentage,
+            firstTimeServiceFeeMaximum,
+            firstTimeServiceFeeMinimum,
+            updatedDate
+        FROM
+            BookingType
+        WHERE
+            BookingTypeID = @0
+            AND Active = 1
+    ";
+    public static BookingType Get(int id)
+    {
+        using (var db = new LcDatabase())
+        {
+            return FromDB(db.QuerySingle(sqlGetItem, id));
+        }
+    }
+    #endregion
+}
