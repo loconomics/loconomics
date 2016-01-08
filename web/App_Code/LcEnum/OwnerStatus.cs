425168b7649427b2311c02b6043deff01e9725e1
IagoSRL <iagosrl@gmail.com>
2016-01-08 19:33:27 +0100

    Partial work on Owners REST API #847. Need rework based on Meeting with Josh

---
+++ web/App_Code/LcEnum/OwnerStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LcEnum
+{
+    public enum OwnerStatus : short
+    {
+        inTrial = 1,
+        active = 2,
+        inactive = 3,
+        inDefault = 4,
+        cancelled = 5,
+        suspended = 6
+    }
+}
--- web/_DBUpdate/Release-7.1 - B-4 OwnerStatus.dbtable.sql
+++ web/_DBUpdate/Release-7.1 - B-4 OwnerStatus.dbtable.sql
@@ -2,6 +2,126 @@
 /*__NOT_IN_BATCH__*/EXEC sp_msforeachtable 'ALTER TABLE ? NOCHECK CONSTRAINT all'
 /*__NOT_IN_BATCH__*/EXEC sp_msforeachtable 'ALTER TABLE ? DISABLE TRIGGER  all'
 DELETE FROM OwnerStatus 
+INSERT INTO [OwnerStatus]
+           ([OwnserStatusID]
+           ,[LanguageID]
+           ,[CountryID]
+           ,[OwnerStatusName]
+           ,[OwnerStatusDescription]
+           ,[CreatedDate]
+           ,[UpdatedDate]
+           ,[Active]
+           ,[UpdatedBy])
+     VALUES
+           ('1'
+           ,'1'
+           ,'1'
+           ,'In trial'
+           ,'Owner has completed two bookings and activated their marketplace profile but is still in trial so has not paid their first payment.'
+           ,'12/1/2015 12:00:00 AM'
+           ,'12/1/2015 12:00:00 AM'
+           ,'True'
+           ,'jd')
+INSERT INTO [OwnerStatus]
+           ([OwnserStatusID]
+           ,[LanguageID]
+           ,[CountryID]
+           ,[OwnerStatusName]
+           ,[OwnerStatusDescription]
+           ,[CreatedDate]
+           ,[UpdatedDate]
+           ,[Active]
+           ,[UpdatedBy])
+     VALUES
+           ('2'
+           ,'1'
+           ,'1'
+           ,'Active'
+           ,'Owner has completed two bookings, marketplace profile active, and payment being collected'
+           ,'12/1/2015 12:00:00 AM'
+           ,'12/1/2015 12:00:00 AM'
+           ,'True'
+           ,'jd')
+INSERT INTO [OwnerStatus]
+           ([OwnserStatusID]
+           ,[LanguageID]
+           ,[CountryID]
+           ,[OwnerStatusName]
+           ,[OwnerStatusDescription]
+           ,[CreatedDate]
+           ,[UpdatedDate]
+           ,[Active]
+           ,[UpdatedBy])
+     VALUES
+           ('3'
+           ,'1'
+           ,'1'
+           ,'Inactive'
+           ,'Owner has at one time completed all required steps to becoming an owner but has turned off their marketplace profile'
+           ,'12/1/2015 12:00:00 AM'
+           ,'12/1/2015 12:00:00 AM'
+           ,'True'
+           ,'jd')
+INSERT INTO [OwnerStatus]
+           ([OwnserStatusID]
+           ,[LanguageID]
+           ,[CountryID]
+           ,[OwnerStatusName]
+           ,[OwnerStatusDescription]
+           ,[CreatedDate]
+           ,[UpdatedDate]
+           ,[Active]
+           ,[UpdatedBy])
+     VALUES
+           ('4'
+           ,'1'
+           ,'1'
+           ,'In default'
+           ,'Owner is past due on their payment.'
+           ,'12/1/2015 12:00:00 AM'
+           ,'12/1/2015 12:00:00 AM'
+           ,'True'
+           ,'jd')
+INSERT INTO [OwnerStatus]
+           ([OwnserStatusID]
+           ,[LanguageID]
+           ,[CountryID]
+           ,[OwnerStatusName]
+           ,[OwnerStatusDescription]
+           ,[CreatedDate]
+           ,[UpdatedDate]
+           ,[Active]
+           ,[UpdatedBy])
+     VALUES
+           ('5'
+           ,'1'
+           ,'1'
+           ,'Cancelled'
+           ,'Owner has cancelled their account'
+           ,'12/1/2015 12:00:00 AM'
+           ,'12/1/2015 12:00:00 AM'
+           ,'True'
+           ,'jd')
+INSERT INTO [OwnerStatus]
+           ([OwnserStatusID]
+           ,[LanguageID]
+           ,[CountryID]
+           ,[OwnerStatusName]
+           ,[OwnerStatusDescription]
+           ,[CreatedDate]
+           ,[UpdatedDate]
+           ,[Active]
+           ,[UpdatedBy])
+     VALUES
+           ('6'
+           ,'1'
+           ,'1'
+           ,'Suspended'
+           ,'Owner has been suspended'
+           ,'12/1/2015 12:00:00 AM'
+           ,'12/1/2015 12:00:00 AM'
+           ,'True'
+           ,'jd')
 
 /*__NOT_IN_BATCH__*/EXEC sp_msforeachtable 'ALTER TABLE ? WITH CHECK CHECK CONSTRAINT all'
 /*__NOT_IN_BATCH__*/EXEC sp_msforeachtable 'ALTER TABLE ? ENABLE TRIGGER  all'
