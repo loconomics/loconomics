0c5d04dc41c01faf6edfc9bdd350cffb8a83c222
IagoSRL <iagosrl@gmail.com>
2016-01-27 21:43:01 +0100

    Added required viewModel to ownerInfo to avoid bug and new verison

--- app/source/js/activities/ownerInfo.js
+++ app/source/js/activities/ownerInfo.js
@@ -4,11 +4,13 @@
 'use strict';
 
 var Activity = require('../components/Activity');
+var ko = require('knockout');
 
 var A = Activity.extend(function OwnerInfoActivity() {
     
     Activity.apply(this, arguments);
 
+    this.viewModel = new ViewModel();
     this.accessLevel = this.app.UserType.serviceProfessional;
     this.navBar = Activity.createSubsectionNavBar('Account', {
         backLink: 'account'
@@ -16,3 +18,11 @@ var A = Activity.extend(function OwnerInfoActivity() {
 });
 
 exports.init = A.init;
+
+function ViewModel() {
+    this.ownerStatus = ko.observable('pending');
+    this.ownerStatusName = ko.observable('Pending');
+    this.ownerStatusOK = ko.pureComputed(function() {
+        return this.ownerStatus() === 'active';
+    }, this);
+}
