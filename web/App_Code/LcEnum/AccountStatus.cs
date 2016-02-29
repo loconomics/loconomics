08eb1f0cbd5be26d6fa44ee2f1a15137dd9d6a80
IagoSRL <iagosrl@gmail.com>
2016-02-29 18:30:40 +0100

    Implemented generic locale routing for special account URL rewriting, with generic utilitye. Implemented account/confirm page, with updated confirmation logic #900

---
+++ web/App_Code/LcEnum/AccountStatus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LcEnum
+{
+    public enum AccountStatus : short
+    {
+        cancelled = 0,
+        active = 1,
+        inactive = 2,
+        suspended = 3,
+        unauthorized = 4,
+        revoked = 5,
+        serviceProfessionalClient = 6
+    }
+}
