ed4604c86a164f5c1f164ed679cbaa5a5acd3998
IagoSRL <iagosrl@gmail.com>
2016-04-04 15:05:18 +0200

    Fixing problem with (info.booking.paymentLastFourCardNumberDigits) not showing up in templates #771

--- web/App_Code/LcEmailTemplate.cs
+++ web/App_Code/LcEmailTemplate.cs
@@ -310,8 +310,11 @@ public static class LcEmailTemplate
     public static BookingEmailInfo GetBookingInfo(int bookingID)
     {
         var bID = bookingID;
-
-        var b = LcRest.Booking.Get(bID, true);
+        // We need the booking including some internal sensitive data (like 'payment last four digits')
+        // since here we have not enough context to know what userID is the target, and must not be a problem
+        // since this data is never provided as is, but only used by the templates that must be carefull with what
+        // data to show to every case.
+        var b = LcRest.Booking.Get(bID, true, true);
         if (b == null) throw new Exception("BookingID not found #" + bID + ", at Email template");
 
         /* Generics not used in new email template organization, but keep this commented
