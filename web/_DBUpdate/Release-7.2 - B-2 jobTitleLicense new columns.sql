aa32f1bbb1024b6ab7bcea0eeb7a5b4162300157
IagoSRL <iagosrl@gmail.com>
2016-04-28 17:35:06 +0200

    Last fixes to Release-7.1 and 7.2 SQL upgrade scripts #939

--- web/_DBUpdate/Release-7.2 - B-2 jobTitleLicense new columns.sql
+++ web/_DBUpdate/Release-7.2 - B-2 jobTitleLicense new columns.sql
@@ -6,6 +6,6 @@ ALTER TABLE jobTitleLicense ADD MunicipalityID int DEFAULT 0 NOT NULL
 
 ALTER TABLE jobTitleLicense ADD CountyID int DEFAULT 0 NOT NULL
 	
-ALTER TABLE jobTitleLicense ADD PRIMARY KEY (PositionID, LicenseCertificationID, StateProvinceID, CountryID, MunicipalityID, CountyID)
+ALTER TABLE jobTitleLicense ADD CONSTRAINT PK__jobTitle__5E077F7A5FC911C6 PRIMARY KEY (PositionID, LicenseCertificationID, StateProvinceID, CountryID, MunicipalityID, CountyID)
 
 COMMIT TRANSACTION
