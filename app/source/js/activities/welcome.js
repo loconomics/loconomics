c583346c30bb057cd646bcde01ea8077ab427ad1
IagoSRL <iagosrl@gmail.com>
2016-06-13 13:19:20 +0200

    Fixed the onboarding for clients so after clicking 'take the tour' at welcome this onboarding stops for next times #978

--- app/source/js/activities/welcome.js
+++ app/source/js/activities/welcome.js
@@ -17,6 +17,16 @@ var A = Activity.extend(function WelcomeActivity() {
         startOnboarding: function startOnboarding() {
             app.model.onboarding.goNext();
         },
+        clientOnboarding: function clientOnboarding() {
+            // IMPORTANT: right now, there is not an onboarding for client, and the onboarding appmodel
+            // takes care of steps only for professionals, so clients has a single onboarding step, this
+            // 'welcome' activity. Because of that, once the client choose to 'start' here we just
+            // remove the onboarding step to prevent show them this welcome again every time it enters again.
+            app.model.onboarding.stepNumber(-1);
+            app.model.userProfile.saveOnboardingStep(null);
+            // We left the link behind to go wherever is linking (to add here an app.model.shell.go('/')
+            // can conflict)
+        },
         userProfile: app.model.userProfile.data,
         isServiceProfessional: app.model.userProfile.data.isServiceProfessional
     };
@@ -26,6 +36,8 @@ var A = Activity.extend(function WelcomeActivity() {
         leftAction: Activity.NavAction.goLogout,
         rightAction: null
     });
+    
+    
 });
 
 exports.init = A.init;
