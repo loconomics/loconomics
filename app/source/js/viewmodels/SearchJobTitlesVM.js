e3ed723d03ccdcdfa10524faa021c8a7332c4c89
IagoSRL <iagosrl@gmail.com>
2016-10-04 17:20:36 +0200

    SearchJobTitles #952: implemented the request-abort technique from previous addJobTitles code so performance can be better

--- app/source/js/viewmodels/SearchJobTitlesVM.js
+++ app/source/js/viewmodels/SearchJobTitlesVM.js
@@ -25,16 +25,18 @@ module.exports = function SearchJobTitlesVM(app) {
     //create an object named SearchResults to hold the search results returned from the API
     this.searchResults = new SearchResults();
 
+    var latestRequest = null;
     this.loadData = function(searchTerm, lat, lng) {
+        if (latestRequest) latestRequest.xhr.abort();
         this.isLoading(true);
         //Call the get rest API method for api/v1/en-US/search
-        return app.model.rest.get('search', {
+        latestRequest = app.model.rest.get('search', {
             searchTerm: searchTerm, 
             origLat: lat || DEFAULT_LOCATION.lat,
             origLong: lng || DEFAULT_LOCATION.lng,
             searchDistance: DEFAULT_LOCATION.searchDistance
-        })
-        .then(function(searchResults) {
+        });
+        latestRequest.then(function(searchResults) {
             if(searchResults){
                 //update searchResults object with all the data from the API
                 this.searchResults.model.updateWith(searchResults, true);
@@ -44,9 +46,11 @@ module.exports = function SearchJobTitlesVM(app) {
             }
             this.isLoading(false);
         }.bind(this))
-        .catch(function(/*err*/) {
+        .catch(function(err) {
             this.isLoading(false);
+            if (err && err.statusText === 'abort') return null;
         }.bind(this));
+        return latestRequest;
     };
     //creates a handler function for the html search button (event)
     this.search = function() {
@@ -55,15 +59,15 @@ module.exports = function SearchJobTitlesVM(app) {
         if(s && s.length > 2) {
             this.loadData(s, this.lat(), this.lng());
         }
-        else{
+        else {
             this.searchResults.model.reset();
         }
     };
     //anything that happens in the computed function after a timeout of 60 seconds, run the code
-    ko.computed(function(){
+    ko.computed(function() {
         this.search();
     //add ",this" for ko.computed functions to give context, when the search term changes, only run this function every 60 milliseconds
-    },this).extend({ rateLimit: { method: 'notifyAtFixedRate', timeout: 1000 } });
+    },this).extend({ rateLimit: { method: 'notifyAtFixedRate', timeout: 400 } });
     
     this.isInput = ko.pureComputed(function() {
         var s = this.searchTerm();
