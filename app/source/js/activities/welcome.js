c9f5795520eb2791cd874c8482a9bc00034454a8
Joshua Danielson <joshua.danielson@loconomics.com>
2016-10-04 12:49:12 -0700

    Added HelpLink to welcome
    #1030
    - Revised format as well
    - Added welcome as a voc element

--- app/source/js/activities/welcome.js
+++ app/source/js/activities/welcome.js
@@ -11,25 +11,7 @@ var A = Activity.extend(function WelcomeActivity() {
 
     this.accessLevel = this.app.UserType.loggedUser;
     
-    var app = this.app;
-    
-    this.viewModel = {
-        startOnboarding: function startOnboarding() {
-            app.model.onboarding.goNext();
-        },
-        clientOnboarding: function clientOnboarding() {
-            // IMPORTANT: right now, there is not an onboarding for client, and the onboarding appmodel
-            // takes care of steps only for professionals, so clients has a single onboarding step, this
-            // 'welcome' activity. Because of that, once the client choose to 'start' here we just
-            // remove the onboarding step to prevent show them this welcome again every time it enters again.
-            app.model.onboarding.stepNumber(-1);
-            app.model.userProfile.saveOnboardingStep(null);
-            // We left the link behind to go wherever is linking (to add here an app.model.shell.go('/')
-            // can conflict)
-        },
-        userProfile: app.model.userProfile.data,
-        isServiceProfessional: app.model.userProfile.data.isServiceProfessional
-    };
+    this.viewModel = new ViewModel(this.app);        
     
     var serviceProfessionalNavBar = Activity.createSubsectionNavBar('Get started', {
         leftAction: Activity.NavAction.goLogout, helpLink: this.viewModel.helpLinkProfessionals
@@ -40,26 +22,34 @@ var A = Activity.extend(function WelcomeActivity() {
     });
     this.clientNavBar = serviceProfessionalNavBar.model.toPlainObject(true);
     this.navBar = this.viewModel.user.isServiceProfessional() ? serviceProfessionalNavBar : clientNavBar;
-    
-//    this.navBar = new Activity.NavBar({
-//        title: null,
-//        leftAction: Activity.NavAction.goLogout,
-//        rightAction: null
-//    });
-//    
-    
 });
 
 exports.init = A.init;
 
-//var ko = require('knockout');
-//
-////function ViewModel(app) {
-////    
-////    this.user = app.model.userProfile.data;
-////    this.helpLinkProfessionals = '/help/relatedArticles/201211855-getting-started';
-////    this.helpLinkClients = '/help/relatedArticles/201313875-getting-started';
-////    this.helpLink = ko.pureComputed(function() {
-////        return this.user.isServiceProfessional() ? this.helpLinkProfessionals : this.helpLinkClients ;
-////    }, this);
-////}
+var ko = require('knockout');
+
+function ViewModel(app) {
+    
+    this.isInOnboarding = app.model.onboarding.inProgress;
+    this.user = app.model.userProfile.data;
+    this.helpLinkProfessionals = '/help/relatedArticles/201211855-getting-started';
+    this.helpLinkClients = '/help/relatedArticles/201313875-getting-started';
+    this.helpLink = ko.pureComputed(function() {
+        return this.user.isServiceProfessional() ? this.helpLinkProfessionals : this.helpLinkClients ;
+    }, this);
+    this.startOnboarding = function startOnboarding() {
+        app.model.onboarding.goNext();
+    };
+    this.clientOnboarding = function clientOnboarding() {
+        // IMPORTANT: right now, there is not an onboarding for client, and the onboarding appmodel
+        // takes care of steps only for professionals, so clients has a single onboarding step, this
+        // 'welcome' activity. Because of that, once the client choose to 'start' here we just
+        // remove the onboarding step to prevent show them this welcome again every time it enters again.
+        app.model.onboarding.stepNumber(-1);
+        app.model.userProfile.saveOnboardingStep(null);
+        // We left the link behind to go wherever is linking (to add here an app.model.shell.go('/')
+        // can conflict)
+    };
+    this.userProfile = app.model.userProfile.data;
+    this.isServiceProfessional = app.model.userProfile.data.isServiceProfessional;
+}
