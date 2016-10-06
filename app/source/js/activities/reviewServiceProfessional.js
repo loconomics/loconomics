003abcc1557966f2c6e7aff45b7722458b0e7fba
Joshua Danielson <joshua.danielson@loconomics.com>
2016-10-06 15:08:32 -0700

    Mockups start
    #874

--- app/source/js/activities/reviewServiceProfessional.js
+++ app/source/js/activities/reviewServiceProfessional.js
@@ -4,6 +4,9 @@
 'use strict';
 
 var Activity = require('../components/Activity');
+
+var $ = require('jquery');
+
 var A = Activity.extend(function reviewServiceProfessionalActivity() {
 
     Activity.apply(this, arguments);
@@ -14,6 +17,22 @@ var A = Activity.extend(function reviewServiceProfessionalActivity() {
     this.navBar = Activity.createSubsectionNavBar('Scheduler', {
         backLink: '/scheduling', helpLink: '/help/relatedArticles/201964153-how-owner-user-fees-work'
     });
+    this.$activity.find('#reviewServiceProfessional-index a').click(function (e) {
+        e.preventDefault();
+        e.stopImmediatePropagation();
+        $(this).tab('show');
+        var link = $(this).attr('href').replace(/^#reviewServiceProfessional-/, '');
+        this.app.shell.replaceState(null, null, '#!reviewServiceProfessional/' + link);
+    });
 });
 
 module.exports = A;
+
+A.prototype.show = function show(state) {
+    Activity.prototype.show.call(this, state);
+    
+    var tabName = state && state.route.segments && state.route.segments[0] || 'recommendation';
+    var tab = this.$activity.find('[href="#reviewServiceProfessional-' + tabName + '"]');
+    if (tab.length) tab.tab('show');
+};
+
