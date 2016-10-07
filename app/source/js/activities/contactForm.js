7e4344e968ad09459d80965e938fa39d9d8c7b95
Joshua Danielson <joshua.danielson@loconomics.com>
2016-10-06 17:23:18 -0700

    Revert "Added missing isInOnboarding to js files"
    This reverts commit 0b6b33f2cbabf2e62a767f6f8e5c8586f4d83489.

--- app/source/js/activities/contactForm.js
+++ app/source/js/activities/contactForm.js
@@ -43,8 +43,6 @@ A.prototype.show = function show(options) {
 
 var ko = require('knockout');
 function ViewModel(app) {
-
-    this.isInOnboarding = app.model.onboarding.inProgress;
     
     this.message = ko.observable('');
     this.isSending = ko.observable(false);
