b007a8ef68669bba439065ba4a9a168561f0e792
Joshua Danielson <joshua.danielson@loconomics.com>
2016-10-12 13:55:37 -0700

    Added ko placeholders
    #874

--- app/source/js/activities/userReview.js
+++ app/source/js/activities/userReview.js
@@ -4,25 +4,22 @@
 'use strict';
 
 var Activity = require('../components/Activity');
-
+var ko = require('knockout');
 var $ = require('jquery');
 
 var A = Activity.extend(function UserReviewActivity() {
-
-    Activity.apply(this, arguments);
-
-    this.accessLevel = null;
-    this.viewModel = {};
     
-    this.navBar = Activity.createSubsectionNavBar('Scheduler', {
-        backLink: '/scheduling', helpLink: '/help/relatedArticles/201964153-how-owner-user-fees-work'
-    });
-    this.$activity.find('#reviewServiceProfessional-index a').click(function (e) {
+    Activity.apply(this, arguments);
+    
+    this.viewModel = new ViewModel(this.app);
+    this.accessLevel = this.app.UserType.loggedUser;
+
+    this.$activity.find('#userReview-index a').click(function (e) {
         e.preventDefault();
         e.stopImmediatePropagation();
         $(this).tab('show');
-        var link = $(this).attr('href').replace(/^#reviewServiceProfessional-/, '');
-        this.app.shell.replaceState(null, null, '#!reviewServiceProfessional/' + link);
+        var link = $(this).attr('href').replace(/^#userReview-/, '');
+        this.app.shell.replaceState(null, null, '#!userReview/' + link);
     });
 });
 
@@ -32,7 +29,20 @@ A.prototype.show = function show(state) {
     Activity.prototype.show.call(this, state);
     
     var tabName = state && state.route.segments && state.route.segments[0] || 'recommendation';
-    var tab = this.$activity.find('[href="#reviewServiceProfessional-' + tabName + '"]');
+    var tab = this.$activity.find('[href="#userReview-' + tabName + '"]');
     if (tab.length) tab.tab('show');
 };
 
+function ViewModel(app) {
+    this.user = app.model.userProfile.data;
+    this.clientFirstName = ko.observable('Tim');
+    this.serviceProfessionalFirstName = ko.observable('Joshua');
+    this.serviceProfessionalJobTitle = ko.observable('Cleaning Professional');
+    this.isServiceProfessionalX = ko.observable(true);
+    this.isClientX = ko.observable(false);
+    this.isBookingEligibleToBeReviewed = ko.observable('1');
+    this.isClientAlreadyReviewed = ko.observable('0');
+    this.isServiceProfessionalAlreadyReviewed = ko.observable('0');
+    this.isClientEligibleToBeReviewed = ko.observable('1');
+    this.isServiceProfessionalEligibleToBeReviewed = ko.observable('1');
+}
