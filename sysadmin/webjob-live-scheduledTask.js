1e81278bb288106ca70f58422106dc9ccbc3d3a8
Joshua Danielson <joshua.danielson@loconomics.com>
2016-11-10 13:30:17 -0500

    Adding back docs & sysadmin
    #1145

---
+++ sysadmin/webjob-live-scheduledTask.js
@@ -0,0 +1,23 @@
+/**
+    Script to perform a call of the ScheduledTask URL
+    for a specific channel.
+    Was created to be used as an Azure Webjobs
+**/
+'use strict';
+
+// Change depending on TLS: http or https
+var request = require('https');
+
+var req = request.get({
+    host: 'loconomics.com',
+    path: '/ScheduledTask'
+}, function(response) {
+    // no need to do something with result.
+    //var body = ''
+    //response.on('data',function(d){ body += d })
+    response.on('end', function(){
+        console.log('Request End');
+    });
+});
+
+req.end();
