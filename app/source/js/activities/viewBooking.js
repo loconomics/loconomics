a3a3f58ff9305b0f0770205c0cb0b54a5360ed78
IagoSRL <iagosrl@gmail.com>
2017-02-27 13:29:24 +0100

    Using replaceState at viewBooking to enable back button #5

--- app/source/js/activities/viewBooking.js
+++ app/source/js/activities/viewBooking.js
@@ -1,6 +1,6 @@
 /**
     ViewBooking activity
-    
+
     It manages links to booking card given at emails.
     Because the app has different URLs for client and professional profiles,
     and sometimes the same URL needs to be provided, this behaves as the common URL
@@ -11,7 +11,7 @@
 var Activity = require('../components/Activity');
 
 var A = Activity.extend(function ViewBookingActivity() {
-    
+
     Activity.apply(this, arguments);
 
     this.accessLevel = this.app.UserType.loggedUser;
@@ -21,16 +21,16 @@ exports.init = A.init;
 
 A.prototype.show = function show(state) {
     Activity.prototype.show.call(this, state);
-    
+
     var bookingID = state && state.route.segments && state.route.segments[0];
     var currentUserID = this.app.model.userProfile.data.userID();
     this.app.model.bookings.getBooking(bookingID)
     .then(function(booking) {
         if (booking.serviceProfessionalUserID() === currentUserID) {
-            this.app.shell.go('/appointment/' + booking.serviceDateID());
+            this.app.shell.go('/appointment/' + booking.serviceDateID(), null, true);
         }
         else {
-            this.app.shell.go('/clientAppointment/' + bookingID);
+            this.app.shell.go('/clientAppointment/' + bookingID, null, true);
         }
     }.bind(this));
 };
