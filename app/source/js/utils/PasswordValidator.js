66c1a52cec0a9db8038111c7f686dd306b224ab8
IagoSRL <iagosrl@gmail.com>
2017-03-27 14:01:12 +0200

    Reduced password requirements to 3 out of 4 #1

--- app/source/js/utils/PasswordValidator.js
+++ app/source/js/utils/PasswordValidator.js
@@ -2,16 +2,22 @@
  * Lets validate a text against our password requirements.
  */
 'use strict';
-var pwdRequirementsLabel = 'Your password must be at least 8 characters long, have at least: one lowercase letter, one uppercase letter, one symbol (~!@#$%^*&;?.+_), and one numeric digit.';
-var pwdRegex = /(?=.{8,})(?=.*?[^\w\s])(?=.*?[0-9])(?=.*?[A-Z]).*?[a-z].*/;
+var DEFAULT_ERROR = 'Your password must be at least 8 characters long, have at least 3 out of these 4 requirements: one lowercase letter, one uppercase letter, one numeric digit and one symbol (~!@#$%^*&;?.+_).';
+var rLength = /.{8,}/;
+var rSymbol = /[^\w\s]/;
+var rNumber = /[0-9]/;
+var rUpper = /[A-Z]/;
+var rLower = /[a-z]/;
+var MIN_REQUIREMENTS = 3;
+
 /**
  * Creates a password validator
  * @class
- * @param {string} [msg=pwdRequirementsLabel] Error message when validation fails
+ * @param {string} [msg=DEFAULT_ERROR] Error message when validation fails
  */
 function PasswordValidator(msg) {
 
-    var defaultErrorMessage = msg || pwdRequirementsLabel;
+    var defaultErrorMessage = msg || DEFAULT_ERROR;
 
     /**
      * Error message from last tested value
@@ -32,11 +38,22 @@ function PasswordValidator(msg) {
     this.test = function(value) {
         // cache
         if (value === this.latestValue) return !!this.errorMessage;
-        // remember and check
+        // remember
         this.latestValue = value;
-        var t = pwdRegex.test(value);
-        this.errorMessage = t ? defaultErrorMessage : null;
-        return t;
+        // check
+        this.correctLength = rLength.test(value);
+        this.correctSymbol = rSymbol.test(value);
+        this.correctNumber = rNumber.test(value);
+        this.correctUpper = rUpper.test(value);
+        this.correctLower = rLower.test(value);
+        // Lenght is ever required, others requirements are optional if
+        // almost a minimum amount are met.
+        var count = this.correctSymbol + this.correctNumber +
+            this.correctUpper + this.correctLower;
+        var valid = this.correctLength && count >= MIN_REQUIREMENTS;
+        // set error
+        this.errorMessage = valid ? defaultErrorMessage : null;
+        return valid;
     };
 }
 
