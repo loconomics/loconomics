760f79112ec4e6afb8455c195be9282b55847fac
Matt Schaefer <matt@situatedbit.com>
2017-04-10 11:26:54 -0400

    on short displays scroll to validated password field on focus #318

--- app/source/js/viewmodels/ValidatedPassword.js
+++ app/source/js/viewmodels/ValidatedPassword.js
@@ -3,9 +3,11 @@
   */
 'use strict';
 
-var ko = require('knockout'),
+var $ = require('jquery'),
+    ko = require('knockout'),
     PasswordValidator = require('../utils/PasswordValidator'),
-    Field = require('./Field');
+    Field = require('./Field'),
+    scrollToElement = require('../utils/scrollToElement');
 
 /*
  * View model representing a password field with validation and messages.
@@ -14,74 +16,117 @@ var ko = require('knockout'),
  * @class
  */
 function ValidatedPassword() {
+    var MINIMUM_HEIGHT_FOR_SCROLL = 700,
+        SCROLL_ANIMATION_DURATION = 300,
+        hasScrolledToField = false,
+        showRequirements = ko.observable(false),
+        showRequirementsLink = ko.observable(true);
+
     /**
       * password is the value of the password field
       *
       * @public
       */
     this.password = new Field();
-    this.showRequirements = ko.observable(false);
-    this.showRequirementsLink = ko.observable(true);
 
     /**
      * @public
      */
     this.reset = function() {
-        this.showRequirements(false);
-        this.showRequirementsLink(true);
+        showRequirements(false);
+        showRequirementsLink(true);
         this.password('');
+        hasScrolledToField = false;
     };
 
+    /**
+     * Requirements link click handler
+     */
     this.requirementsLinkClick = function() {
-        this.showRequirements(true);
-    }.bind(this);
-
-    this.fieldFocus = function() {
-        if (!this.showRequirements()) {
-            // Only hide the link on focus if it hasn't been clicked on alreadyy
-            this.showRequirementsLink(false);
-        }
-
-        this.showRequirements(true);
+        showRequirements(true);
     }.bind(this);
 
     /**
+     * Password field focus handler
+     */
+    this.fieldFocus = function(viewModel, event) {
+        if (!showRequirements()) {
+            // Only hide the link on focus if it hasn't been clicked on already
+            showRequirementsLink(false);
+        }
+
+        showRequirements(true);
+
+        scrollToField(event.target);
+    }.bind(this);
+
+    /**
+     * Scroll to the password field when the heigt of the window
+     * is under a threshold. Assumption is that small devices will
+     * have a soft keyboard that will cover up the password requirements
+     * text.
+     *
+     * @private
+     */
+    var scrollToField = function(field) {
+        // only scroll to the field once and only if we are on a short display
+        if($(window).height() < MINIMUM_HEIGHT_FOR_SCROLL && !hasScrolledToField) {
+            scrollToElement(field, { animation: { duration: SCROLL_ANIMATION_DURATION } });
+            hasScrolledToField = true;
+        }
+    };
+
+    /**
+     * @public
+     */
+    this.showRequirements = ko.pureComputed(function() {
+        return showRequirements();
+    }, this);
+
+    /**
+     * @public
+     */
+    this.showRequirementsLink = ko.pureComputed(function() {
+        return showRequirementsLink();
+    }, this);
+
+    /**
       * @private
-      */ 
-    this.validator = ko.pureComputed(function() {
+      */
+    var validator = ko.pureComputed(function() {
         return new PasswordValidator(this.password());
     }, this);
 
     this.isValid = ko.pureComputed(function() {
-        return this.validator().isValid();
+        return validator().isValid();
     }, this);
 
     this.isLengthValid = ko.pureComputed(function() {
-        return this.validator().isCorrectLength();
+        return validator().isCorrectLength();
     }, this);
 
     this.isCharacterKindsValid = ko.pureComputed(function() {
-        return this.validator().isCorrectCharacterKinds();
+        return validator().isCorrectCharacterKinds();
     }, this);
 
     this.usesSymbol = ko.pureComputed(function() {
-        return this.validator().isCorrectSymbol();
+        return validator().isCorrectSymbol();
     }, this);
 
     this.usesUpper = ko.pureComputed(function() {
-        return this.validator().isCorrectUpper();
+        return validator().isCorrectUpper();
     }, this);
 
     this.usesLower = ko.pureComputed(function() {
-        return this.validator().isCorrectLower();
+        return validator().isCorrectLower();
     }, this);
 
     this.usesNumber = ko.pureComputed(function() {
-        return this.validator().isCorrectNumber();
+        return validator().isCorrectNumber();
     }, this);
 
     this.characterKindsLabel = ko.pureComputed(function() {
-        var count = this.validator().isCorrectCharacterKindsCount(),
+        var count = validator().isCorrectCharacterKindsCount(),
             labels = [
                     'Includes at least 3 character types:',
                     'Includes at least 2 more character types:',
