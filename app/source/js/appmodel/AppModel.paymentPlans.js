ed2707c4be752716f0616c16e4a372ec4fab7322
Joshua Danielson <joshua.danielson@loconomics.com>
2017-05-08 13:30:46 -0700

    Draft User Plans
    #191
    loconomics/loconomics-internal#1238

--- app/source/js/appmodel/AppModel.paymentPlans.js
+++ app/source/js/appmodel/AppModel.paymentPlans.js
@@ -10,22 +10,21 @@ var ListRemoteModel = require('../utils/ListRemoteModel');
 // PRESET
 var data = [
     {//new PaymentPlan({
-        paymentPlanID: 'Free',
-        name: 'Free',
-        summary: '',
-        description: '<ul>Includes:<li>x</li><li>y</li><li>z</li><ul>'
-    },
-    {//new PaymentPlan({
         paymentPlanID: 'OwnerGrowth',
         name: 'Owner Growth',
         summary: '$19 charged once a month',
-        description: '<ul>Includes:<li>x</li><li>y</li><li>z</li><ul>'
+        description: '<ul class="TilesList TilesList--openEnd">Includes:<li class="ItemTile"><i class="Tile-marker fa ion-ios-glasses-outline text-secondary"></i><div class="Tile-content"><p>Learn new skills to grow your business at our monthly workshops</p></div></li><li class="ItemTile"><i class="Tile-marker fa ion-android-contacts text-secondary"></i><div class="Tile-content"><p>Meet referral partners through exclusive networking events</p></div></li><li class="ItemTile"><i class="Tile-marker fa ion-ios-paper-outline text-secondary"></i><div class="Tile-content"><p>Expand your offerings with an extra marketplace listing</p></div></li><li class="ItemTile"><i class="Tile-marker fa ion-ios-cart-outline text-secondary"></i><div class="Tile-content"><p>Preferred listing placement in the marketplace</p></div></li><li class="ItemTile"><i class="Tile-marker fa ion-ios-color-filter-outline text-secondary"></i><div class="Tile-content"><p>Earn patronage and elect and/or run for our board of directors</p></div></li><ul>'
     },
     {//new PaymentPlan({
         paymentPlanID: 'OwnerPro',
         name: 'Owner Pro',
         summary: '$29 charged once a month',
-        description: 'Includes: ...'
+        description: '<ul class="TilesList TilesList--openEnd">Includes all features in Owner Growth plus<li class="ItemTile"><i class="Tile-marker fa ion-android-calendar text-secondary"></i><div class="Tile-content"><p>Get organized with our scheduling software</p></div></li><li class="ItemTile"><i class="Tile-marker fa ion-ios-pricetags-outline text-secondary"></i><div class="Tile-content"><p>Attract more clients by adding scheduling to your website</p></div></li><li class="ItemTile"><i class="Tile-marker fa ion-ios-bolt-outline text-secondary"></i><div class="Tile-content"><p>Increase your income & allow clients to book you instantly</p></div></li><li class="ItemTile"><i class="Tile-marker fa fa-vcard-o text-secondary"></i><div class="Tile-content"><p>Build loyalty with client-specific pricing</p></div></li><li class="ItemTile"><i class="Tile-marker fa fa-bank text-secondary"></i><div class="Tile-content"><p>Assistance with zero-interest loan application through our partnership with Kiva Local</p></div></li><ul>'
+        
+        
+
+
+
     },
     {//new PaymentPlan({
         paymentPlanID: 'OwnerProAnnual',
