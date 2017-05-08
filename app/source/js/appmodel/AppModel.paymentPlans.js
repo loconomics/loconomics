4be6f6b9666aa87c49eabf360a9e5de9b211ecc3
Joshua Danielson <joshua.danielson@loconomics.com>
2017-05-08 15:13:33 -0700

    Updates to User Plans
    #146
    #1238 (internal)

--- app/source/js/appmodel/AppModel.paymentPlans.js
+++ app/source/js/appmodel/AppModel.paymentPlans.js
@@ -13,24 +13,19 @@ var data = [
         paymentPlanID: 'OwnerGrowth',
         name: 'Owner Growth',
         summary: '$19 charged once a month',
-        description: '<ul class="TilesList TilesList--openEnd">Includes:<li class="ItemTile"><i class="Tile-marker fa ion-ios-glasses-outline text-secondary"></i><div class="Tile-content"><p>Learn new skills to grow your business at our monthly workshops</p></div></li><li class="ItemTile"><i class="Tile-marker fa ion-android-contacts text-secondary"></i><div class="Tile-content"><p>Meet referral partners through exclusive networking events</p></div></li><li class="ItemTile"><i class="Tile-marker fa ion-ios-paper-outline text-secondary"></i><div class="Tile-content"><p>Expand your offerings with an extra marketplace listing</p></div></li><li class="ItemTile"><i class="Tile-marker fa ion-ios-cart-outline text-secondary"></i><div class="Tile-content"><p>Preferred listing placement in the marketplace</p></div></li><li class="ItemTile"><i class="Tile-marker fa ion-ios-color-filter-outline text-secondary"></i><div class="Tile-content"><p>Earn patronage and elect and/or run for our board of directors</p></div></li><ul>'
+        description: '<ul class="TilesList TilesList--openEnd">Includes:<li class="ItemTile"><i class="Tile-marker hidden-xs fa ion-ios-glasses-outline text-secondary"></i><div class="Tile-content"><p>Learn new skills to grow your biz at our monthly workshops</p></div></li><li class="ItemTile"><i class="Tile-marker hidden-xs fa ion-android-contacts text-secondary"></i><div class="Tile-content"><p>Meet & match with referral partners in the cooperative</p></div></li><li class="ItemTile"><i class="Tile-marker hidden-xs fa ion-ios-paper-outline text-secondary"></i><div class="Tile-content"><p>Expand your offerings with an extra marketplace listing</p></div></li><li class="ItemTile"><i class="Tile-marker hidden-xs fa ion-ios-cart-outline text-secondary"></i><div class="Tile-content"><p>Preferred listing placement in the marketplace</p></div></li><li class="ItemTile"><i class="Tile-marker hidden-xs fa ion-ios-color-filter-outline text-secondary"></i><div class="Tile-content"><p>Earn patronage & vote/run for our board of directors</p></div></li><ul>'
     },
     {//new PaymentPlan({
         paymentPlanID: 'OwnerPro',
         name: 'Owner Pro',
-        summary: '$29 charged once a month',
-        description: '<ul class="TilesList TilesList--openEnd">Includes all features in Owner Growth plus<li class="ItemTile"><i class="Tile-marker fa ion-android-calendar text-secondary"></i><div class="Tile-content"><p>Get organized with our scheduling software</p></div></li><li class="ItemTile"><i class="Tile-marker fa ion-ios-pricetags-outline text-secondary"></i><div class="Tile-content"><p>Attract more clients by adding scheduling to your website</p></div></li><li class="ItemTile"><i class="Tile-marker fa ion-ios-bolt-outline text-secondary"></i><div class="Tile-content"><p>Increase your income & allow clients to book you instantly</p></div></li><li class="ItemTile"><i class="Tile-marker fa fa-vcard-o text-secondary"></i><div class="Tile-content"><p>Build loyalty with client-specific pricing</p></div></li><li class="ItemTile"><i class="Tile-marker fa fa-bank text-secondary"></i><div class="Tile-content"><p>Assistance with zero-interest loan application through our partnership with Kiva Local</p></div></li><ul>'
-        
-        
-
-
-
+        summary: '$39 charged once a month',
+        description: '<ul class="TilesList TilesList--openEnd">Includes everything in Owner Growth plus:<li class="ItemTile"><i class="Tile-marker hidden-xs fa ion-android-calendar text-secondary"></i><div class="Tile-content"><p>Get organized with our scheduling software</p></div></li><li class="ItemTile"><i class="Tile-marker hidden-xs fa ion-ios-pricetags-outline text-secondary"></i><div class="Tile-content"><p>Attract more clients by adding scheduling to your website</p></div></li><li class="ItemTile"><i class="Tile-marker hidden-xs fa ion-ios-bolt-outline text-secondary"></i><div class="Tile-content"><p>Increase your income & allow clients to book you instantly</p></div></li><li class="ItemTile"><i class="Tile-marker hidden-xs fa fa-vcard-o text-secondary"></i><div class="Tile-content"><p>Build loyalty with client-specific pricing</p></div></li><li class="ItemTile"><i class="Tile-marker hidden-xs fa fa-bank text-secondary"></i><div class="Tile-content"><p>Apply for a zero-interest loan through our partnership with Kiva Local</p></div></li><ul>'
     },
     {//new PaymentPlan({
         paymentPlanID: 'OwnerProAnnual',
         name: 'Owner Pro Annual',
-        summary: '$290 charged once a year (Save $58 with this option)',
-        description: 'Includes: ...'
+        summary: '$390 charged once a year (Save $78 with this option)',
+        description: '<ul class="TilesList TilesList--openEnd">Includes everything in Owner Growth & Pro plus:<li class="ItemTile"><i class="Tile-marker hidden-xs fa ion-arrow-graph-up-right text-secondary"></i><div class="Tile-content"><p>Get on track with a half-day biz assessment from experts</p></div></li><ul>'
     }
 ];
 
