cb7d34f9e3c5e14aea7bda3b8490be2d94531b4b
Joshua Danielson <joshua.danielson@loconomics.com>
2017-05-09 09:00:47 -0700

    Added Upgrade page
    #385

---
+++ app/source/js/activities/upgrade.js
@@ -0,0 +1,23 @@
+/**
+    Upgrade activity
+**/
+'use strict';
+
+var Activity = require('../components/Activity');
+
+var A = Activity.extend(function UpgradeActivity() {
+    
+    Activity.apply(this, arguments);
+
+    this.accessLevel = this.app.UserType.loggedUser;
+    
+    var app = this.app;
+    
+    this.navBar = new Activity.NavBar({
+        title: null,
+        leftAction: Activity.NavAction.menuIn,
+        rightAction: Activity.NavAction.menuNewItem
+    });
+});
+
+exports.init = A.init;
