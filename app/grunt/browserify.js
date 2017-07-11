20061c677d9f1d5478ba6d79c8546500ae3b97d7
IagoSRL <iagosrl@gmail.com>
2017-07-11 18:16:34 +0200

    Removed 'require' config from browserify, aliases ported to the 'browser' field properly. Removed explicit 'events' alias, most modern version pre-included in Browserify #360

--- app/grunt/browserify.js
+++ app/grunt/browserify.js
@@ -4,7 +4,6 @@ module.exports = function(/*grunt*/) {
     /**
         Browserify config
     **/
-<<<<<<< HEAD
 	var bconfig = {
         options: {
             transform: [['stringify', {
@@ -16,9 +15,6 @@ module.exports = function(/*grunt*/) {
         }
     };
 
-=======
-    var bconfig = {};
->>>>>>> Workaround to load jquery.fileupload-image.js with latest Browserify. Now everything seems to be working with up-to-date versions #360
     /**
         App bundle
     **/
@@ -29,36 +25,7 @@ module.exports = function(/*grunt*/) {
         'dest': './build/assets/js/app.js',
         'options': {
             // Enable debug evern when compiling script.js, the min.js will delete debug info for production use:
-            'debug': false,
-            'require': [
-                'jquery',
-                'bootstrap',
-                'bootstrap-switch',
-                'jquery.ajaxQueue',
-
-                'moment',
-                'numeral',
-                'knockout',
-                'knockout.mapping',
-                //'lodash', // specific modules fetched on demand to minimize the size
-                'es6-promise',
-                'localforage',
-                'is_js',
-                'moment-timezone',
-                './source/js/custom-modernizr.js:custom-modernizr',
-                // Using a specific browser version of events.EventEmitter, to avoid
-                // the extra load of NodeJS/Browserify 'events' module that has heavy-unneed
-                // dependencies as 'utils'.
-                './node_modules/events:events',
-                './vendor/iagosrl/ko/formatBinding:ko/formatBinding',
-                './vendor/iagosrl/ko/domElementBinding:ko/domElementBinding',
-                './vendor/iagosrl/layoutUpdateEvent:layoutUpdateEvent',
-                './vendor/iagosrl/throttle:iagosrl/throttle',
-                'knockout-sortable',
-                'geocomplete',
-                'fastclick',
-                'jquery.ui.touch-punch'
-            ]
+            'debug': false
         }
     };
 
