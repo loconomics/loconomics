df0b584c60b9fd9888087e2f26d176c42afaebb2
IagoSRL <iagosrl@gmail.com>
2017-07-13 17:07:28 +0200

    Initial set-up for factor-bundle plugin for Browserify, with new 'common.js' bundle before the app specific one #360

--- app/grunt/browserify.js
+++ app/grunt/browserify.js
@@ -10,14 +10,29 @@ module.exports = function(/*grunt*/) {
         App bundle
     **/
     bconfig.app = {
-        'src': [
-            './source/js/app.js'
-        ],
-        'dest': './build/assets/js/app.js',
-        'options': {
-            'browserifyOptions': {
-                'debug': true
-            }
+        files: {
+            './build/assets/js/common.js': [
+                './source/js/app.js',
+                './source/js/landingPage.js'
+            ]
+        },
+        options: {
+            browserifyOptions: {
+                debug: true
+            },
+            plugin: [
+                [
+                    'factor-bundle', {
+                        // Output take strictkly the same order than listed
+                        // source files, so they match the expected name-content
+                        o: [
+                            './build/assets/js/app.js',
+                            // landingPage renamed as 'welcome' in the result
+                            './build/assets/js/welcome.js'
+                        ]
+                    }
+                ]
+            ]
         }
     };
 
