5d69edabca3db1f6a1dd2908f6a0d321d28c7adf
Joshua Danielson <joshua.danielson@loconomics.com>
2017-08-09 17:19:04 -0700

    Revised headings
    #526
    #532

--- app/source/js/activities/calendar.js
+++ app/source/js/activities/calendar.js
@@ -19,7 +19,8 @@ var A = Activity.extend(function CalendarActivity() {
 
     this.accessLevel = this.app.UserType.loggedUser;
     this.viewModel = new ViewModel(this.app);
-    this.navBar = Activity.createSectionNavBar('Calendar');
+    // null for logo
+    this.navBar = Activity.createSectionNavBar(null);
 
     /* Getting elements */
     this.$datepicker = this.$activity.find('#calendarDatePicker');
