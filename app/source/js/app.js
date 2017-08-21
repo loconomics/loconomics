c99d1fd98a8daaba50ef4c7365f79ebe81ea5d64
IagoSRL <iagosrl@gmail.com>
2017-08-21 18:28:18 +0200

    Removed third party plugin and integration of Boostrap-switch, now unused (not accessible, over complex) #528

--- app/source/js/app.js
+++ app/source/js/app.js
@@ -252,7 +252,6 @@ var appInit = function appInit() {
 
     // Load Knockout binding helpers
     bootknock.plugIn(ko);
-    require('./utils/bootstrapSwitchBinding').plugIn(ko);
     require('./utils/pressEnterBindingHandler').plugIn(ko);
     require('./utils/fileUploaderBindingHandler').plugIn(ko);
 
