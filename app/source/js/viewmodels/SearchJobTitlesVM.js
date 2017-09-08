c377610e0d5b6d1dc548d4113766edc1253786bd
IagoSRL <iagosrl@gmail.com>
2017-09-08 22:47:01 +0200

    Refactored duplicated and common code for auto-search by job-title code into MarketplaceSearch ViewModel #528 #635 #626 #625

--- app/source/js/viewmodels/SearchJobTitlesVM.js
+++ app/source/js/viewmodels/SearchJobTitlesVM.js
@@ -3,58 +3,13 @@
 **/
 'use strict';
 
-var SearchResults = require('../models/SearchResults');
 var ko = require('knockout');
 var user = require('../data/userProfile').data;
-var search = require('../data/search');
+var MarketplaceSearchVM = require('./MarketplaceSearch');
 
 module.exports = function SearchJobTitlesVM() {
-    this.isLoading = ko.observable(false);
-    //create an observable variable to hold the search term
-    this.searchTerm = ko.observable();
-    // Coordinates
-    this.lat = ko.observable(search.DEFAULT_LOCATION.lat);
-    this.lng = ko.observable(search.DEFAULT_LOCATION.lng);
-    this.city = ko.observable();
-    this.searchDistance = ko.observable(search.DEFAULT_LOCATION.searchDistance);
-    //create an object named SearchResults to hold the search results returned from the API
-    this.searchResults = new SearchResults();
-
-    this.loadData = function(searchTerm, lat, lng) {
-        this.isLoading(true);
-
-        return search.byTerm(searchTerm, lat, lng)
-        .then(function(searchResults) {
-            if(searchResults){
-                //update searchResults object with all the data from the API
-                this.searchResults.model.updateWith(searchResults, true);
-            }
-            else {
-                this.searchResults.model.reset();
-            }
-            this.isLoading(false);
-        }.bind(this))
-        .catch(function(err) {
-            this.isLoading(false);
-            if (err && err.statusText === 'abort') return null;
-        }.bind(this));
-    };
-    //creates a handler function for the html search button (event)
-    this.search = function() {
-        //creates a variable for the search term to check to see when a user enters more than 2 characters, we'll auto-load the data.
-        var s = this.searchTerm();
-        if(s && s.length > 2) {
-            this.loadData(s, this.lat(), this.lng());
-        }
-        else {
-            this.searchResults.model.reset();
-        }
-    };
-    //anything that happens in the computed function after a timeout of 60 seconds, run the code
-    ko.computed(function() {
-        this.search();
-    //add ",this" for ko.computed functions to give context, when the search term changes, only run this function every 60 milliseconds
-    },this).extend({ rateLimit: { method: 'notifyAtFixedRate', timeout: 300 } });
+    // Inherits
+    MarketplaceSearchVM.call(this);
 
     this.isInput = ko.pureComputed(function() {
         var s = this.searchTerm();
