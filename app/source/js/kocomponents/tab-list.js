b0519f6dd8915a1298f31630a77f145f44a1b8e0
IagoSRL <iagosrl@gmail.com>
2017-09-29 11:10:37 -0700

    Internal change of tab-list component that refactor a bit simplifying initialization and must help to allow dynamically rendered tabs/links #528

--- app/source/js/kocomponents/tab-list.js
+++ app/source/js/kocomponents/tab-list.js
@@ -49,138 +49,136 @@ function ViewModel(params) {
 var create = function(params, componentInfo) {
     var vm = new ViewModel(params);
     var $root = $(componentInfo.element);
-    var $tabLinks = $(componentInfo.templateNodes)
-    .find('a[href^="' + vm.prefix + '"]');
-    // Set-up ARIA attributes
-    $root.attr('role', 'tablist');
-    // Set-up each link
-    $tabLinks
-    .attr('role', 'tab')
-    .attr('tabindex', '-1')
-    .each(function(i, tabLink) {
-        tabLink = $(tabLink);
-        var link = tabLink.attr('href');
-        var id = link.replace(/^#/, '');
-        tabLink.attr('aria-controls', id);
-        // Set-up panel
-        $(link).attr('role', 'tabpanel');
-    });
-    // Catch link clicks and set active tab
-    var setElementAsActive = function(tabElement, e) {
-        var link = $(tabElement).attr('href');
-        var isPrefix = link.indexOf(vm.prefix) === 0;
-        if (isPrefix) {
-            // Save which one is active
-            var tabName = isPrefix ? link.replace(vm.prefix, '') : link;
-            vm.active(tabName);
-            if (e) {
-                // Avoid standard behavior
-                e.preventDefault();
-                e.stopImmediatePropagation();
-            }
-        }
-    };
-    $root.on('click', 'a[href]', function(e) {
-        setElementAsActive(this, e);
-    });
-    // Switch active tab on change
-    var updateActive = function(tabName) {
-        if(!tabName) return;
-        // Get fragment URL
-        var url = vm.prefix + tabName;
-        if (!/^#/.test(url)) {
-            url = '#' + url;
-        }
-        // Get new elements
-        var tab = $root.find('[href="' + url + '"]');
-        var panel = $(url);
-        if (tab.length) {
-            // Previous tab is disabled an new one make active, touching tab
-            // and panel; when tab/link is part of list, is useful to mark the
-            // container list-item too
 
-            // Disable previous one
-            var prevTab = $tabLinks
-            .filter('.' + ACTIVE_TAB_CLASS)
-            .removeClass(ACTIVE_TAB_CLASS)
-            .attr('aria-selected', null)
-            .attr('tabindex', '-1');
-            prevTab.parent('li')
-            .removeClass(ACTIVE_TAB_CLASS);
-            var prevUrl = prevTab.attr('href');
-            if (prevUrl) {
-                $(prevUrl)
-                .removeClass(ACTIVE_TAB_CLASS)
-                .attr('aria-selected', null)
-                .hide();
-            }
-            // Enable new ones
-            tab
-            .addClass(ACTIVE_TAB_CLASS)
-            .attr('aria-selected', 'true')
-            .attr('tabindex', '0');
-            tab.parent('li')
-            .addClass(ACTIVE_TAB_CLASS);
-            panel
-            .addClass(ACTIVE_TAB_CLASS)
-            .attr('aria-selected', 'true')
-            .show();
-        }
-    };
-    ko.computed(function() {
-        updateActive(vm.active());
-    });
     /**
-     * @overview Run some logics after component was rendered (cannot be done
-     * before), this must be available at the viewModel.
+     * @overview Set-up children elements, external linked panels, event
+     * handlers and 'active tab' detection.
+     * This all requires to have access to the children elements rendered that
+     * means cannot be done right at the constructor (at 'create') but just
+     * once time after the component and it's children elements are 'rendered'
+     * (created on the DOM).
      * @private
      */
     vm.afterRender = function() {
-        // Get updated list of links from the component
-        // (previously, is filled in with the children nodes for set-up, but
-        // later needs to get new ones created by cloning that ones)
-        // (this is in the closure, accessed by previous functions to manage
-        // change of the active tab)
-        $tabLinks = $root
+        var $tabLinks = $root
         .find('a[href^="' + vm.prefix + '"]');
-        // Forces an update of the 'active' when component is ready
-        updateActive(vm.active());
+        // Set-up ARIA attributes
+        $root.attr('role', 'tablist');
+        // Set-up each link
+        $tabLinks
+        .attr('role', 'tab')
+        .attr('tabindex', '-1')
+        .each(function(i, tabLink) {
+            tabLink = $(tabLink);
+            var link = tabLink.attr('href');
+            var id = link.replace(/^#/, '');
+            tabLink.attr('aria-controls', id);
+            // Set-up panel
+            $(link).attr('role', 'tabpanel');
+        });
+        // Catch link clicks and set active tab
+        var setElementAsActive = function(tabElement, e) {
+            var link = $(tabElement).attr('href');
+            var isPrefix = link.indexOf(vm.prefix) === 0;
+            if (isPrefix) {
+                // Save which one is active
+                var tabName = isPrefix ? link.replace(vm.prefix, '') : link;
+                vm.active(tabName);
+                if (e) {
+                    // Avoid standard behavior
+                    e.preventDefault();
+                    e.stopImmediatePropagation();
+                }
+            }
+        };
+        $root.on('click', 'a[href]', function(e) {
+            setElementAsActive(this, e);
+        });
+        // Switch active tab on change
+        var updateActive = function(tabName) {
+            if(!tabName) return;
+            // Get fragment URL
+            var url = vm.prefix + tabName;
+            if (!/^#/.test(url)) {
+                url = '#' + url;
+            }
+            // Get new elements
+            var tab = $root.find('[href="' + url + '"]');
+            var panel = $(url);
+            if (tab.length) {
+                // Previous tab is disabled an new one make active, touching tab
+                // and panel; when tab/link is part of list, is useful to mark the
+                // container list-item too
+
+                // Disable previous one
+                var prevTab = $tabLinks
+                .filter('.' + ACTIVE_TAB_CLASS)
+                .removeClass(ACTIVE_TAB_CLASS)
+                .attr('aria-selected', null)
+                .attr('tabindex', '-1');
+                prevTab.parent('li')
+                .removeClass(ACTIVE_TAB_CLASS);
+                var prevUrl = prevTab.attr('href');
+                if (prevUrl) {
+                    $(prevUrl)
+                    .removeClass(ACTIVE_TAB_CLASS)
+                    .attr('aria-selected', null)
+                    .hide();
+                }
+                // Enable new ones
+                tab
+                .addClass(ACTIVE_TAB_CLASS)
+                .attr('aria-selected', 'true')
+                .attr('tabindex', '0');
+                tab.parent('li')
+                .addClass(ACTIVE_TAB_CLASS);
+                panel
+                .addClass(ACTIVE_TAB_CLASS)
+                .attr('aria-selected', 'true')
+                .show();
+            }
+        };
+        ko.computed(function() {
+            updateActive(vm.active());
+        });
+
+        // Keyboard management
+        var goTabIndexOffset = function(indexOffset) {
+            var active = $tabLinks.filter('.' + ACTIVE_TAB_CLASS);
+            var index = $tabLinks.index(active);
+            var last = $tabLinks.length - 1;
+            index += indexOffset;
+            if (index < 0) {
+                index = last;
+            }
+            else if (index > last) {
+                index = 0;
+            }
+            var newTab = $tabLinks.eq(index);
+            setElementAsActive(newTab);
+            newTab.focus();
+        };
+        var goPrevious = function() {
+            goTabIndexOffset(-1);
+        };
+        var goNext = function() {
+            goTabIndexOffset(1);
+        };
+        // Arrow keys must move between tabs
+        var LEFT_KEY = 37;
+        var RIGHT_KEY = 39;
+        $root.on('keydown', function(e) {
+            switch (e.which) {
+                case LEFT_KEY:
+                    goPrevious();
+                    break;
+                case RIGHT_KEY:
+                    goNext();
+                    break;
+            }
+        });
     };
-    // Keyboard management
-    var goTabIndexOffset = function(indexOffset) {
-        var active = $tabLinks.filter('.' + ACTIVE_TAB_CLASS);
-        var index = $tabLinks.index(active);
-        var last = $tabLinks.length - 1;
-        index += indexOffset;
-        if (index < 0) {
-            index = last;
-        }
-        else if (index > last) {
-            index = 0;
-        }
-        var newTab = $tabLinks.eq(index);
-        setElementAsActive(newTab);
-        newTab.focus();
-    };
-    var goPrevious = function() {
-        goTabIndexOffset(-1);
-    };
-    var goNext = function() {
-        goTabIndexOffset(1);
-    };
-    // Arrow keys must move between tabs
-    var LEFT_KEY = 37;
-    var RIGHT_KEY = 39;
-    $root.on('keydown', function(e) {
-        switch (e.which) {
-            case LEFT_KEY:
-                goPrevious();
-                break;
-            case RIGHT_KEY:
-                goNext();
-                break;
-        }
-    });
+
     // Ready
     return vm;
 };
