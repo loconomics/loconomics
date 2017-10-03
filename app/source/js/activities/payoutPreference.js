0c383ba11c80eb6d4e08772a77ddcbc865695ee6
IagoSRL <iagosrl@gmail.com>
2017-10-03 11:15:30 -0700

    Removed onboarding from payoutPreferences #571. Moved viewModel to activity, removed explicit viewModel (simple and clear). #567 Updated markup for title and description following accessibility guidelines #528

--- app/source/js/activities/payoutPreference.js
+++ app/source/js/activities/payoutPreference.js
@@ -1,35 +1,28 @@
 /**
     Professionals Payment Preference activity
+
+    IMPORTANT: At template we are using activity.isShown to enable components at the template,
+    their set-up is simplified by being re-created every time the
+    activity is displayed and disposed when hidden (this prevents from
+    needing reset/discard/show-hide-detection logics at component level;
+    they does init at constructor and implement 'dispose' method).
 **/
 'use strict';
 
 var Activity = require('../components/Activity');
-var onboarding = require('../data/onboarding');
 require('../kocomponents/payout/preference-view');
 
 var A = Activity.extend(function PaymentPreferenceActivity() {
 
     Activity.apply(this, arguments);
 
-    this.viewModel = new ViewModel();
     this.accessLevel = this.app.UserType.serviceProfessional;
+    this.helpLink = '/help/relatedArticles/201967096-accepting-and-receiving-payments';
     this.navBar = Activity.createSubsectionNavBar('Account', {
         backLink: '/account',
-        helpLink: this.viewModel.helpLink
+        helpLink: this.helpLink
     });
-    // Share navBar with desktop nav through viewModel
-    this.viewModel.navBar = this.navBar;
-    // Using activity.isShown to enable components at the template,
-    // their set-up is simplified by being re-created every time the
-    // activity is displayed and disposed when hidden (this prevents from
-    // needing reset/discard/show-hide-detection logics at component level;
-    // they does init at constructor and implement 'dispose' method).
-    this.viewModel.isShown = this.isShown;
+    this.title = 'Payout preferences';
 });
 
 exports.init = A.init;
-
-function ViewModel() {
-    this.helpLink = '/help/relatedArticles/201967096-accepting-and-receiving-payments';
-    this.isInOnboarding = onboarding.inProgress;
-}
