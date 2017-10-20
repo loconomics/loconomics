7453dcf92b60909ab7aeaa710ac9e2113d19ff86
Joshua Danielson <joshua.danielson@loconomics.com>
2017-10-20 11:32:19 -0700

    Removed reviews from listingEditor.js
    #580
    - not used

--- app/source/js/activities/listingEditor.js
+++ app/source/js/activities/listingEditor.js
@@ -92,8 +92,6 @@ A.prototype.loadData = function(userID, jobTitleID) {
                     });
                 });
                 this.viewModel.user().selectedJobTitleID(jobTitleID);
-                // Load extra job data (reviews)
-                this.viewModel.reviews.load({ limit: 2 });
             }
         }.bind(this))
         .catch(function(err) {
@@ -120,7 +118,6 @@ A.prototype.show = function show(options) {
     var userID = (params[0] |0) || user.userID();
     var jobTitleID = params[1] |0;
     this.loadData(userID, jobTitleID);
-    this.viewModel.reviews.reset(userID, jobTitleID);
     this.viewModel.refreshTs(new Date());
     this.viewModel.userID(userID);
     this.viewModel.jobTitleID(jobTitleID);
@@ -139,7 +136,6 @@ function ViewModel(app) {
     this.jobTitleName = ko.pureComputed(function() {
         return this.jobTitle() && this.jobTitle().singularName() || 'Job Title';
     }, this);
-    this.reviews = new ReviewsVM();
 
     this.timeZone = ko.pureComputed(function(){
         return this.user() && this.user().weeklySchedule() && this.user().weeklySchedule().timeZone().replace('US/','');
@@ -210,8 +206,6 @@ function ViewModel(app) {
 
     this.changeJobTitle = function(jobTitle, event) {
         this.user().selectedJobTitleID(jobTitle.jobTitleID());
-        this.reviews.reset(undefined, jobTitle.jobTitleID());
-        this.reviews.load({ limit: 2 });
         if (event) {
             event.preventDefault();
             event.stopImmediatePropagation();
@@ -360,89 +354,4 @@ function ViewModel(app) {
             return AlertLink.fromProfileAlert(profileAlert, { jobTitleID: jobTitleID });
         });
     }, this);
-}
-
-var PublicUserReview = require('../models/PublicUserReview');
-function ReviewsVM() {
-    this.userID = ko.observable(null);
-    this.jobTitleID = ko.observable(null);
-    this.list = ko.observableArray([]);
-    this.isLoading = ko.observable(false);
-    this.endReached = ko.observable(false);
-    var currentXhr = null;
-
-    this.reset = function reset(userID, jobTitleID) {
-        this.list([]);
-        if (userID)
-            this.userID(userID);
-        this.jobTitleID(jobTitleID);
-        this.endReached(false);
-        if (currentXhr && currentXhr.abort) {
-            currentXhr.abort();
-        }
-    };
-
-    this.load = function loadReviews(options) {
-        options = options || {};
-        if (this.isLoading() || this.endReached() || !this.userID()) return;
-        this.isLoading(true);
-        var task = users.getReviews(this.userID(), this.jobTitleID(), options)
-        .then(function(newData) {
-            //jshint maxcomplexity:8
-            if (newData && newData.length) {
-                if (newData.length < (options.limit || 20)) {
-                    this.endReached(true);
-                }
-                // convert the newData to Model instances
-                newData = newData.map(function(d) { return new PublicUserReview(d); });
-                var list = this.list();
-                if (options.since && options.until) {
-                    // Insert in the middle
-                    // TODO Unused situation right now, not supported in the UI
-                    throw new Error('UNSUPPORTED SET-UP: since and until parameters at the same time');
-                }
-                else if (options.since) {
-                    // We 'suppose' that a 'since' request from the previous first item (descending order) was performed
-                    // so we add the newData to the beggining of the list
-                    list = newData.concat.apply(newData, list);
-                }
-                else if (options.until) {
-                    // We 'suppose' that an 'until' request from the previous last item  (descending order) was performed
-                    // so we add the newData to the ending of the list
-                    list.push.apply(list, newData);
-                }
-                else {
-                    // Just new, to replace, data
-                    list = newData;
-                }
-                this.list(list);
-            }
-            else {
-                this.endReached(true);
-            }
-            this.isLoading(false);
-            currentXhr = null;
-        }.bind(this))
-        .catch(function(err) {
-            this.isLoading(false);
-            currentXhr = null;
-            if (err && err.statusText !== 'abort') {
-                console.error('Error loading user reviews', err);
-                throw err;
-            }
-        }.bind(this));
-        currentXhr = task.xhr;
-        return task;
-    }.bind(this);
-
-    this.loadOlder = function loadOlderReviews() {
-        var l = this.list();
-        var last = l[l.length - 1];
-        return this.load({ limit: 10, until: last && last.updatedDate() });
-    }.bind(this);
-
-    this.loadNewer = function loadOlderReviews() {
-        var first = this.list()[0];
-        return this.load({ limit: 10, since: first && first.updatedDate() });
-    }.bind(this);
 }
