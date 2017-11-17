2b22f520efb184bb8047ca86eb1f2ecebd884241
IagoSRL <iagosrl@gmail.com>
2017-11-17 12:13:37 +0100

    Added new accountStatus -1 #675

---
+++ web/_DBUpdate/Issue-675 - 1 - New account status for non users subscribers.sql
@@ -0,0 +1,5 @@
+  INSERT INTO accountstatus (AccountStatusID, AccountStatusName, AccountStatusDescription, CreatedDate, UpdatedDate, ModifiedBy, Active)
+  VALUES (-1, 'Subscriber',
+  'As any negative ID, this represents a not enabled account -no TOU accepted, no password. User subscribed for newsletter and/or referring professionals',
+  '2017-11-17 03:10:00', '2017-11-17 03:10:00', 'ils', 1)
+  
