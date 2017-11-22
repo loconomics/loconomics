c9bb7bb6041550a26fffe6d35a1c4e38ea0f8ed5
IagoSRL <iagosrl@gmail.com>
2017-11-22 18:19:43 +0100

    Updated 'git archive_branches' to allow not ask for every step

--- devops/git-tools/archive_merged_branches.py
+++ devops/git-tools/archive_merged_branches.py
@@ -4,6 +4,9 @@ parameter -single value-).
 This is done by tagging them as archive/<branchname> and removing them both locally and
 remotely. Before each operation, the user is asked for confirmation.
 
+Author: http://ctenbrinke.net/author/chiel/
+Source: http://ctenbrinke.net/2016/06/07/archiving-branches-with-git/
+
 @iagosrl 2017-08-01 Added command line parameter to specify the branch of
 reference (master by default)
 
@@ -12,7 +15,7 @@ NOTE: It asks for confirmation of every found branch.
 Set-up this at git to be used as 'git archive-branches' by adding it to the '.gitconfig' file under alias section like:
 [alias]
     archive-branches = !python ~/loconomics/devops/git-tools/archive_merged_branches.py
-    
+
 Usage
 # archive branches merged into master
 > git archive-branches
@@ -34,20 +37,24 @@ merged_branches = [line.strip() for line in
 merged_branches = [branch for branch in merged_branches
                    if branch != '' and not '*' in branch and not branch == MASTER and not branch == REFERENCE_BRANCH]
 archived_branches = []
+archived_branches_all = []
 for branch in merged_branches:
-    if confirm('Archive branch {}?', branch):
+    if confirm('Archive branch {}?', branch)
         out('git tag archive/{} {}', branch, branch)
         archived_branches.append(branch)
 
 if not archived_branches:
     exit('No branches archived. Bye.')
 
+print('Next tasks are: push tags to remote, delete remote and local branches')
+do_not_ask = not confirm('Request confirmation for every step?')
+
 # Push archive tags to remote
 print()
 print('Created archive tags:')
 for branch in archived_branches:
     print('    ' + branch)
-if confirm('Push archive tags to remote?'):
+if do_not_ask or confirm('Push archive tags to remote?'):
     for branch in archived_branches:
         out('git push origin archive/{}', branch)
 
@@ -57,7 +64,7 @@ print()
 print('Corresponding remote branches:')
 for branch in archived_branches:
     print('    origin/' + branch)
-if confirm('Delete remote branches?'):
+if do_not_ask or confirm('Delete remote branches?'):
     for branch in archived_branches:
         out('git push origin :{}', branch)
 
@@ -67,6 +74,6 @@ print()
 print('Corresponding local branches:')
 for branch in archived_branches:
     print('    ' + branch)
-if confirm('Delete local branches?'):
+if do_not_ask or confirm('Delete local branches?'):
     for branch in archived_branches:
         out('git branch -d {}', branch)
