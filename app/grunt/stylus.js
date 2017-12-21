e615ea5fc779cb88f20162a0fa9bdd1eed966d69
IagoSRL <iagosrl@gmail.com>
2017-12-21 11:41:25 +0100

    Updated settings for Stylus, based on testing, using compress takes even less compile time no more and just removes whitespace, doesn't hurts debugging; linenos was never helpful, and slows >2x the process #745

--- app/grunt/stylus.js
+++ app/grunt/stylus.js
@@ -4,19 +4,14 @@ var assetsBannerTpl = require('./shared/assetsBannerTpl');
 
 module.exports = {
     options: {
-        // Se usa cssmin para compresión, éste sin comprimir y con información de depuración
-        compress: false,
-        linenos: true,
-        //paths: ['path/to/import', 'another/to/import'],
+        // Let it remove whitespace; it actually takes less time no more to compile
+        compress: true,
+        // Line numbers in th CSS are not really helpful, disabled (has not proper source maps)
+        linenos: false,
         // use embedurl('test.png') in our code to trigger Data URI embedding
         urlfunc: 'embedurl',
         'include css': true,
         banner: assetsBannerTpl
-        /*
-        import: [      //  @import 'foo', 'bar/moo', etc. into every .styl file
-        'foo',       //  that is compiled. These might be findable based on values you gave
-        'bar/moo'    //  to `paths`, or a plugin you added under `use`
-        ]*/
     },
     app: {
         files: {
