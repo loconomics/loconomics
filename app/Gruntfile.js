47cf91ca465e25b04719acefaf0618aed37fbd1c
IagoSRL <iagosrl@gmail.com>
2017-12-26 12:26:26 +0100

    Added workaround for experimental http2 warning-error running Grunt

--- app/Gruntfile.js
+++ app/Gruntfile.js
@@ -1,12 +1,19 @@
 /** Grunt tasks configuration
 **/
 module.exports = function (grunt) {
+    /* eslint-env node */
+    /* eslint no-process-env:0 */
+
+    // WORKAROUND Node 8.9.x and Connect using http2 still experimental
+    if(!process.env['NODE_NO_HTTP2']) {
+        process.env['NODE_NO_HTTP2'] = 1;
+    }
 
     require('time-grunt')(grunt);
 
     require('load-grunt-config')(grunt);
-    
+
     grunt.loadTasks('./grunt/custom/');
-    
+
     grunt.task.run('notify_hooks');
 };
