344ae98adf360d78dd0faab2cd3e6a9c1755d866
samwaoz <samwa_oz@yahoo.co.uk>
2017-12-28 01:28:17 +1100

    #9 initial commit for localised app using i18n nuget package

---
+++ globalisation-poc-web/Scripts/_references.js
@@ -0,0 +1,6 @@
+﻿/// <reference path="modernizr-2.8.3.js" />
+/// <reference path="jquery-ui-1.12.1.js" />
+/// <reference path="jquery-3.2.1.js" />
+/// <reference path="jquery.validate.js" />
+/// <reference path="jquery.validate.unobtrusive.js" />
+/// <reference path="knockout-2.3.0.debug.js" />
