e019e210b5e4fc47cdae0f94b05fcd328ebde78b
Nolan Darilek <nolan@thewordnerd.info>
2017-12-29 12:46:27 -0600

    Add initial badge-view component.

--- app/source/js/kocomponents/badge/view/index.js
+++ app/source/js/kocomponents/badge/view/index.js
@@ -4,8 +4,30 @@
  */
 'use strict';
 
+var getObservable = require('../../../utils/getObservable');
+
 var TAG_NAME = 'badge-view';
 var template = require('./template.html');
 
 var ko = require('knockout');
-var showError = require('../../../modals/error').show;
+//var showError = require('../../../modals/error').show;
+
+function ViewModel(params) {
+    const {src} = params;
+    this.image = getObservable('');
+    this.narrative = getObservable('');
+
+    fetch(src)
+    .then((r) => {
+      if(r.ok)
+        return r.json();
+    }).then((json) => {
+      this.image(json.image);
+      this.narrative(json.narrative);
+    });
+}
+
+ko.components.register(TAG_NAME, {
+    template: template,
+    viewModel: ViewModel
+});
