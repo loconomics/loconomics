da3230d9ace5cd3b41904dd02dfc7770bf74a566
samwaoz <samwa_oz@yahoo.co.uk>
2018-01-04 16:01:46 +1100

    #9 adding PO translation files
    adding pot and po files and updating gitignore for backup files that POEdit generates
    included SQL server compact db for POC

---
+++ globalisation-poc-web/App_Code/LcUrl.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for LcUrl
+/// </summary>
+public class LcUrl
+{
+    public LcUrl()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+    
+    /// <summary>
+    /// Get the Lang Identifier for URL.
+    /// Example: en-US, es-ES
+    /// </summary>
+    public static string LangId
+    {
+        get
+        {
+            return System.Globalization.CultureInfo.CurrentUICulture.Name;
+        }
+    }
+}
