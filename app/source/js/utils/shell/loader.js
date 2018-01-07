494dc4052ba3691c2f2492162a0390c68880e95c
IagoSRL <iagosrl@gmail.com>
2018-01-07 21:32:13 +0100

    Changed loader of script files: get back source maps #460

--- app/source/js/utils/shell/loader.js
+++ app/source/js/utils/shell/loader.js
@@ -3,8 +3,8 @@
 **/
 'use strict';
 
-var $ = require('jquery');
 var appActivities = require('../../activities');
+var loadScript = require('load-script2');
 
 const folder = 'assets/js/activities/';
 
@@ -15,13 +15,23 @@ module.exports = {
     load: function load(route) {
         return new Promise(function(resolve, reject) {
             //console.debug('Shell loading on demand', route.name, route);
-            $.getScript(
-                module.exports.baseUrl + folder + route.name + '.js'
-            ).done(function() {
-                // Resolve with the html, that will be injected by the Shell/DomItemsManager
-                resolve(appActivities.get(route.name).template);
-            })
-            .fail(reject);
+            loadScript(
+                module.exports.baseUrl + folder + route.name + '.js',
+                function(err) {
+                    if (err) { reject(err); }
+                    else {
+                        // Added timeout only for edge cases where this callback runs
+                        // on script loaded but not completely executed (for any weird reason, but
+                        // happened the very first time tried this code -and never more after that, just
+                        // being a bit paranoid to prevent strange to debug or support errors;
+                        // making it run after next task may ensure script was executed).
+                        setTimeout(function() {
+                            // Resolve with the html, that will be injected by the Shell/DomItemsManager
+                            resolve(appActivities.get(route.name).template);
+                        }, 13);
+                    }
+                }
+            );
         });
     }
 };
