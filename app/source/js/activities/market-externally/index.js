cc500ae53bd46d8dac2dd40f6ff697cf181d33f4
Joshua Danielson <joshua.danielson@loconomics.com>
2018-01-24 11:59:46 -0800

    Revised market-externally and suggestions-list
    #765
    - documentation completed
    - added backlinks
    - added userID data
     - specified userType to serviceProfessional

--- app/source/js/activities/market-externally/index.js
+++ app/source/js/activities/market-externally/index.js
@@ -1,31 +1,17 @@
 /**
- * Example of the most basic activity, implementing base properties and a
- * minimal template that uses an example component.
+ * Shows a professional what external platforms we suggest 
+ * they list their services on based on the job titles 
+ * they've already created.
  *
- * @module activities/_examples/a-basic-activity
- *
- * FIXME: Update this activity description
- * FIXME: After copy this example, some paths to imported modules needs to be
- * updated by removing one parent path ('../')
- *
- * REMOVEME: Check other examples for more documentation and the Activity
- * class source code (is partially documented and annotated --we are in the process to
- * enhance that documentation and remove obsolete parts)
+ * @module activities/market-externally
  */
 
-// REMOVEME: We MUST explicitly load each component used directly at the
-// template. Be aware that the example component we are loading register
-// itself with the name `component-example` rather than the folder name, not
-// following convention but done for simplicity of examples as commented
-// on their kocomponents/_examples/README.md file.
 import '../../kocomponents/external-platform/suggestions-list';
 import * as activities from '../index';
-// REMOVEME: After migration of old activities into folders, the base class will
-// move from this path into '../helpers/Activity', we will upgrade existent
-// activities too.
 import Activity from '../../components/Activity';
 import UserType from '../../enums/UserType';
 import template from './template.html';
+import userProfile from '../../data/userProfile';
 
 const ROUTE_NAME = 'market-externally';
 
@@ -33,64 +19,20 @@ export default class MarketExternallyActivity extends Activity {
 
     static get template() { return template; }
 
-    /**
-     * REMOVEME: No need to document standard behavior and parameters for the
-     * activity, since usually is just copy&paste of base Activity class and
-     * is redundant. But document with a jsdoc comment like this if some special
-     * behavior is worth note it.
-     * Use this constructor to set instance values for inherit and required
-     * members and create new members (usually observables and computed) usually
-     * needed at the template; methods needed at the template can be created
-     * as class methods or defined at the constructor if there is need to access
-     * some private/closure values or problems with the execution context ('bind')
-     * @param {jQuery} $activity
-     * @param {App} app
-     */
     constructor($activity, app) {
-        // REMOVEME: This set of parameters is being reviewed and can change/remove
-        // in a future (specially the 'app' global instance, replacing by modules
-        // explicitly imported for specific tasks).
+
         super($activity, app);
 
-        // FIXME: REQUIRED: Define which users can access this activity filtering by
-        // user type. Assign any valid UserType value.
-        // You can set the value 'null' to allow any
-        this.accessLevel = UserType.loggedUser;
-        // FIXME: REQUIRED: Define the kind of activity navbar used and values
-        // For now, next set-up is the usual for first-level activities (like
-        // home, dashboard, cms, ...). Use Activity.createSubsectionNavBar
-        // and accepted values for activities that are behind another in the
-        // navigation hierarchy (check Activity class documentation)
+        this.accessLevel = UserType.serviceProfessional;
+
         this.navBar = Activity.createSectionNavBar(null);
         this.navBar.rightAction(null);
-        // FIXME: REQUIRED: Define the activity title, used at window/tab
-        // and top title in the template
-        // This value can be a string (then is a constant value for all the life
-        // of the activity), an observable (dynamically changed depending
-        // on the route or user interaction), a pureComputed (dynamically
-        // changed depending on other activity observables --usually this is
-        // recommended over an observable since it keeps all possible values
-        // in one place)
+        /*
+        * Passes in the current user's ID as an observable.
+        */
+        this.userID = userProfile.data.userID;
+  
         this.title = 'Market yourself outside of Loconomics';
-    }
-
-    /**
-     * REMOVEME: If there is no parameters/state accepted by this activity,
-     * there is no need to document here the standard behavior and parameters for the
-     * show method, since usually is just copy&paste of base method and
-     * is redundant. But document with a jsdoc comment like this if some special
-     * behavior is worth note it and when specific state properties and values
-     * are expected.
-     * Replace this method only if need to execute some code when the activity
-     * is being displayed and to read/analyze incoming state parameters (routing
-     * internal to the activity, query-string parameters, communication between
-     * activities through state properties).
-     * Remove all this 'show' code if you don't need to do anything here
-     * @param {Object} state
-     */
-    show(state) {
-        super.show(state);
-        // Check other examples for some code using 'state'
     }
 }
 
--- app/source/js/kocomponents/external-platform/suggestions-list/index.js
+++ app/source/js/kocomponents/external-platform/suggestions-list/index.js
@@ -1,27 +1,18 @@
 /**
- * Example of a basic KnockoutComponent with styles, based on basicKomponent.
+ * A list of suggested external platforms for a 
+ * professional to list themselves on based on their job 
+ * titles. It lists only platforms they don't already have 
+ * an external listing created.
  *
- * @module kocomponents/_examples/c-styled-component
+ * @module kocomponents/external-platform/suggestions-list
  *
- * FIXME: Update this component description
- * FIXME: Document parameters allowed using jsdoc syntax in the constructor,
- * or if there is no one, at this initial commit
- * FIXME: Keep code, members, methods documented, using jsdoc and inline comments
- * so code keeps clear; but code that just overwrite an inherit member (like
- * template) does not need a comment except some additional thing should be
- * noted; same if some comment looks repeatitive or not helpfull (like the
- * register line).
  */
 
 import '../../utilities/icon-dec.js';
 import Komponent from '../../helpers/KnockoutComponent';
 import getObservable from '../../../utils/getObservable';
 import ko from 'knockout';
-
 import template from './template.html';
-// FIXME: If the component uses in the template other components, you need
-// to import them from here, like
-// import '../another/component';
 
 const TAG_NAME = 'external-platform-suggestions-list';
 const dummyData = {};
@@ -38,6 +29,7 @@ dummyData[540] =
     'ShortDescription': 'Marketplace for freelance gigs.'
   }
 ];
+
 /**
  * Component
  */
@@ -47,48 +39,32 @@ export default class ExternalPlatformSuggestionsList extends Komponent {
 
     /**
      * @param {object} params
-     * @param {(string|KnockoutObservable<string>)} [params.name=World] A name for the greating.
-     * @param {function<number,void>} [params.onCount] Callback executed each time the 'count'
-     * button is executed with the current counter.
+     * @param {(number|KnockoutObservable<number>)} [params.userID] The user's ID that enables us to query suggested platforms specifically for them.
      */
     constructor(params) {
         super();
 
         /**
-         * A name for the greating.
+         * The userID the suggestions list is created for.
          * @member {KnockoutObservable<string>}
          */
         this.userID = getObservable(params.userID || -1);
+
         /**
-         * Internal counter for how many times pressed the button
-         * @member {KnockoutObservable<number>}
+         * An array of the platforms suggested.
+         * @member {KnockoutObservable<array>}
          */
         this.suggestedPlatform = ko.observableArray();
-        /**
-         * Optional callback for external notifications on clicking 'count'
-         */
-        this.onCount = params.onCount || undefined;
 
-        // FIXME: A callback is usual to notify some event, but on this case
-        // we could allow the 'counter' being provided externally as an
-        // observable (like the 'name') and reset the number at constructor.
+        /**
+         * When the userID changes, the information is 
+         * updated for the specific user.
+         */
         this.observeChanges(() => {
             const data = dummyData[this.userID()];
             this.suggestedPlatform(data);
         });
     }
-
-    /**
-     * Increases the counter and notify through callback
-     */
-    count() {
-        this.counter(this.counter() + 1);
-        if (this.onCount) {
-            this.onCount(this.counter());
-        }
-    }
 }
 
-// FIXME: Just reminder that EVER should register the component with this line
-// at the end, but don't need a comment (remove me!)
 ko.components.register(TAG_NAME, ExternalPlatformSuggestionsList);
