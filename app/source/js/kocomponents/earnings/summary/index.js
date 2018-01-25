6b435d7f2916686eb9edd61852b8a275c66dddda
Joshua Danielson <joshua.danielson@loconomics.com>
2018-01-25 15:59:52 -0800

    Revised earnings-summary
    #780
    - updated documentation removing placeholder
    - changed dummy data to object from array

--- app/source/js/kocomponents/earnings/summary/index.js
+++ app/source/js/kocomponents/earnings/summary/index.js
@@ -1,31 +1,19 @@
 /**
- * Example of a basic Knockout Component that uses the helper KnockoutComponent
- * and ES6 class syntax to define it.
+ * Used to capture a summary of earnings for a given 
+ * set of parameters.
  *
  * @module kocomponents/_examples/b-basic-komponent
  *
- * FIXME: Update this component description
- * FIXME: Document parameters allowed using jsdoc syntax in the constructor,
- * or if there is no one, at this initial commit
- * FIXME: Keep code, members, methods documented, using jsdoc and inline comments
- * so code keeps clear; but code that just overwrite an inherit member (like
- * template) does not need a comment except some additional thing should be
- * noted; same if some comment looks repeatitive or not helpfull (like the
- * register line).
  */
 import Komponent from '../../helpers/KnockoutComponent';
 import getObservable from '../../../utils/getObservable';
 import ko from 'knockout';
 import template from './template.html';
-// FIXME: If the component uses in the template other components, you need
-// to import them from here, like
-// import '../another/component';
 
 const TAG_NAME = 'earnings-summary';
 const dummyData = {};
 dummyData[0] =
-[
-  {
+{
     'total': 23250.00,
     'period': '2017 Year-to-date',
     'jobTitle': 'All job titles',
@@ -35,8 +23,7 @@ dummyData[0] =
     'averageHourlyRate': 23.83,
     'hoursWorked': 975.50,
     'bookings': 57
-  }
-];
+};
 
 
 /**
@@ -75,40 +62,15 @@ export default class EarningsSummary extends Komponent {
 
         /**
          * Earnings summary returned given query parameters.
-         * @member {KnockoutObservable<string>}
+         * @member {KnockoutObservable<object>}
          */
-        this.earningsSummary = ko.observableArray();
+        this.earningsSummary = ko.observable();
 
-        /**
-         * Internal counter for how many times pressed the button
-         * @member {KnockoutObservable<number>}
-         */
-        this.counter = ko.observable(0);
-        /**
-         * Optional callback for external notifications on clicking 'count'
-         */
-        this.onCount = params.onCount || undefined;
-
-        // FIXME: A callback is usual to notify some event, but on this case
-        // we could allow the 'counter' being provided externally as an
-        // observable (like the 'name') and reset the number at constructor.
         this.observeChanges(() => {
             const data = dummyData[0];
             this.earningsSummary(data);
         });
     }
-
-    /**
-     * Increases the counter and notify through callback
-     */
-    count() {
-        this.counter(this.counter() + 1);
-        if (this.onCount) {
-            this.onCount(this.counter());
-        }
-    }
 }
 
-// FIXME: Just reminder that EVER should register the component with this line
-// at the end, but don't need a comment (remove me!)
 ko.components.register(TAG_NAME, EarningsSummary);
