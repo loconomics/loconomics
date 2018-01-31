5b17037862ff2e3aa7ad40d0450f65e207b08a45
Joshua Danielson <joshua.danielson@loconomics.com>
2018-01-30 16:28:46 -0800

    Revised earnings-edit activity
    #777
    - added earningsEntryID param
    - added backlink

--- app/source/js/activities/earnings-edit/index.js
+++ app/source/js/activities/earnings-edit/index.js
@@ -9,6 +9,7 @@ import '../../kocomponents/earnings/editor';
 import * as activities from '../index';
 import Activity from '../../components/Activity';
 import UserType from '../../enums/UserType';
+import ko from 'knockout';
 import template from './template.html';
 
 const ROUTE_NAME = 'earnings-edit';
@@ -23,12 +24,22 @@ export default class EarningsEditActivity extends Activity {
 
         this.accessLevel = UserType.serviceProfessional;
         this.navBar = Activity.createSubsectionNavBar(null);
+        /**
+         * Creates a placeholder for the external listing ID
+         * to be populated using the show(state) method below.
+         */
+        this.earningsEntryID = ko.observable();
         this.title = 'Edit earnings';
     }
 
     show(state) {
         super.show(state);
-        // Check other examples for some code using 'state'
+        var params = state.route && state.route.segments;
+        /**
+         * externalListingID is the first segment in the activity 
+         * URL.
+         */
+        this.earningsEntryID(params[0] |0);
     }
 }
 
