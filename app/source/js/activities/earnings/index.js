0d9b8cda692acc8317d2bd4e3b04ccc5a98026c4
Joshua Danielson <joshua.danielson@loconomics.com>
2018-01-30 16:38:49 -0800

    Revised earnings activity
    #777
    - repositioned “Add earnings” button
    - added placeholder for filters

--- app/source/js/activities/earnings/index.js
+++ app/source/js/activities/earnings/index.js
@@ -11,6 +11,7 @@ import '../../kocomponents/earnings/summary';
 import * as activities from '../index';
 import Activity from '../../components/Activity';
 import UserType from '../../enums/UserType';
+import ko from 'knockout';
 import template from './template.html';
 
 const ROUTE_NAME = 'earnings';
@@ -20,12 +21,31 @@ export default class EarningsActivity extends Activity {
     static get template() { return template; }
 
     constructor($activity, app) {
+        /* eslint max-statements:"off" */
 
         super($activity, app);
 
         this.accessLevel = UserType.serviceProfessional;
         this.navBar = Activity.createSectionNavBar(null);
         this.navBar.rightAction(null);
+
+
+        /**
+         * Earnings summary returned given query parameters.
+         * @member {KnockoutObservable<boolean>}
+         */
+        this.showFilter = ko.observable(false);
+
+        this.showFilters = function() {
+            if (this.showFilter() == false) {
+                this.showFilter(true);
+            }
+            else {
+                this.showFilter(false);
+            }
+        }; 
+
+
         this.title = 'Earnings';
     }
 }
