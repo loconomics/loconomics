989f5873b6216d24929c417c73a57c215028f7f5
Joshua Danielson <joshua.danielson@loconomics.com>
2018-02-01 07:02:38 -0800

    Added userID to earnings-copy activity
    #778

--- app/source/js/activities/earnings-copy/index.js
+++ app/source/js/activities/earnings-copy/index.js
@@ -12,6 +12,7 @@ import * as activities from '../index';
 import Activity from '../../components/Activity';
 import UserType from '../../enums/UserType';
 import template from './template.html';
+import userProfile from '../../data/userProfile';
 
 const ROUTE_NAME = 'earnings-copy';
 
@@ -22,6 +23,10 @@ export default class EarningsCopyActivity extends Activity {
     constructor($activity, app) {
 
         super($activity, app);
+        /**
+         * Passes in the current user's ID as an observable.
+         */
+        this.userID = userProfile.data.userID;
 
         this.accessLevel = UserType.serviceProfessional;
         this.navBar = Activity.createSubsectionNavBar(null);
