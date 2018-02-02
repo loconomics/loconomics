23673dfde9b81f77f7d1467b31924cc9d6994cef
IagoSRL <iagosrl@gmail.com>
2018-02-02 20:45:57 +0100

    Updated platforms data module based on server changes #764

--- app/source/js/data/platforms.js
+++ app/source/js/data/platforms.js
@@ -1,5 +1,6 @@
 /**
- * Access to the list of known external platforms for GIGs.
+ * Access to the list of known platforms for GIGs, based on ones available to
+ * the user depending on its listing/job-titles.
  */
 
 import CachedDataProvider from './helpers/CachedDataProvider';
@@ -10,35 +11,36 @@ import RestSingleDataProviderDriver from './helpers/RestSingleDataProviderDriver
 import localforage from './drivers/localforage';
 import rest from './drivers/restClient';
 
-const API_NAME = 'external-platforms';
+const API_NAME = 'me/platforms';
+const LOCAL_KEY = 'platforms';
 
 /**
- * Provides access to the list of all external platforms available.
- * @returns {CachedDataProvider<Array<PlainExternalPlatform>>}
+ * Provides access to the list of all platforms available.
+ * @returns {CachedDataProvider<Array<api/Platform>>}
  * Usage:
  * - list.onData.subscribe(fn) to get the list, fn keeps being triggered on incoming updated data
  * - list.onLoadError.subscribe(fn) to get notified of errors happening as of onData
  */
 export const list = new CachedDataProvider({
-    // 1 hour
-    ttl: 1 * 60 * 60 * 1000,
+    // 10 minutes
+    ttl: 10 * 60 * 1000,
     remote: new RestSingleDataProviderDriver(rest, API_NAME),
-    local: new LocalForageIndexedListDataProviderDriver(localforage, API_NAME, 'externalPlatformID')
+    local: new LocalForageIndexedListDataProviderDriver(localforage, LOCAL_KEY, 'platformID')
 });
 
 /**
  * Provides access to an API to fetch a specific platform data.
- * @param {number} id The externalPlatformID
- * @returns {CachedDataProvider<PlainExternalPlatform>}
+ * @param {number} id The platformID
+ * @returns {CachedDataProvider<api/Platform>}
  * Usage:
- * - item(externalPlatformID).onData.subscribe(fn) to get the list, fn keeps being triggered on incoming updated data
- * - item(externalPlatformID).onLoadError.subscribe(fn) to get notified of errors happening as of onData
+ * - item(platformID).onData.subscribe(fn) to get the list, fn keeps being triggered on incoming updated data
+ * - item(platformID).onLoadError.subscribe(fn) to get notified of errors happening as of onData
  */
 export function item(id) {
     return new CachedDataProvider({
-        // 1 hour
-        ttl: 1 * 60 * 60 * 1000,
+        // 10 minutes
+        ttl: 10 * 60 * 1000,
         remote: new RestItemDataProviderDriver(rest, API_NAME, id),
-        local: new LocalForageItemDataProviderDriver(localforage, API_NAME, id)
+        local: new LocalForageItemDataProviderDriver(localforage, LOCAL_KEY, id)
     });
 }
