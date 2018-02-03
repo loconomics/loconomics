32d6022ba164db01fe3ec4fb77fc745178bfb70b
IagoSRL <iagosrl@gmail.com>
2018-02-03 15:22:01 +0100

    Connected external-platforms/info to data provider #764; updated template field names and building link safely

--- app/source/js/kocomponents/external-platform/info/index.js
+++ app/source/js/kocomponents/external-platform/info/index.js
@@ -1,6 +1,6 @@
 /**
- * Shows the information about the external platform to the 
- * professional to help them decide if they should list 
+ * Shows the information about the external platform to the
+ * professional to help them decide if they should list
  * themselves.
  *
  * @module kocomponents/external-platform/info
@@ -11,33 +11,12 @@ import '../../utilities/icon-dec.js';
 import Komponent from '../../helpers/KnockoutComponent';
 import getObservable from '../../../utils/getObservable';
 import ko from 'knockout';
+import { item as platformsItem } from '../../../data/platforms';
+import { show as showError } from '../../../modals/error';
 
 import template from './template.html';
 
 const TAG_NAME = 'external-platform-info';
-const dummyData = {};
-dummyData[1] ={
-    'PlatformID': 1,
-    'PlatformName': '99designs',
-    'ShortDescription': 'Marketplace for freelance designers.',
-    'LongDescription': 'Hi there. We’re 99designs, the world’s largest online graphic design marketplace. We connect more than one million talented freelance designers with creative people, genius entrepreneurs, savvy businesses… anyone who needs great work.',
-    'FeesDescription': '-$0 sign-up fee↵-20% commission if design chosen',
-    'PositiveAspects': '-Global demand',
-    'NegativeAspects': '-Zero pay if design not chosen↵-High commissions if chosen',
-    'Advice': '-Enter many contests to build a reputation↵-Repurpose designs to multiple clients if they fit the criteria',
-    'SignUpURL': 'https://99designs.com/designers'
-};
-dummyData[2] ={
-      'PlatformID': 2,
-      'PlatformName': 'TaskRabbit',
-      'ShortDescription': 'Marketplace for tasks.',
-      'LongDescription': 'TaskRabbit connects you with gigs that focus mainly around laborious chores',
-      'FeesDescription': '-$0 sign-up fee↵-20% commission if design chosen',
-      'PositiveAspects': '-Global demand',
-      'NegativeAspects': '-Zero pay if design not chosen↵-High commissions if chosen',
-      'Advice': '-Enter many contests to build a reputation↵-Repurpose designs to multiple clients if they fit the criteria',
-      'SignUpURL': 'https://www.taskrabbit.com/become-a-tasker'
-};
 
 /**
  * Component
@@ -48,12 +27,9 @@ export default class ExternalPlatformInfo extends Komponent {
 
     /**
      * @param {object} params
-     * @param {(string|KnockoutObservable<string>)} 
-     * [params.platformName]
-     * @param {(number|KnockoutObservable<number>)} 
-     * [params.platformID]
-     * @param {(boolean|KnockoutObservable<boolean>)} 
-     * [params.showAddListing]
+     * @param {(string|KnockoutObservable<string>)} [params.platformName]
+     * @param {(number|KnockoutObservable<number>)}  [params.platformID]
+     * @param {(boolean|KnockoutObservable<boolean>)} [params.showAddListing]
      **/
     constructor(params) {
         super();
@@ -65,48 +41,92 @@ export default class ExternalPlatformInfo extends Komponent {
          */
         this.platformID = getObservable(params.platformID);
 
-        /** 
+        /**
          * Determines whether or not to display the add link.
          * @member {KnockoutObservable<boolean>}
          */
         this.hideAddListing = getObservable(params.hideAddListing || false);
 
         /**
-         * An object containging information about the external 
+         * An object containging information about the external
          * platform.
          * @member {KnockoutObservable<object>}
          */
         this.externalPlatform = ko.observable();
-        
+
         /**
-         * An "out" parameter that fills the platform name 
+         * An "out" parameter that fills the platform name
          * for use in the title of the activity.
          * @member {KnockoutObservable<string>}
          */
         this.platformName = params.platformName;
 
         /**
-         * When the platformID changes, the information is 
+         * Creates a link to the 'add external listing' activity for current
+         * platform, setting back link to here with proper label
+         * @member {KnockoutComputed<string>}
+         */
+        this.linkToAdd = ko.pureComputed(() => {
+            const platform = this.externalPlatform();
+            if (platform) {
+                const text = encodeURIComponent(`${platform.platformName} info`);
+                return `/external-listing-add/${platform.platformID}?mustReturn=external-platform-view/'${platform.platformID}&returnText=${text}`;
+            }
+            else {
+                return '';
+            }
+        });
+
+        /**
+         * Holds a subscription to updates about data for a specific platform
+         * @private {SingleEvent/Subscription}
+         */
+        let dataSubscription;
+        /**
+         * Holds a subscription to error notifications load data for a specific platform
+         * @private {SingleEvent/Subscription}
+         */
+        let dataErrorSubscription;
+        /**
+         * Reset current data displayed and remove previous subscriptions
+         * to data updates.
+         * Useful when the ID changes, in order to prevent displaying previous ID
+         * data and stop receiving notifications for that.
+         * @private
+         * @method
+         */
+        const resetData = () => {
+            this.externalPlatform(null);
+            if (dataSubscription) {
+                dataSubscription.dispose();
+            }
+            if (dataErrorSubscription) {
+                dataErrorSubscription.dispose();
+            }
+        };
+
+        /**
+         * When the platformID changes, the information is
          * updated for the specific platform.
          */
-        // ASKIAGO this piece is throwing a console error and not returning data in the external-listing-view activity (but does in external-platform-view)
-        // index.js:98 Uncaught TypeError: Cannot read property 'PlatformName' of undefined
-        // at ExternalPlatformInfo.<anonymous> (index.js:98)
-        // at Function.evaluateImmediate_CallReadThenEndDependencyDetection (knockout-latest.debug.js:2183)
-        // at Function.evaluateImmediate_CallReadWithDependencyDetection (knockout-latest.debug.js:2150)
-        // at Function.evaluateImmediate (knockout-latest.debug.js:2111)
-        // at Object.ko.computed.ko.dependentObservable (knockout-latest.debug.js:1964)
-        // at ExternalPlatformInfo.observeChanges (KnockoutComponent.js:210)
-        // at new ExternalPlatformInfo (index.js:93)
-        // at Function.from (KnockoutComponent.js:234)
-        // at createViewModel (knockout-latest.debug.js:4035)
-        // at ko.subscription.callback (knockout-latest.debug.js:4006)
         this.observeChanges(() => {
             const id = this.platformID();
+            // reset data and previous ID notifications
+            resetData();
             if (id) {
-                const data = dummyData[id];
-                this.externalPlatform(data);
-                this.platformName(data.PlatformName);
+                const item = platformsItem(id);
+                // Load platform data
+                dataSubscription = this.subscribeTo(item.onData, (data) => {
+                    this.externalPlatform(data);
+                    this.platformName(data.PlatformName);
+                });
+                // Notify data load errors
+                dataErrorSubscription = this.subscribeTo(item.onDataError, (err) => {
+                    showError({
+                        title: 'There was an error loading the platform info',
+                        error: err
+                    });
+                });
             }
         });
     }
