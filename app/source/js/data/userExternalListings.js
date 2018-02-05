3102b9df39dc4fd6205c90d84948b1591e50c205
IagoSRL <iagosrl@gmail.com>
2018-02-05 03:19:19 +0100

    Fixed sync problems at data module userExternalListing, changes on item, like adding new one, gets not reflected in the list that showed outdated; this change enable that and other changes/updates to get notified items->list; notifications from list to items is commented cause of memory leak and no real use case now #764

--- app/source/js/data/userExternalListings.js
+++ app/source/js/data/userExternalListings.js
@@ -12,6 +12,9 @@ import rest from './drivers/restClient';
 
 const API_NAME = 'me/external-listings';
 const LOCAL_KEY = 'external-listings';
+const ID_PROPERTY_NAME = 'userExternalListingID';
+
+const localListDriver = new LocalForageIndexedListDataProviderDriver(localforage, LOCAL_KEY, ID_PROPERTY_NAME);
 
 /**
  * Provides access to the list of all external listings.
@@ -24,7 +27,7 @@ export const list = new CachedDataProvider({
     // 1 minute
     ttl: 1 * 60 * 1000,
     remote: new RestSingleDataProviderDriver(rest, API_NAME),
-    local: new LocalForageIndexedListDataProviderDriver(localforage, LOCAL_KEY, 'userExternalListingID')
+    local: localListDriver
 });
 
 /**
@@ -36,10 +39,62 @@ export const list = new CachedDataProvider({
  * - item(platformID).onLoadError.subscribe(fn) to get notified of errors happening as of onData
  */
 export function item(id) {
-    return new CachedDataProvider({
+    const localItemDriver = new LocalForageItemDataProviderDriver(localforage, LOCAL_KEY, id, ID_PROPERTY_NAME);
+    const itemProvider = new CachedDataProvider({
         // 1 minutes
         ttl: 1 * 60 * 1000,
         remote: new RestItemDataProviderDriver(rest, API_NAME, id),
-        local: new LocalForageItemDataProviderDriver(localforage, LOCAL_KEY, id)
+        local: localItemDriver
     });
+    const notifyList = function() {
+        // If someone subscribed to new list data
+        if (list.onLoaded.count) {
+            // We need to notify them, but we need the full list
+            // and we just have an item, load it from cache and provide it
+            localListDriver.fetch().then((cache) => list.onLoaded.emit(cache.data));
+        }
+    };
+    // Keep list notified because of item updates
+    itemProvider.onRemoteLoaded.subscribe((itemData) => {
+        // ensure the fresh item is registered in the index (then, included in the list)
+        localListDriver.registerID(itemData[ID_PROPERTY_NAME]);
+        notifyList();
+    });
+    itemProvider.onDeleted.subscribe((itemData) => {
+        // the item needs to be removed from the index
+        localListDriver.unregisterID(itemData[ID_PROPERTY_NAME]);
+        notifyList();
+    });
+    itemProvider.onSaved.subscribe((itemData) => {
+        // ensure the item (maybe new) is registered in the index (then, included in the list)
+        localListDriver.registerID(itemData[ID_PROPERTY_NAME]);
+        notifyList();
+    });
+
+    /* In theory, if an updated load of the list happens in the meantime with
+        an item() in use, we must notify the item of that new data.
+        It's very strange for this to happens because of use cases, but in theory can happens.
+        Next commented code can do that, BUT it will leak memory if we don't
+        add an explicit disposal of the subscription when 'itemProvider' is
+        not used anymore. With subscriptions in previous lines don't happens
+        because are done to the own instance, while this subscription is done on the list
+        and inside it holds a reference to 'itemProvider', preventing it from GC'ed.
+
+    list.onRemoteLoaded.subscribe((list) => {
+        const found = list.some((item) => {
+            if (item[ID_PROPERTY_NAME] === id) {
+                itemProvider.onLoaded.emit(item);
+                return true;
+            }
+        });
+        // If not found in updated list, means was deleted in the middle, notify
+        // (actual deletion of local data happens already as part of the list
+        // synching process, before of this).
+        if (!found) {
+            itemProvider.onDeleted.emit();
+        }
+    });
+    */
+    // Return the instance
+    return itemProvider;
 }
