5df70cf2fb5d4cc7faccfac204543a220c991af4
IagoSRL <iagosrl@gmail.com>
2018-02-05 18:39:52 +0100

    Filtering platforms as suggestedPlatforms locally by comparing ones that already have an external listing #764

--- app/source/js/kocomponents/external-platform/suggestions-list/index.js
+++ app/source/js/kocomponents/external-platform/suggestions-list/index.js
@@ -13,6 +13,7 @@ import ko from 'knockout';
 import { list as platformsList } from '../../../data/platforms';
 import { show as showError } from '../../../modals/error';
 import template from './template.html';
+import { list as userExternalListingList } from '../../../data/userExternalListings';
 
 const TAG_NAME = 'external-platform-suggestions-list';
 
@@ -27,25 +28,55 @@ export default class ExternalPlatformSuggestionsList extends Komponent {
         super();
 
         /**
-         * An array of the platforms suggested.
+         * List of all platforms available.
          * @member {KnockoutObservable<Array<rest/Platform>>}
          */
-        this.suggestedPlatforms = ko.observableArray();
+        this.platforms = ko.observableArray();
 
         /**
-         * Load the suggested platforms data.
+         * List of external listings the user has. Used to filter out
+         * the suggested list of platforms
+         * @member {KnockoutObservable<Array<rest/UserExternalListing>>}
          */
-        this.subscribeTo(platformsList.onData, this.suggestedPlatforms);
+        this.externalListings = ko.observableArray();
 
         /**
-         * Notify data load errors
+         * List of platforms suggested.
+         * @member {KnockoutComputed<Array<rest/Platform>>}
          */
-        this.subscribeTo(platformsList.onDataError, (err) => {
+        this.suggestedPlatforms = ko.pureComputed(() => {
+            const platforms = this.platforms();
+            const listings = this.externalListings();
+            if (platforms && platforms.length && listings) {
+                // Filter platforms by one already registered as a listing
+                const platformsAdded = listings.map((l) => l.platformID);
+                return platforms.filter((platform) => platformsAdded.indexOf(platform.platformID) === -1);
+            }
+            else {
+                // No platforms
+                return [];
+            }
+        });
+
+        /**
+         * Load platforms data.
+         */
+        this.subscribeTo(platformsList.onData, this.platforms);
+
+        /**
+         * Load listing data.
+         */
+        this.subscribeTo(userExternalListingList.onData, this.externalListings);
+
+        /// Notify data load errors
+        const notifyError = (err) => {
             showError({
                 title: 'There was an error loading the platforms',
                 error: err
             });
-        });
+        };
+        this.subscribeTo(platformsList.onDataError, notifyError);
+        this.subscribeTo(userExternalListingList.onDataError, notifyError);
     }
 }
 
