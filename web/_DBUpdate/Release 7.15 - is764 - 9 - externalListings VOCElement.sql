6c818069cdc7c20d6f4ecd8f309b9750d4bcd2c3
IagoSRL <iagosrl@gmail.com>
2018-02-06 11:49:03 +0100

    Removed already applied sql scripts (released)

--- web/_DBUpdate/Release 7.15 - is764 - 9 - externalListings VOCElement.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO dbo.VOCElement (VOCElementID, LanguageID, CountryID, VOCElementName, ScoreStartValue, ScoreMidValue, ScoreEndValue, ScoreStartLabel, ScoreMidLabel, ScoreEndLabel, CreateDate, UpdatedDate, ModifiedBy, Active) VALUES (21, 1, 1, 'External Listings', null, null, null, null, null, null, '2018-01-25 00:00:00.0', '2018-01-25 00:00:00.0', 'jd', 1);
