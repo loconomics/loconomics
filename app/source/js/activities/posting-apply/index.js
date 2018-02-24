15949223f9d50c6c7bdddb40adc5028d3abe8c73
Joshua Danielson <joshua.danielson@loconomics.com>
2018-02-24 10:52:27 -0800

    Create initial file structure
    #824

---
+++ app/source/js/activities/posting-apply/index.js
@@ -0,0 +1,53 @@
+/**
+ * Allows a client to add a posting.
+ *
+ * @module activities/posting-add
+ *
+ */
+
+import '../../kocomponents/earnings/editor';
+import * as activities from '../index';
+import Activity from '../../components/Activity';
+import UserType from '../../enums/UserType';
+import ko from 'knockout';
+import template from './template.html';
+
+const ROUTE_NAME = 'posting-add';
+
+export default class PostingAddActivity extends Activity {
+
+    static get template() { return template; }
+
+    constructor($activity, app) {
+
+        super($activity, app);
+
+        this.accessLevel = UserType.serviceProfessional;
+        this.navBar = Activity.createSubsectionNavBar(null);
+        this.title = 'Add posting';
+        /**
+         * Creates a placeholder for the external listing ID
+         * to be populated using the show(state) method below.
+         */
+        this.userPostingID = ko.observable(null);
+
+        /**
+         * After data being saved, notice and go back
+         */
+        this.onSaved = () => {
+            app.successSave();
+        };
+    }
+
+    show(state) {
+        super.show(state);
+        var params = state.route && state.route.segments;
+        /**
+         * userPostingID as the first segment in the activity URL,
+         * allowing to preset that value in the new earnings entry.
+         */
+        this.userPostingID(params[0] |0);
+    }
+}
+
+activities.register(ROUTE_NAME, PostingAddActivity);
