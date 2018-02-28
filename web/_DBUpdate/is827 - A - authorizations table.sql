5b50bd9a51e44619512b5a9a6b25745c727725d2
IagoSRL <iagosrl@gmail.com>
2018-02-28 20:05:39 +0100

    Updated authorization table scheme to simplify work with LoopBack on new back-end #827

--- web/_DBUpdate/is827 - A - authorizations table.sql
+++ web/_DBUpdate/is827 - A - authorizations table.sql
@@ -4,11 +4,18 @@ GO
 
 CREATE TABLE dbo.authorizations
 (
-	Token varchar(216) PRIMARY KEY,
+	AuthorizationID int NOT NULL IDENTITY (1, 1) PRIMARY KEY,
+	Token varchar(216) ,
 	UserID int NOT NULL,
 	Scope varchar(100),
 	CreatedDate datetimeoffset(0),
 	ClientAddress varchar(64),
 	UserAgent text
 )
+GO
+
+CREATE UNIQUE NONCLUSTERED INDEX IX_authorizations_token ON dbo.authorizations
+	(
+	Token
+	) WITH( STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 GO
