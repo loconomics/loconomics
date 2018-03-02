60e45f799d0767e762eaae0dea9cf5d0a55eaf8d
IagoSRL <iagosrl@gmail.com>
2018-03-02 21:21:28 +0100

    Adds position -2 to database #803 fixes #805

---
+++ web/_DBUpdate/is803 - 3 - position -2 to link to user generated titles.sql
@@ -0,0 +1,57 @@
+INSERT INTO [positions]
+           ([PositionID]
+           ,[LanguageID]
+           ,[CountryID]
+           ,[PositionSingular]
+           ,[PositionPlural]
+           ,[Aliases]
+           ,[PositionDescription]
+           ,[CreatedDate]
+           ,[UpdatedDate]
+           ,[ModifiedBy]
+           ,[GovID]
+           ,[GovPosition]
+           ,[GovPositionDescription]
+           ,[Active]
+           ,[DisplayRank]
+           ,[PositionSearchDescription]
+           ,[AttributesComplete]
+           ,[StarRatingsComplete]
+           ,[PricingTypeComplete]
+           ,[EnteredByUserID]
+           ,[Approved]
+           ,[AddGratuity]
+           ,[HIPAA]
+           ,[SendReviewReminderToClient]
+           ,[CanBeRemote]
+           ,[SuppressReviewOfClient])
+     VALUES
+           (-2
+           ,1
+           ,1
+           ,'User Generated'
+           ,'User Generated'
+           ,null
+           ,''
+           ,'2018-03-02 20:36:00'
+           ,'2018-03-02 20:36:00'
+           ,'il'
+           ,null
+           ,null
+           ,null
+           ,1
+           ,null
+           ,null
+           ,0
+           ,0
+           ,0
+           ,null
+           ,1
+           ,0
+           ,1
+           ,1
+           ,1
+           ,0)
+
+
+
