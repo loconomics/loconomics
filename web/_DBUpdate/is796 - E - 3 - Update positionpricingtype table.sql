c7a9f9299ee2b5858352d65098f6320e41bdfeac
IagoSRL <iagosrl@gmail.com>
2018-03-07 18:44:29 +0100

    Renamed epic #796 SQL scripts, minor changes (position -2 set-up removed, it collides with another branch change)

--- web/_DBUpdate/is796 - E - 3 - Update positionpricingtype table.sql
+++ web/_DBUpdate/is796 - E - 3 - Update positionpricingtype table.sql
@@ -617,6 +617,4 @@ INSERT INTO dbo.positionpricingtype (PositionID,PricingTypeID,ClientTypeID,Langu
 INSERT INTO dbo.positionpricingtype (PositionID,PricingTypeID,ClientTypeID,LanguageID,CountryID,CreatedDate,UpdatedDate,ModifiedBy,Active) VALUES (1205,6,1,1,1,'2018-03-05 11:05:00.000','2018-03-05 11:05:00.000','jd',1);
 INSERT INTO dbo.positionpricingtype (PositionID,PricingTypeID,ClientTypeID,LanguageID,CountryID,CreatedDate,UpdatedDate,ModifiedBy,Active) VALUES (1206,6,1,1,1,'2018-03-05 11:05:00.000','2018-03-05 11:05:00.000','jd',1);
 INSERT INTO dbo.positionpricingtype (PositionID,PricingTypeID,ClientTypeID,LanguageID,CountryID,CreatedDate,UpdatedDate,ModifiedBy,Active) VALUES (1207,6,1,1,1,'2018-03-05 11:05:00.000','2018-03-05 11:05:00.000','jd',1);
-INSERT INTO dbo.positionpricingtype (PositionID,PricingTypeID,ClientTypeID,LanguageID,CountryID,CreatedDate,UpdatedDate,ModifiedBy,Active) VALUES (-2,5,1,1,1,'2018-03-05 11:05:00.000','2018-03-05 11:05:00.000','jd',1);
-INSERT INTO dbo.positionpricingtype (PositionID,PricingTypeID,ClientTypeID,LanguageID,CountryID,CreatedDate,UpdatedDate,ModifiedBy,Active) VALUES (-2,6,1,1,1,'2018-03-05 11:05:00.000','2018-03-05 11:05:00.000','jd',1);
 
