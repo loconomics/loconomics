bcc6ec7bade156f3cbe42b797cd2ee069de17b9a
IagoSRL <iagosrl@gmail.com>
2018-03-07 01:03:58 +0100

    Updated how list is updated after an item change, at userListings #803

--- app/source/js/data/userListings.js
+++ app/source/js/data/userListings.js
@@ -56,55 +56,31 @@ export function item(id) {
         remote: new RestItemDataProviderDriver(rest, API_NAME, id),
         local: localItemDriver
     });
-    const notifyList = function() {
-        // If someone subscribed to new list data
+    // Keep list notified because of item updates
+    itemProvider.onRemoteLoaded.subscribe((itemData) => {
+        // ensure the fresh item is registered in the index (then, included in the list)
+        localListDriver.registerID(itemData[ID_PROPERTY_NAME]);
+        localListDriver.invalidateCache();
+    });
+    itemProvider.onSaved.subscribe((itemData) => {
+        // ensure the item (maybe new) is registered in the index (then, included in the list)
+        localListDriver.registerID(itemData[ID_PROPERTY_NAME]);
+        localListDriver.invalidateCache();
+    });
+    itemProvider.onDeleted.subscribe((itemData) => {
+        // the item needs to be removed from the index
+        localListDriver.unregisterID(itemData[ID_PROPERTY_NAME]);
+        // On delete, the current list without the item is OK and don't require list revalidation
+        // but needs notification to anyone listening to data changes:
         if (list.onLoaded.count) {
             // We need to notify them, but we need the full list
             // and we just have an item, load it from cache and provide it
             localListDriver.fetch().then((cache) => list.onLoaded.emit(cache.data));
         }
-    };
-    // Keep list notified because of item updates
-    itemProvider.onRemoteLoaded.subscribe((itemData) => {
-        // ensure the fresh item is registered in the index (then, included in the list)
-        localListDriver.registerID(itemData[ID_PROPERTY_NAME]);
-        notifyList();
-    });
-    itemProvider.onDeleted.subscribe((itemData) => {
-        // the item needs to be removed from the index
-        localListDriver.unregisterID(itemData[ID_PROPERTY_NAME]);
-        notifyList();
-    });
-    itemProvider.onSaved.subscribe((itemData) => {
-        // ensure the item (maybe new) is registered in the index (then, included in the list)
-        localListDriver.registerID(itemData[ID_PROPERTY_NAME]);
-        notifyList();
     });
 
-    /* In theory, if an updated load of the list happens in the meantime with
-        an item() in use, we must notify the item of that new data.
-        It's very strange for this to happens because of use cases, but in theory can happens.
-        Next commented code can do that, BUT it will leak memory if we don't
-        add an explicit disposal of the subscription when 'itemProvider' is
-        not used anymore. With subscriptions in previous lines don't happens
-        because are done to the own instance, while this subscription is done on the list
-        and inside it holds a reference to 'itemProvider', preventing it from GC'ed.
+    /* See note at userExternalListings at this point about list.onRemoteLoaded */
 
-    list.onRemoteLoaded.subscribe((list) => {
-        const found = list.some((item) => {
-            if (item[ID_PROPERTY_NAME] === id) {
-                itemProvider.onLoaded.emit(item);
-                return true;
-            }
-        });
-        // If not found in updated list, means was deleted in the middle, notify
-        // (actual deletion of local data happens already as part of the list
-        // synching process, before of this).
-        if (!found) {
-            itemProvider.onDeleted.emit();
-        }
-    });
-    */
     // Return the instance
     return itemProvider;
 }
