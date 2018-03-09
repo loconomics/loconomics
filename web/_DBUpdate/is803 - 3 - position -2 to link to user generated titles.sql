e71b98bee70a00c28d4483bb8d7956bc9bb8c0d3
IagoSRL <iagosrl@gmail.com>
2018-03-09 18:14:37 +0100

    Updated script for -2 job title, with pricing types #803

--- web/_DBUpdate/is803 - 3 - position -2 to link to user generated titles.sql
+++ web/_DBUpdate/is803 - 3 - position -2 to link to user generated titles.sql
@@ -31,7 +31,7 @@ INSERT INTO [positions]
            ,1
            ,'User Generated'
            ,'User Generated'
-           ,null
+           ,''
            ,''
            ,'2018-03-02 20:36:00'
            ,'2018-03-02 20:36:00'
@@ -49,9 +49,9 @@ INSERT INTO [positions]
            ,1
            ,0
            ,1
-           ,1
+           ,0
            ,1
            ,0)
-
-
-
+;
+INSERT INTO dbo.positionpricingtype (PositionID,PricingTypeID,ClientTypeID,LanguageID,CountryID,CreatedDate,UpdatedDate,ModifiedBy,Active) VALUES (-2,5,1,1,1,'2018-03-05 11:05:00.000','2018-03-05 11:05:00.000','jd',1);
+INSERT INTO dbo.positionpricingtype (PositionID,PricingTypeID,ClientTypeID,LanguageID,CountryID,CreatedDate,UpdatedDate,ModifiedBy,Active) VALUES (-2,6,1,1,1,'2018-03-05 11:05:00.000','2018-03-05 11:05:00.000','jd',1);
