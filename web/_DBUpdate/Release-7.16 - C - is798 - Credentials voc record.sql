3874bd13c5aa0bcf4bcd99ea4147e6b03fd63376
IagoSRL <iagosrl@gmail.com>
2018-03-09 14:20:48 +0100

    Removed old release SQL scripts

--- web/_DBUpdate/Release-7.16 - C - is798 - Credentials voc record.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO dbo.VOCElement (VOCElementID, LanguageID, CountryID, VOCElementName, ScoreStartValue, ScoreMidValue, ScoreEndValue, ScoreStartLabel, ScoreMidLabel, ScoreEndLabel, CreateDate, UpdatedDate, ModifiedBy, Active) VALUES (23, 1, 1, 'Credentials', null, null, null, null, null, null, '2018-01-25 00:00:00.0', '2018-01-25 00:00:00.0', 'jd', 1);
