c59f18338951f68ebbb2ab059bcd1eaea08491fc
Joshua Danielson <joshua.danielson@loconomics.com>
2018-03-13 14:43:39 -0700

    Updated UserBadge Table SQL
    #746

--- web/_DBUpdate/is746 - 1 - Create UserBadge table.sql
+++ web/_DBUpdate/is746 - 1 - Create UserBadge table.sql
@@ -1,6 +1,7 @@
-CREATE TABLE dbo.UserListingBadge
-(UserID int NOT NULL,
-UserListingID int NOT NULL,
+CREATE TABLE dbo.UserBadge
+(UserBadgeID int NOT NULL,
+UserID int NOT NULL,
+SolutionID int NOT NULL,
 BadgeURL varchar(2078) NOT NULL,
 Type varchar(25) NOT NULL,
 Category varchar(25) NOT NULL,
@@ -9,4 +10,4 @@ CreatedDate datetime NOT NULL,
 ModifiedDate datetime NOT NULL,
 CreatedBy nvarchar(12) NOT NULL,
 Active bit DEFAULT 1 NOT NULL,
-PRIMARY KEY (UserID, UserListingID))
+PRIMARY KEY (UserBadgeID))
