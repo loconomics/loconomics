8af6ade7319bd3ea255f4f80362e1668acbaaa0d
IagoSRL <iagosrl@gmail.com>
2018-03-16 13:20:16 +0100

    Renamed SQL upgrade scripts for Release 7.17

--- web/_DBUpdate/Release-7.17 - A is803 - 3 - position -2 to link to user generated titles.sql
+++ web/_DBUpdate/Release-7.17 - A is803 - 3 - position -2 to link to user generated titles.sql
@@ -23,8 +23,7 @@ INSERT INTO [positions]
            ,[AddGratuity]
            ,[HIPAA]
            ,[SendReviewReminderToClient]
-           ,[CanBeRemote]
-           ,[SuppressReviewOfClient])
+           ,[CanBeRemote])
      VALUES
            (-2
            ,1
@@ -50,8 +49,7 @@ INSERT INTO [positions]
            ,0
            ,1
            ,0
-           ,1
-           ,0)
+           ,1)
 ;
 INSERT INTO dbo.positionpricingtype (PositionID,PricingTypeID,ClientTypeID,LanguageID,CountryID,CreatedDate,UpdatedDate,ModifiedBy,Active) VALUES (-2,5,1,1,1,'2018-03-05 11:05:00.000','2018-03-05 11:05:00.000','jd',1);
 INSERT INTO dbo.positionpricingtype (PositionID,PricingTypeID,ClientTypeID,LanguageID,CountryID,CreatedDate,UpdatedDate,ModifiedBy,Active) VALUES (-2,6,1,1,1,'2018-03-05 11:05:00.000','2018-03-05 11:05:00.000','jd',1);
