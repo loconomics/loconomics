af1960bfee73a4e62fc1ed0fa3db099ffed6c94a
IagoSRL <iagosrl@gmail.com>
2018-03-20 19:24:31 +0100

    Refactored latest SQL scripts, fixing problems creating UserBadge, alterning UserListingSolution (is re-created properly from scractch) #796 #746

--- web/_DBUpdate/is796 - C - 1 - Create UserListingSolution table.sql
+++ web/_DBUpdate/is796 - C - 1 - Create UserListingSolution table.sql
@@ -1,5 +1,7 @@
-CREATE TABLE dbo.UserListingSolution
-(UserID int NOT NULL,
+CREATE TABLE dbo.UserSolution
+(
+UserSolutionID int NOT NULL IDENTITY (1, 1),
+UserID int NOT NULL,
 UserListingID int NOT NULL,
 SolutionID int NOT NULL,
 LanguageID int NOT NULL,
@@ -9,4 +11,36 @@ CreatedDate datetimeoffset NOT NULL,
 UpdatedDate datetimeoffset NOT NULL,
 ModifiedBy nvarchar(4) DEFAULT 'sys' NOT NULL,
 Active bit DEFAULT 1 NOT NULL,
-PRIMARY KEY (UserID,UserListingID,SolutionID,LanguageID, CountryID));
+CONSTRAINT PK_UserSolution PRIMARY KEY (UserSolutionID),
+CONSTRAINT
+	FK_UserSolution_users FOREIGN KEY
+	(
+	UserID
+	) REFERENCES dbo.users
+	(
+	UserID
+	) ON UPDATE  NO ACTION 
+	 ON DELETE  NO ACTION,
+CONSTRAINT
+	FK_UserSolution_Solution FOREIGN KEY
+	(
+	SolutionID,
+	LanguageID,
+	CountryID
+	) REFERENCES dbo.Solution
+	(
+	SolutionID,
+	LanguageID,
+	CountryID
+	) ON UPDATE  NO ACTION 
+	 ON DELETE  NO ACTION,
+CONSTRAINT
+	FK_UserSolution_userprofilepositions FOREIGN KEY
+	(
+	UserListingID
+	) REFERENCES dbo.userprofilepositions
+	(
+	UserListingID
+	) ON UPDATE  NO ACTION 
+	 ON DELETE  NO ACTION 
+);
--- web/_DBUpdate/is796 - G - 2 - Renamed UserListingSolution to UserSolution.sql
+++
@@ -1,1 +0,0 @@
-EXEC sp_rename 'UserListingSolution', 'UserSolution'
