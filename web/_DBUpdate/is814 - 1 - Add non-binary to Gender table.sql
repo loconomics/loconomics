9be5891e35edecae86797fa745d5168dc2524f42
Joshua Danielson <joshua.danielson@loconomics.com>
2018-03-23 08:38:59 -0700

    Revert "Add "Non-binary" to Gender table"
    This reverts commit 1c655b11025bb977f034fcadd52d4699894f80fc.

--- web/_DBUpdate/is814 - 1 - Add non-binary to Gender table.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO dbo.Gender (GenderID, LanguageID, CountryID, GenderSingular, GenderPlural, SubjectPronoun, ObjectPronoun, PossesivePronoun) VALUES (3, 1, 1, N'Non-binary', N'People', 'they', 'them', 'their');
