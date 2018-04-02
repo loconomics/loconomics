317279edf727e85908801eeac1764d7fbc838742
IagoSRL <iagosrl@gmail.com>
2018-04-02 18:03:29 +0200

    User badges data module (item and listing, no cache) #746)

--- app/source/js/data/userBadges.js
+++ app/source/js/data/userBadges.js
@@ -3,15 +3,24 @@
  */
 
 import CachedDataProvider from './helpers/CachedDataProvider';
-import LocalForageItemDataProviderDriver from './helpers/LocalForageItemDataProviderDriver';
-import LocalForageSingleDataProviderDriver from './helpers/LocalForageSingleDataProviderDriver';
 import RestItemDataProviderDriver from './helpers/RestItemDataProviderDriver';
 import RestSingleDataProviderDriver from './helpers/RestSingleDataProviderDriver';
-import localforage from './drivers/localforage';
 import rest from './drivers/restClient';
 
-const API_NAME = 'me/badges';
-const LOCAL_KEY = 'badges';
+const API_NAME_ITEM = 'me/badges';
+const API_NAME_LISTING = 'me/badges/by-listing';
+
+/**
+ * Fake local driver that does NOT cache.
+ * TODO: Implement cache taking care of invalidations and interrelations between indexes and items.
+ * There are interelations that make implementing cache difficult here, but in order to keep the same API
+ * as when we implement it and follow other data modules design, we use CachedDataProvider instances still.
+ */
+const noCache = {
+    fetch: () => Promise.resolve(undefined),
+    push: (data) => Promise.resolve(data),
+    delete: () => Promise.resolve()
+};
 
 /**
  * Get and manage individual user badges entries.
@@ -29,8 +38,8 @@ export function item(id) {
     return new CachedDataProvider({
         // 5 minutes
         ttl: 5 * 60 * 1000,
-        remote: new RestItemDataProviderDriver(rest, API_NAME, id),
-        local: new LocalForageItemDataProviderDriver(localforage, LOCAL_KEY, id)
+        remote: new RestItemDataProviderDriver(rest, API_NAME_ITEM, id),
+        local: noCache
     });
 }
 
@@ -39,14 +48,15 @@ export function item(id) {
  * @param {number} id The platformID
  * @returns {CachedDataProvider<rest/Platform>}
  * Usage:
- * - item(platformID).onData.subscribe(fn) to get the list, fn keeps being triggered on incoming updated data
- * - item(platformID).onDataError.subscribe(fn) to get notified of errors happening as of onData
+ * - const provider = byListing(userListingID);
+ * - provider.onData.subscribe(fn) to get the list, fn keeps being triggered on incoming updated data
+ * - provider.onDataError.subscribe(fn) to get notified of errors happening as of onData
  */
 export function byListing(userListingID) {
     return new CachedDataProvider({
-        // 10 minutes
-        ttl: 10 * 60 * 1000,
-        remote: new RestSingleDataProviderDriver(rest, API_NAME + '/' + userListingID),
-        local: new LocalForageSingleDataProviderDriver(localforage, LOCAL_KEY + '/' + userListingID)
+        // 5 minutes
+        ttl: 5 * 60 * 1000,
+        remote: new RestSingleDataProviderDriver(rest, API_NAME_LISTING + '/' + userListingID),
+        local: noCache
     });
 }
