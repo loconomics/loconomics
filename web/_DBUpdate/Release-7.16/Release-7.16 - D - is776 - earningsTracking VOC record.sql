81edd4a9fffad141bb68d17cab538f77b3ca9e27
IagoSRL <iagosrl@gmail.com>
2018-04-17 15:40:39 +0200

    Recovered DB SQL scripts back to release 7.10, reorganized per release folders to make it easy to know what is left from being applied locally for Sam

---
+++ web/_DBUpdate/Release-7.16/Release-7.16 - D - is776 - earningsTracking VOC record.sql
@@ -0,0 +1,1 @@
+INSERT INTO dbo.VOCElement (VOCElementID, LanguageID, CountryID, VOCElementName, ScoreStartValue, ScoreMidValue, ScoreEndValue, ScoreStartLabel, ScoreMidLabel, ScoreEndLabel, CreateDate, UpdatedDate, ModifiedBy, Active) VALUES (22, 1, 1, 'Earnings Tracking', null, null, null, null, null, null, '2018-01-25 00:00:00.0', '2018-01-25 00:00:00.0', 'jd', 1);
