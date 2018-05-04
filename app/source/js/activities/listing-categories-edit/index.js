c35923fc4b085d550d93fda122bc91ccbf68b9e2
IagoSRL <iagosrl@gmail.com>
2018-05-04 14:07:40 +0200

    Completed the listing-categories-edit activity #796

--- app/source/js/activities/listing-categories-edit/index.js
+++ app/source/js/activities/listing-categories-edit/index.js
@@ -1,8 +1,8 @@
 /**
- * Allows a professional to edit an earnings entry.
+ * Let's a professional to edit the 'search categories' (AKA solutions) has
+ * attached to a specific listing.
  *
  * @module activities/listing-categories-edit
- *
  */
 
 import '../../kocomponents/listing/solutions-editor';
@@ -11,6 +11,7 @@ import Activity from '../../components/Activity';
 import UserType from '../../enums/UserType';
 import ko from 'knockout';
 import template from './template.html';
+import { item as userListing } from '../../data/userListings';
 
 const ROUTE_NAME = 'listing-categories-edit';
 
@@ -23,24 +24,35 @@ export default class ListingCategoriesEditActivity extends Activity {
         super($activity, app);
 
         this.accessLevel = UserType.serviceProfessional;
-        this.navBar = Activity.createSubsectionNavBar(null);
+        this.navBar = Activity.createSubsectionNavBar('Listing Editor');
 
         /**
-         * Creates a placeholder for an "out" parameter to be
-         * populated by the component.
-        */
-        this.jobTitleName = ko.observable('');
-
-        /**
-         * Creates a placeholder for the external listing ID
-         * to be populated using the show(state) method below.
+         * @member {KnockoutObservable<number>}
          */
         this.jobTitleID = ko.observable();
         /**
+         * @member {KnockoutObservable<number>}
+         */
+        this.userListingID = ko.observable();
+        /**
+         * @member {KnockoutObservable<string>}
+         */
+        this.listingTitle = ko.observable('');
+
+        /**
          * Title uses a pureComputed to ensure the platformName
          * is updated.
-        */
-        this.title = ko.pureComputed( () => 'Edit ' + this.jobTitleName() + ' listing categories');
+         */
+        this.title = ko.pureComputed( () => 'Edit ' + this.listingTitle() + ' listing categories');
+        /**
+         * Automatic back link based on listing
+         * @member {KnockoutComputed<string>}
+         */
+        this.navBackLink = ko.pureComputed(() => `/listingEditor/${this.jobTitleID()}`);
+        // Connect back link and listingTitle to the navbar
+        this.navBackLink.subscribe(this.navBar.leftAction().link);
+        this.navBackLink.subscribe(() => this.navBar.leftAction().isShell(false));
+        this.listingTitle.subscribe(this.navBar.leftAction().text);
 
         /**
          * After data being saved, notice and go back
@@ -50,14 +62,29 @@ export default class ListingCategoriesEditActivity extends Activity {
         };
     }
 
+    /**
+     * The route accepted is like `/${jobTitleID}`
+     * @param {Object} state Parameter for the listing to load
+     */
     show(state) {
         super.show(state);
-        var params = state.route && state.route.segments;
+
         /**
-         * ID is the first segment in the activity
-         * URL.
+         * The jobTitleID from the first URL segment
          */
-        this.jobTitleID(params[0] |0);
+        this.jobTitleID(state.route.segments[0]);
+
+        // If no ID given, we cannot load anything, go back
+        if (!state.route.segments[0]) {
+            this.app.shell.goBack();
+        }
+
+        // Pull listing title and jobTitleID from the listing data
+        const provider = userListing(this.jobTitleID());
+        this.subscribeTo(provider.onData, (listing) => {
+            this.listingTitle(listing.title);
+            this.userListingID(listing.userListingID);
+        });
     }
 }
 
