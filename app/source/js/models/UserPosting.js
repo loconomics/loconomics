96181c561e448261ec38b63b544c22a0fe255263
IagoSRL <iagosrl@gmail.com>
2018-05-09 13:41:43 +0200

    Adjusted UserPosting app model to server output #820

--- app/source/js/models/UserPosting.js
+++ app/source/js/models/UserPosting.js
@@ -3,6 +3,17 @@
  */
 import Model from './Model';
 
+export class UserPostingSpecialization {
+    constructor(values) {
+        Model(this);
+
+        this.model.defProperties({
+            specializationID: 0,
+            name: ''
+        }, values);
+    }
+}
+
 export default class UserPosting {
     constructor(values) {
         Model(this);
@@ -11,11 +22,18 @@ export default class UserPosting {
             userPostingID: 0,
             userID: 0,
             solutionID: 0,
+            solutionName: '',
             postingTemplateID: null,
             statusID: 0,
             title: '',
-            neededSpecializationIds: null,
-            desiredSpecializationIds: null,
+            neededSpecializations: {
+                isArray: true,
+                Model: UserPostingSpecialization
+            },
+            desiredSpecializations: {
+                isArray: true,
+                Model: UserPostingSpecialization
+            },
             languageID: null,
             countryID: null,
             createdDate: null,
