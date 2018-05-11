a988b1a09628718cf9f6cdee414737c5dc036cc8
IagoSRL <iagosrl@gmail.com>
2018-05-11 13:50:25 +0200

    Simplified implementation of posting-suggested komponent as template-only #820

--- app/source/js/kocomponents/posting/suggested/index.js
+++ app/source/js/kocomponents/posting/suggested/index.js
@@ -3,34 +3,14 @@
  *
  * @module kocomponents/posting/suggested
  *
+ * @param {object} params
+ * @param {(models/UserPosting|KnockoutObservable<models/UserPosting>)} params.data
  */
-import Komponent from '../../helpers/KnockoutComponent';
-import getObservable from '../../../utils/getObservable';
 import ko from 'knockout';
 import template from './template.html';
 
 const TAG_NAME = 'posting-suggested';
 
-/**
- * Component
- */
-export default class PostingSuggested extends Komponent {
-
-    static get template() { return template; }
-
-    /**
-     * @param {object} params
-     * @param {(models/UserPosting|KnockoutObservable<models/UserPosting>)} params.data
-     */
-    constructor(params) {
-        super();
-
-        /**
-         * Holds the ID for the earnings entry being viewed.
-         * @member {KnockoutObservable<models/UserPosting>}
-         */
-        this.data = getObservable(params.data);
-    }
-}
-
-ko.components.register(TAG_NAME, PostingSuggested);
+ko.components.register(TAG_NAME, {
+    template: template
+});
