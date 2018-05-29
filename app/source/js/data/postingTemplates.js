6be60db3c186aff9f1bb6b1811c6619b9ab4875d
IagoSRL <iagosrl@gmail.com>
2018-05-29 17:43:58 +0200

    Connected posting templates data to REST #820

--- app/source/js/data/postingTemplates.js
+++ app/source/js/data/postingTemplates.js
@@ -1,24 +1,15 @@
 /**
  * Access posting templates.
- *
- * IMPORTANT: Using EMBEDDED DATA rather than connected to the REST API for now
  */
 import CachedDataProvider from './helpers/CachedDataProvider';
-//import LocalForageItemDataProviderDriver from './helpers/LocalForageItemDataProviderDriver';
-//import RestItemDataProviderDriver from './helpers/RestItemDataProviderDriver';
-import data from '../data/embedded/postingTemplates';
-//import localforage from './drivers/localforage';
-//import rest from './drivers/restClient';
+import LocalForageItemDataProviderDriver from './helpers/LocalForageItemDataProviderDriver';
+import RestItemDataProviderDriver from './helpers/RestItemDataProviderDriver';
+import localforage from './drivers/localforage';
+import rest from './drivers/restClient';
 
-// const API_NAME = 'posting-templates';
-// const LOCAL_KEY = 'postingTemplates';
-// const ID_PROPERTY_NAME = 'postingTemplateID';
-
-const embeddedDataProvider = (id) => ({
-    fetch: () => Promise.resolve(data[id]),
-    push: (d) => Promise.resolve(d),
-    delete: () => Promise.resolve()
-});
+const API_NAME = 'posting-templates';
+const LOCAL_KEY = 'postingTemplates';
+const ID_PROPERTY_NAME = 'postingTemplateID';
 
 /**
  * Provides access to an API to fetch a specific record.
@@ -30,11 +21,10 @@ const embeddedDataProvider = (id) => ({
  * - dataProvider.onLoadError.subscribe(fn) to get notified of errors happening as of onData
  */
 export function item(id) {
-    //const localItemDriver = new LocalForageItemDataProviderDriver(localforage, LOCAL_KEY, id, ID_PROPERTY_NAME);
     return new CachedDataProvider({
         // 1 day
         ttl: 1 * 24 * 60 * 60 * 1000,
-        remote: embeddedDataProvider(id),
-        local: embeddedDataProvider(id)
+        remote: new RestItemDataProviderDriver(rest, API_NAME, id),
+        local: new LocalForageItemDataProviderDriver(localforage, LOCAL_KEY, id, ID_PROPERTY_NAME)
     });
 }
