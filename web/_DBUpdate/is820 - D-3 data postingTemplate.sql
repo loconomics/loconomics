61ab10e89d734bfa4ff47203188a245c9b5886a7
IagoSRL <iagosrl@gmail.com>
2018-05-29 14:07:19 +0200

    Tables postingTemplate and postingTemplateQuestion with init data #820

---
+++ web/_DBUpdate/is820 - D-3 data postingTemplate.sql
@@ -0,0 +1,1 @@
+INSERT INTO postingTemplate ([postingTemplateID],[name],[languageID],[countryID],[createdDate],[updatedDate],[modifiedBy]) VALUES (1,'Default Home Projects',1, 1, '2018-05-29', '2018-05-29', 'ils')
