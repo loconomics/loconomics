dab2d587a8ada80c0bc99907c5a4251298a3583f
IagoSRL <iagosrl@gmail.com>
2018-07-26 18:47:11 +0200

    Impl. pricingSummary isRemoteService at App, used to include the editable address at appointment when properly #784

--- app/source/js/models/PricingSummary.js
+++ app/source/js/models/PricingSummary.js
@@ -7,7 +7,7 @@ var ko = require('knockout');
 var PricingSummaryDetail = require('./PricingSummaryDetail');
 
 module.exports = function PricingSummary(values) {
-    
+
     Model(this);
 
     this.model.defProperties({
@@ -15,7 +15,7 @@ module.exports = function PricingSummary(values) {
         pricingSummaryRevision: 0,
         serviceDurationMinutes: null,
         firstSessionDurationMinutes: null,
-        
+
         subtotalPrice: null,
         clientServiceFeePrice: null,
         totalPrice: null,
@@ -28,16 +28,16 @@ module.exports = function PricingSummary(values) {
         paymentProcessingFeeFixed: null,
         firstTimeServiceFeeMaximum: null,
         firstTimeServiceFeeMinimum: null,
-        
+
         createdDate: null,
         updatedDate: null,
-        
+
         details: {
             Model: PricingSummaryDetail,
             isArray: true
         }
     }, values);
-    
+
     this.servicesSummary = ko.pureComputed(function() {
         return this.details()
         .map(function(detail) {
@@ -45,4 +45,20 @@ module.exports = function PricingSummary(values) {
         }).join(', ');
     }, this)
     .extend({ rateLimit: { method: 'notifyWhenChangesStop', timeout: 20 } });
+
+    /**
+     * Whether the whole service(s) included is done remotely (without needing
+     * and address where must to be performed).
+     * It returns true too when there is no details (since there is no services,
+     * doesn't require an address => is remote).
+     * @member {KnockoutComputed<boolean>}
+     */
+    this.isRemoteService = ko.pureComputed(() => {
+        for (const detail of this.details()) {
+            if (!detail.isRemoteService()) {
+                return false;
+            }
+        }
+        return true;
+    });
 };
