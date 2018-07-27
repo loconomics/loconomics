5e825d1d4e7bf259eb7adce360483e2821e17a66
IagoSRL <iagosrl@gmail.com>
2018-07-27 11:59:12 +0200

    Removed all pending detached things, documented at the issue #731 (see all comments)

--- web/_detachedThings/front-end release-6.3/Scripts/LC/RegExp.quote.js
+++
@@ -1,4 +0,0 @@
-﻿// http://stackoverflow.com/questions/2593637/how-to-escape-regular-expression-in-javascript
-RegExp.quote = function (str) {
-  return (str + '').replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1");
-};
